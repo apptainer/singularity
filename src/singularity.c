43fac2b243dc06cb6f5a1cd9d5c7c502020ca86c
Krishna Muriki <kmuriki@57.223.243.131.in-addr.dhcp.lbl.gov>
2015-10-18 02:57:14 -0700

    Added input argument checking and some code reorganization for clear flow

--- src/singularity.c
+++ src/singularity.c
@@ -1,75 +1,103 @@
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/mount.h>
 #include <unistd.h>
+      
+show_usage() {
+    printf("Usage : singularity filename.sapp application-arguments\n");
+    printf("        -h|-help for this usage info\n\n");
+}
 
-
+need_help(char *arg1) {
+    if( !strcmp(arg1,"-h") || !strcmp(arg1,"--h") || !strcmp(arg1,"-help") || !strcmp(arg1,"--help")) {    
+       return(1);
+    } else {
+       return(0);
+    }
+}
+    
 int main(int argc, char *argv[]) {
-    int exit_status = 255;
-    int SMALLBUFF = 64;
-    int BUFF = 512;
+
+    //Make sure the UID is set back to the user
     int uid = getuid();
     int euid = geteuid();
-    int sapp_file_len = strlen(argv[1]);
-    int arg_string_len = 0;
-    int i = 0;
-    int j = 0;
+    seteuid(uid);
+
+    //Check for argument count and help option
+    int exit_status = 255;
+    if(argc < 2 || need_help(argv[1])) {
+       show_usage();
+       return(exit_status);
+    }
+
+    int i=0, j=0;
+    int SMALLBUFF = 64;
+    int BUFF = 512;
+    char cwd[BUFF];
+    int sapp_file_len;
     char *sapp_file;
+    int arg_string_len = 0;
+    char *arg_string;
     char *tmpdir;
     char *mktmpdir;
     char *rmtmpdir;
     char *explode_sapp;
     char *run_cmd;
-    char *arg_string;
 //    char *bind_mountpoint;
-    char cwd[BUFF];
-
-    seteuid(uid);
     
     getcwd(cwd, BUFF);
 
+    //Get sapp file
+    sapp_file_len = strlen(argv[1]);
+    sapp_file = (char *) malloc(sapp_file_len + 1);
+    strcpy(sapp_file, argv[1]);
+
+
+   //Get app arguments
     for (i = 2; i < argc; i++) {
         arg_string_len += strlen(argv[i]) + 1;
     }
     arg_string_len ++;
-
-    sapp_file = (char *) malloc(sapp_file_len + 1);
-    tmpdir = (char *) malloc(SMALLBUFF);
-    mktmpdir = (char *) malloc(SMALLBUFF);
-    rmtmpdir = (char *) malloc(SMALLBUFF);
-    explode_sapp = (char *) malloc(BUFF + sapp_file_len);
-    run_cmd = (char *) malloc(BUFF + arg_string_len);
     arg_string = (char *) malloc(arg_string_len);
-//    bind_mountpoint = (char *) malloc(BUFF);
 
-    strcpy(sapp_file, argv[1]);
-
-    
     for (i = 2; i < argc; i++) {
         memcpy(arg_string + j, argv[i], strlen(argv[i]));
         j += strlen(argv[i]);
         arg_string[j] = ' ';
         j++;
     }
-
     arg_string[j+1] = '\0';
 
+
+    //Setup temporary space to work with
+    tmpdir = (char *) malloc(SMALLBUFF);
     snprintf(tmpdir, /*sizeof(tmpdir)*/ SMALLBUFF, "/tmp/.singularity.%d.%d", uid, getpid());
+
+    mktmpdir = (char *) malloc(SMALLBUFF);
     snprintf(mktmpdir, /*sizeof(mktmpdir)*/ SMALLBUFF, "mkdir -p %s", tmpdir);
+
+    rmtmpdir = (char *) malloc(SMALLBUFF);
     snprintf(rmtmpdir, /*sizeof(mktmpdir)*/ SMALLBUFF, "rm -rf %s", tmpdir);
-    snprintf(explode_sapp, BUFF + sapp_file_len, "zcat %s | (cd %s; cpio -id --quiet)", sapp_file, tmpdir);
-    snprintf(run_cmd, BUFF, "/run %s", arg_string);
-//    snprintf(bind_mountpoint, BUFF, "%s/home", tmpdir);
 
-    //Prepare
     system(mktmpdir);
-    system(explode_sapp);
-//    mkdir(bind_mountpoint, 0770);
 
-    //Chroot
+    //Explode the application's cpio archive
+    explode_sapp = (char *) malloc(BUFF + sapp_file_len);
+    snprintf(explode_sapp, BUFF + sapp_file_len, "zcat %s | (cd %s; cpio -id --quiet)", sapp_file, tmpdir);
+
+    run_cmd = (char *) malloc(BUFF + arg_string_len);
+    snprintf(run_cmd, BUFF, "/run %s", arg_string);
+
+    system(explode_sapp);
+
+    //Setup for the bind mounts
+    //bind_mountpoint = (char *) malloc(BUFF);
+    //snprintf(bind_mountpoint, BUFF, "%s/home", tmpdir);
+    //mkdir(bind_mountpoint, 0770);
+
+    //Start the Chroot
     seteuid(0);
     /*
      * It doesn't appear that the mount is necessary.. the chdir command
