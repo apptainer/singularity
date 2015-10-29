89baba8d16aef7cd5cec7775af0f34a0d109b3bc
Gregory M. Kurtzer <gmkurtzer@lbl.gov>
2015-10-29 10:55:31 -0400

    Cleanups, use setreuid/setregid, and confirm drop

--- src/sexec.c
+++ src/sexec.c
@@ -18,21 +18,24 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/param.h>
 #include <errno.h> 
 #include <string.h>
 #include <fcntl.h>  
+#include <grp.h>
 #include "config.h"
 
 int main(int argc, char **argv) {
     char *sappdir;
     struct stat sappdirAttribs = {0};
-    int uid = getuid();
+    uid_t uid = getuid();
+    gid_t gid = getgid();
     int cwd_fd;
 
     sappdir = getenv("SAPPDIR");
 
     /*
-     * Open a FD to where we are now
+     * Open a FD to the current working dir.
      */
 
     if ( (cwd_fd = open(".", O_RDONLY)) < 0 ) {
@@ -42,32 +45,32 @@ int main(int argc, char **argv) {
 
 
     /*
-     * Sanity Checks
+     * Sanity Checks, exit if any don't match.
      */
 
     if ( sappdir == NULL ) {
-        fprintf(stderr, "ERROR: SAPPDIR undefined\n");
+        fprintf(stderr, "ERROR: SAPPDIR undefined!\n");
         return(1);
     }
 
     if (lstat(sappdir, &sappdirAttribs) < 0) {
-        fprintf(stderr, "ERROR: Could not stat %s\n", sappdir);
+        fprintf(stderr, "ERROR: Could not stat %s!\n", sappdir);
         return(1);
     }
 
     if ( uid != (int)sappdirAttribs.st_uid ) {
-        fprintf(stderr, "ERROR: Will not execute in a SAPPDIR you don't own. (%s:%d)\n", sappdir, (int)sappdirAttribs.st_uid);
+        fprintf(stderr, "ERROR: Will not execute in a SAPPDIR you don't own. (%s:%d)!\n", sappdir, (int)sappdirAttribs.st_uid);
         return(255);
     }
 
 
     /*
-     * Warning! Danger! Entering the forbidden zone!
+     * Warning! Danger! Entering the privledged zone!
      */
 
     // Get root
     if ( seteuid(0) != 0 ) {
-        fprintf(stderr, "ERROR: Could not escalate privledges\n");
+        fprintf(stderr, "ERROR: Could not escalate privledges!\n");
         return(1);
     }
 
@@ -78,19 +81,34 @@ int main(int argc, char **argv) {
     }
 
     // Dump privs
-    if ( seteuid(uid) != 0 ) {
-        fprintf(stderr, "ERROR: Could not dump effective privledges\n");
+    if ( setregid(gid, gid) != 0 ) {
+        fprintf(stderr, "ERROR: Could not dump real/effective group privledges!\n");
         return(255);
     }
-    if ( setuid(uid) != 0 ) {
-        fprintf(stderr, "ERROR: Could not dump privledges\n");
+    if ( setreuid(uid, uid) != 0 ) {
+        fprintf(stderr, "ERROR: Could not dump real/effective user privledges!\n");
         return(255);
     }
 
-    // Chdir
+
+    /*
+     * Out of the immediate danger zone... whew!
+     */
+
+    // Confirm we no longer have any escalated privledges
+    if ( setuid(0) == 0 ) {
+        fprintf(stderr, "ERROR: Root not allowed here!\n");
+        return(1);
+    }
+
+    // change directory back to starting point
     if ( fchdir(cwd_fd) != 0 ) {
-        fprintf(stderr, "ERROR: Could not fchdir\n");
-        return(255);
+        fprintf(stderr, "ERROR: Could not fchdir!\n");
+        return(1);
+    }
+    if ( close(cwd_fd) != 0 ) {
+        fprintf(stderr, "ERROR: Could not close cwd_fd!\n");
+        return(1);
     }
 
     // Exec 
