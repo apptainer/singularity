bebff178e3b6fb957f32cf16cf666eef17da631d
Gregory M. Kurtzer <gmkurtzer@lbl.gov>
2015-10-29 00:52:41 -0400

    Fixes, cleanups, and happy times.

--- src/sexec.c
+++ src/sexec.c
@@ -18,28 +18,32 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <errno.h> 
+#include <string.h>
+#include <fcntl.h>  
 #include "config.h"
 
 int main(void) {
-    char *cwd;
     char *sappdir;
     struct stat sappdirAttribs = {0};
     int uid = getuid();
+    int cwd_fd;
 
     sappdir = getenv("SAPPDIR");
 
-    cwd = (char *) malloc(1024);
-    getcwd(cwd, 1024);
+    /*
+     * Open a FD to where we are now
+     */
+
+    if ( (cwd_fd = open(".", O_RDONLY)) < 0 ) {
+        fprintf(stderr, "ERROR: Could not open cwd fd (%s)!\n", strerror(errno));
+        return(1);
+    }
 
 
     /*
      * Sanity Checks
      */
-
-    if ( cwd == NULL ) {
-        fprintf(stderr, "ERROR: Could not obtain current working directory\n");
-        return(1);
-    }
 
     if ( sappdir == NULL ) {
         fprintf(stderr, "ERROR: SAPPDIR undefined\n");
@@ -62,26 +66,41 @@ int main(void) {
      */
 
     // Get root
-    seteuid(0);
+    if ( seteuid(0) != 0 ) {
+        fprintf(stderr, "ERROR: Could not escalate privledges\n");
+        return(1);
+    }
 
-    // Do chroot
+    // Do chroot before dropping privs to escape chroot
     if ( chroot(sappdir) != 0 ) {
         fprintf(stderr, "ERROR: failed enter SAPPDIR: %s\n", sappdir);
         return(255);
     }
 
     // Dump privs
-    seteuid(uid);
-    setuid(uid);
-
-    // Chdir and exec code
-    if ( chdir(cwd) != 0 ) {
-        fprintf(stderr, "ERROR: Could not change to working directory\n");
-        return(1);
+    if ( seteuid(uid) != 0 ) {
+        fprintf(stderr, "ERROR: Could not dump effective privledges\n");
+        return(255);
     }
-    execv("/singularity", NULL);
+    if ( setuid(uid) != 0 ) {
+        fprintf(stderr, "ERROR: Could not dump privledges\n");
+        return(255);
+    }
+
+    // Chdir
+    if ( fchdir(cwd_fd) != 0 ) {
+        fprintf(stderr, "ERROR: Could not fchdir\n");
+        return(255);
+    }
+
+    // Exec 
+    if ( execv("/singularity", NULL) != 0 ) {
+    //if ( execv("/singularity", (char **) "") != 0 ) {
+        fprintf(stderr, "ERROR: Failed to exec SAPP envrionment\n");
+        return(2);
+    }
 
     // We should *never* reach here, but if we do... error out hard!
-    fprintf(stderr, "ERROR: Failed to exec SAPP file\n");
+    fprintf(stderr, "ERROR: We should not be here!\n");
     return(255);
 }
