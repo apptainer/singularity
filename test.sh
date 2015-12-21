e30d1470ad8a257a3037ad05d48cc80a0c2df21d
Gregory M. Kurtzer <gmkurtzer@lbl.gov>
2015-12-20 20:40:10 -0500

    More tests and cleanups

--- test.sh
+++ test.sh
@@ -19,80 +19,88 @@ export SINGULARITY_CACHEDIR MESSAGELEVEL
 
 
 
-echo "Gaining/checking sudo access..."
+echo "${BLUE}Gaining/checking sudo access...${NORMAL}"
 sudo true
 
-echo "Building/Installing Singularity to temporary directory"
-exe 0 sh ./autogen.sh --prefix="$TEMPDIR"
-exe 0 make
-exe 0 make install
-exe 0 sudo make install-perms
+echo "${BLUE}Building/Installing Singularity to temporary directory${NORMAL}"
+stest 0 sh ./autogen.sh --prefix="$TEMPDIR"
+stest 0 make
+stest 0 make install
+stest 0 sudo make install-perms
 
 PATH="$TEMPDIR/bin:$PATH"
 
-echo "Creating temp working space at: $TEMPDIR/tmp"
-exe 0 mkdir -p "$TEMPDIR/tmp"
-exe 0 pushd "$TEMPDIR/tmp"
+echo "${BLUE}Creating temp working space at: $TEMPDIR${NORMAL}"
+stest 0 mkdir -p "$TEMPDIR"
+stest 0 pushd "$TEMPDIR"
 
-echo "Running tests..."
-cat <<EOF > example.sspec
-Name: cat
-Exec: /bin/cat
-#DebugOS: 1
-EOF
-exe 0 singularity --quiet build example.sspec
-exe 0 ./cat.sapp example.sspec
-echo "Testing pipeline"
-if ! cat example.sspec | ./cat.sapp | grep -q '^Name'; then
-    echo "failed: cat example.sspec | ./cat.sapp | grep -q '^Name'"
-    exit 1
-fi
-exe 1 singularity list
-exe 0 singularity install cat.sapp
-exe 0 singularity list
-exe 0 singularity check cat
-exe 0 singularity run cat example.sspec
-exe 0 singularity run cat /etc/hosts
-exe 0 singularity run cat /etc/resolv.conf
-exe 1 singularity run cat /etc/passwd
-exe 1 singularity run cat /etc/shadow
-echo "Testing pipeline"
-if ! cat example.sspec | singularity run cat | grep -q '^Name'; then
-    echo "failed: cat example.sspec | singularity run cat | grep -q '^Name'"
-    exit 1
-fi
-exe 0 singularity strace cat example.sspec
-exe 0 singularity test cat
-echo "echo hello world" | exe 0 singularity shell cat
-exe 0 singularity delete cat
-exe 1 singularity list
+echo "${BLUE}Running tests...${NORMAL}"
 
-echo "Testing base Singularity functions"
-exe 0 singularity
-exe 0 singularity help
-exe 0 singularity help build
-exe 0 singularity help check
-exe 0 singularity help delete
-exe 0 singularity help help
-exe 0 singularity help install
-exe 0 singularity help list
-exe 0 singularity help run
-exe 0 singularity help shell
-exe 0 singularity help specgen
-exe 0 singularity help strace
-exe 1 singularity help blahblah
-exe 0 singularity -h
-exe 0 singularity --help
-exe 0 singularity --version
+# Testing singularity internal commands
+stest 0 singularity
+stest 0 singularity help
+stest 0 singularity help build
+stest 0 singularity help check
+stest 0 singularity help delete
+stest 0 singularity help help
+stest 0 singularity help install
+stest 0 singularity help list
+stest 0 singularity help run
+stest 0 singularity help shell
+stest 0 singularity help specgen
+stest 0 singularity help strace
+stest 1 singularity help blahblah
+stest 0 singularity -h
+stest 0 singularity --help
+stest 0 singularity --version
+
+# Creating a minimal container
+stest 0 sh -c "echo 'Name: cat' > example.sspec"
+stest 0 sh -c "echo 'Exec: /bin/cat' >> example.sspec"
+stest 0 singularity --quiet build example.sspec
+
+# Running basic tests on sapp directly
+stest 1 ls $TEMPDIR/tmp/*
+stest 0 ./cat.sapp example.sspec
+stest 0 ./cat.sapp /etc/hosts
+stest 0 ./cat.sapp /etc/resolv.conf
+stest 1 ./cat.sapp /etc/passwd
+stest 1 ./cat.sapp /etc/shadow
+stest 0 sh -c "cat example.sspec | ./cat.sapp | grep -q '^Name'"
+stest 1 ls $TEMPDIR/tmp/*
+
+# Making sure cache is empty and installing cat.sapp
+stest 1 singularity list
+stest 0 singularity install cat.sapp
+stest 0 singularity list
+stest 0 singularity check cat
+
+# Running 'cat' from singularity cache
+stest 0 singularity run cat example.sspec
+stest 0 singularity run cat /etc/hosts
+stest 0 singularity run cat /etc/resolv.conf
+stest 1 singularity run cat /etc/passwd
+stest 1 singularity run cat /etc/shadow
+stest 0 sh -c "cat example.sspec | singularity run cat | grep -q '^Name'"
+
+# Checking additional tests and functions of installed containers
+stest 0 sh -c "echo 'exit' | singularity shell cat"
+stest 1 sh -c "echo 'exit 1' | singularity shell cat"
+stest 0 sh -c "echo 'echo hello' | singularity shell cat | grep -q 'hello'"
+stest 0 singularity strace cat example.sspec
+stest 0 singularity test cat
+
+# Cleaning up
+stest 0 singularity delete cat
+stest 1 singularity list
 
 
 
-
-echo "Cleaning up"
-exe 0 popd
-exe 0 rm -rf "$TEMPDIR/tmp"
-exe 0 make maintainer-clean
+echo "${BLUE}Cleaning up${NORMAL}"
+stest 0 popd
+stest 0 rm -rf "$TEMPDIR"
+stest 0 make maintainer-clean
 
 echo
-echo "Done. All tests completed successfully"
+echo "${GREEN}Done. All tests completed successfully${NORMAL}"
 echo
