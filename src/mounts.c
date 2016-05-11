b68f59cbdf31dd8797c81de03823a92636d84c65
Gregory M. Kurtzer <gmkurtzer@lbl.gov>
2016-05-11 04:37:40 -0700

    Don't use discard with ext3

--- src/mounts.c
+++ src/mounts.c
@@ -54,15 +54,15 @@ int mount_image(char * loop_device, char * mount_point, int writable) {
     }
 
     if ( writable > 0 ) {
-        if ( mount(loop_device, mount_point, "ext3", MS_NOSUID, "discard") < 0 ) {
-            if ( mount(loop_device, mount_point, "ext4", MS_NOSUID, "discard") < 0 ) {
+        if ( mount(loop_device, mount_point, "ext4", MS_NOSUID, "discard") < 0 ) {
+            if ( mount(loop_device, mount_point, "ext3", MS_NOSUID, "") < 0 ) {
                 fprintf(stderr, "ERROR: Failed to mount '%s' at '%s': %s\n", loop_device, mount_point, strerror(errno));
                 return(-1);
             }
         }
     } else {
-        if ( mount(loop_device, mount_point, "ext3", MS_NOSUID|MS_RDONLY, "discard") < 0 ) {
-            if ( mount(loop_device, mount_point, "ext4", MS_NOSUID|MS_RDONLY, "discard") < 0 ) {
+        if ( mount(loop_device, mount_point, "ext4", MS_NOSUID|MS_RDONLY, "discard") < 0 ) {
+            if ( mount(loop_device, mount_point, "ext3", MS_NOSUID|MS_RDONLY, "") < 0 ) {
                 fprintf(stderr, "ERROR: Failed to mount '%s' at '%s': %s\n", loop_device, mount_point, strerror(errno));
                 return(-1);
             }
