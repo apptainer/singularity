a177db3316e3c6a09f019b50e143a3391109a546
Gregory M. Kurtzer <gmkurtzer@lbl.gov>
2016-07-13 12:43:11 -0700

    Updates to test.sh script

--- test.sh
+++ test.sh
@@ -25,23 +25,22 @@ export SINGULARITY_CACHEDIR MESSAGELEVEL
 /bin/echo "Gaining/checking sudo access..."
 sudo true
 
-#if [ -z "$CLEAN_SHELL" ]; then
-#    /bin/echo "Building/Installing Singularity to temporary directory"
-#    /bin/echo "Reinvoking in a clean shell"
-#    sleep 1
-#    exec env -i CLEAN_SHELL=1 PATH="/bin:/usr/bin:/sbin:/usr/sbin" bash "$0" "$*"
-#fi
+if [ -z "$CLEAN_SHELL" ]; then
+    /bin/echo "Building/Installing Singularity to temporary directory"
+    /bin/echo "Reinvoking in a clean shell"
+    sleep 1
+    exec env -i CLEAN_SHELL=1 PATH="/bin:/usr/bin:/sbin:/usr/sbin" bash "$0" "$*"
+fi
 
 . ./libexec/functions
 
-set +e
+make maintainer-clean >/dev/null 2>&1
+stest 0 sh ./autogen.sh --prefix="$TEMPDIR"
+stest 0 make
+stest 0 sudo make install
 
-#make maintainer-clean >/dev/null 2>&1
-#stest 0 sh ./autogen.sh --prefix="$TEMPDIR"
-#stest 0 make
-#stest 0 sudo make install
-#
-#PATH="$TEMPDIR/bin:$PATH"
+PATH="$TEMPDIR/bin:$PATH"
+MESSAGELEVEL=5
 
 /bin/echo
 /bin/echo "SINGULARITY_CACHEDIR=$SINGULARITY_CACHEDIR"
@@ -50,7 +49,7 @@ set +e
 
 /bin/echo "Creating temp working space at: $TEMPDIR"
 stest 0 mkdir -p "$TEMPDIR"
-#stest 0 pushd "$TEMPDIR"
+stest 0 pushd "$TEMPDIR"
 
 /bin/echo
 /bin/echo "Running base tests..."
@@ -77,8 +76,8 @@ stest 1 singularity bogus help
 /bin/echo "Building test container..."
 /bin/echo
 
-stest 0 sudo singularity -d create -s 15 "$TSTIMG"
-stest 0 sudo singularity bootstrap "$TSTIMG" "$STARTDIR/examples/busybox.def"
+stest 0 sudo singularity create -s 568 "$TSTIMG"
+stest 0 sudo singularity bootstrap "$TSTIMG" "$STARTDIR/examples/debian.def"
 
 /bin/echo
 /bin/echo "Running container tsts..."
@@ -99,7 +98,12 @@ stest 0 sudo singularity copy "$TSTIMG" singularity /
 stest 0 singularity run "$TSTIMG" true
 stest 1 singularity run "$TSTIMG" false
 
+stest 0 sudo singularity export -f out.tar "$TSTIMG"
+stest 0 sudo tar xvf out.tar
 
+stest 0 sudo rm -f "$TSTIMG"
+stest 0 sudo singularity create -s 568 "$TSTIMG"
+stest 0 sh -c "cat out.tar | sudo singularity import $TSTIMG"
 
 /bin/echo
 /bin/echo "Cleaning up"
