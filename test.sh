ebc4f30095ed27b03e13c38cfc8d729141f5656d
Gregory M. Kurtzer <gmkurtzer@lbl.gov>
2016-07-13 10:37:16 -0700

    in progress...

--- test.sh
+++ test.sh
@@ -25,30 +25,32 @@ export SINGULARITY_CACHEDIR MESSAGELEVEL
 /bin/echo "Gaining/checking sudo access..."
 sudo true
 
-if [ -z "$CLEAN_SHELL" ]; then
-    /bin/echo "Building/Installing Singularity to temporary directory"
-    /bin/echo "Reinvoking in a clean shell"
-    sleep 1
-    exec env -i CLEAN_SHELL=1 PATH="/bin:/usr/bin:/sbin:/usr/sbin" bash "$0" "$*"
-fi
+#if [ -z "$CLEAN_SHELL" ]; then
+#    /bin/echo "Building/Installing Singularity to temporary directory"
+#    /bin/echo "Reinvoking in a clean shell"
+#    sleep 1
+#    exec env -i CLEAN_SHELL=1 PATH="/bin:/usr/bin:/sbin:/usr/sbin" bash "$0" "$*"
+#fi
 
 . ./libexec/functions
 
-make maintainer-clean >/dev/null 2>&1
-stest 0 sh ./autogen.sh --prefix="$TEMPDIR"
-stest 0 make
-stest 0 sudo make install
+set +e
 
-PATH="$TEMPDIR/bin:$PATH"
+#make maintainer-clean >/dev/null 2>&1
+#stest 0 sh ./autogen.sh --prefix="$TEMPDIR"
+#stest 0 make
+#stest 0 sudo make install
+#
+#PATH="$TEMPDIR/bin:$PATH"
 
 /bin/echo
 /bin/echo "SINGULARITY_CACHEDIR=$SINGULARITY_CACHEDIR"
 /bin/echo "PATH=$PATH"
 /bin/echo
 
-/bin/echo "$Creating temp working space at: $TEMPDIR"
+/bin/echo "Creating temp working space at: $TEMPDIR"
 stest 0 mkdir -p "$TEMPDIR"
-stest 0 pushd "$TEMPDIR"
+#stest 0 pushd "$TEMPDIR"
 
 /bin/echo
 /bin/echo "Running base tests..."
@@ -75,7 +77,7 @@ stest 1 singularity bogus help
 /bin/echo "Building test container..."
 /bin/echo
 
-stest 0 sudo singularity create -s 15 "$TSTIMG"
+stest 0 sudo singularity -d create -s 15 "$TSTIMG"
 stest 0 sudo singularity bootstrap "$TSTIMG" "$STARTDIR/examples/busybox.def"
 
 /bin/echo
