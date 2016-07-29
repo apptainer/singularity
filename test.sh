beb0267c2e930f8cc230a8e3c766278320e0e3be
Gregory M. Kurtzer <gmkurtzer@lbl.gov>
2016-07-29 10:59:26 -0700

    Fixed test script for root only calls to --writable

--- test.sh
+++ test.sh
@@ -131,9 +131,9 @@ stest 0 sudo chmod 0666 "$CONTAINER"
 stest 0 sudo singularity shell -w "$CONTAINER" -c true
 stest 0 sudo singularity exec -w "$CONTAINER" true
 stest 0 sudo singularity run -w "$CONTAINER" true
-stest 0 singularity shell -w "$CONTAINER" -c true
-stest 0 singularity exec -w "$CONTAINER" true
-stest 0 singularity run -w "$CONTAINER" true
+stest 1 singularity shell -w "$CONTAINER" -c true
+stest 1 singularity exec -w "$CONTAINER" true
+stest 1 singularity run -w "$CONTAINER" true
 stest 1 singularity exec "$CONTAINER" touch /writetest.fail
 stest 1 sudo singularity exec "$CONTAINER" touch /writetest.fail
 stest 0 sudo singularity exec -w "$CONTAINER" touch /writetest.pass
