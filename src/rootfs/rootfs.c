2f527969e20462188c62ca22abd651e7d17dd944
Gregory M. Kurtzer <gmkurtzer@lbl.gov>
2016-08-21 07:44:44 -0700

    Make overlayfs enabled by default

--- src/rootfs/rootfs.c
+++ src/rootfs/rootfs.c
@@ -136,8 +136,9 @@ int singularity_rootfs_mount(void) {
     }
 
 #ifdef SINGULARITY_OVERLAYFS
+    message(DEBUG, "OverlayFS enabled by host build\n");
     config_rewind();
-    if ( config_get_key_bool("enable overlay", 0) <= 0 ) {
+    if ( config_get_key_bool("enable overlay", 1) <= 0 ) {
         message(VERBOSE3, "Not enabling writable overlay via configuration\n");
     } else if ( getenv("SINGULARITY_WRITABLE") == NULL ) {
         snprintf(overlay_options, overlay_options_len, "lowerdir=%s,upperdir=%s,workdir=%s", rootfs_source, overlay_upper, overlay_work);
