4acfb6f419bde4629eb3864191f19396e70d1e78
Gregory M. Kurtzer <gmkurtzer@lbl.gov>
2016-08-22 06:48:03 -0700

    Document fork.h

--- src/fork.h
+++ src/fork.h
@@ -21,7 +21,22 @@
 #ifndef __SINGULARITY_FORK_H_
 #define __SINGULARITY_FORK_H_
 
+
+    // SINGULARITY_FORK()
+    // Wrap the fork() system call and create the necessary communication
+    // pipes and signal handlers so that signals are correctly passed around
+    // between children and parents.
     pid_t singularity_fork(void);
+
+
+    // SINGLARITY_FORK_RUN()
+    // Fork() and automatically have the parent wait on the child while
+    // allowing the child to continue on through the code path. The parent
+    // will automatically wait in the background until the child exits, and
+    // then the parent will also exit with the same exit code as the parent.
+    // Similar to singularity_fork() above, this will maintain the proper
+    // communication channels for signal handling.
     void singularity_fork_run(void);
 
-#endif // __SINGULARITY_FORK_H_
+
+#endif /* __SINGULARITY_FORK_H_ */
