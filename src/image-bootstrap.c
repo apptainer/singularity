4c60063c1cabe251ad8cf0247683b754f53f2ef6
Michael Bauer <bauerm@umich.edu>
2016-11-01 12:53:17 +0100

    Separate logic into v1 and v2 drivers

--- src/image-bootstrap.c
+++ src/image-bootstrap.c
@@ -23,25 +23,46 @@
 
 
 int main(int argc, char ** argv) {
+
+  char *bootdef_path;
   
   if ( argv[1] == NULL ) {
     fprintf(stderr, "USAGE: UPDATE USAGE HERE!! SINGULARITY_IMAGE=[image] %s [command...]\n", argv[0]);
     return(1);
   }
 
+  //Abort if we can't open the bootstrap definition file
+  if( singularity_bootdef_open(argv[1]) != 0 ) {
+    ABORT(255);
+  }
+
   //Check for $SINGULARITY_ROOTFS && $SINGULARITY_libexecdir definitions
   
   //image-mount has finished, we are now inside a fork of image-mount running image-bootstrap binary instead of bootstrap.sh
-  
-  //Parse args for bootstrap_version
 
+  //Execute old driver-v1.sh if bootdef_ver = 1, else execute new bootstrap code
+  if( ( bootstrap_ver = singularity_bootdef_get_version() ) == 1 ) {
+    singularity_message(DEBUG, "Running bootstrap driver v1\n");
+
+    singularity_bootdef_close();
+    
+    //Maybe directly use driver-v2.sh since it is outdated and we don't need to rewrite it for future use?
+  }
+  else {
+    singularity_message(DEBUG, "Running bootstrap driver v2\n");
+
+    singularity_prebootstrap(); //lib/bootstrap/prebootstrap/prebootstrap.c
+
+    singularity_bootstrap_init(); //lib/bootstrap/bootstrap.c
+
+    singularity_postbootstrap(); //lib/bootstrap/postbootstrap/postbootstrap.c
+
+    singularity_bootdef_close();
+    
+  }
+  
+  
   //mkdir -d /tmp/singularity-bootstrap.XXXXXXX
 
-  singularity_prebootstrap(); //lib/bootstrap/prebootstrap/prebootstrap.c
-  
-  singularity_bootstrap_init(); //lib/bootstrap/bootstrap.c
-
-  singularity_postbootstrap(); //lib/bootstrap/postbootstrap/postbootstrap.c
-
-  return(0);
+    return(0);
 }
