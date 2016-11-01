4cb836237486d69f4db9dd7cb4ca5b554b0a083e
Michael Bauer <bauerm@umich.edu>
2016-11-01 11:37:25 +0100

    Started bootstrap_parser function implementations

--- src/lib/bootstrap_parser.c
+++ src/lib/bootstrap_parser.c
@@ -18,6 +18,8 @@
 
 FILE *bootstrap_fp = NULL;
 
+//Should these functions be singularity_bootstrap_* or singularity_bootstrap_def_*?
+
 int singularity_bootstrap_open(char *bootstrap_path) {
   singularity_message(VERBOSE, "Opening bootstrap definition file: %s\n", bootstrap_path);
   if ( is_file(bootstrap_path) == 0 ) {
@@ -29,18 +31,67 @@ int singularity_bootstrap_open(char *bootstrap_path) {
   return(-1);
 }
 
-singularity_bootstrap_key_get()
+void singularity_bootstrap_rewind() {
+  singularity_message(DEBUG, "Rewinding bootstrap definition file\n");
+  if ( _fp != NULL ) {
+    rewind(bootstrap_fp);
+  }
+}
 
+void singularity_bootstrap_close() {
+  singularity_message(VERBOSE, "Closing bootstrap definition file\n");
+  if ( bootstrap_fp != NULL ) {
+    fclose(bootstrap_fp);
+    bootstrap_fp = NULL;
+  }
+}
+
+//Equal to singularity_key_get in functions
+char *singularity_bootstrap_get_value(char *key) {
+  char *bootdef_key;
+  char *bootdef_value;
+  char *line;
+
+  if ( bootstrap_fp == NULL ) {
+    singularity_message(ERROR, "Called singularity_bootstrap_get_value() before opening a bootstrap definition file!\n");
+    ABORT(255);
+  }
+
+  line = (char *)mallaoc(MAX_LINE_LEN);
+
+  while ( fgets(line, MAX_LINE_LEN, bootstrap_fp) ) {
+    if ( ( bootdef_key = strtok(line, ":") ) != NULL ) {
+      chomp(bootdef_key);
+      if ( strcmp(bootdef_key, key) == 0 ) {
+	if ( ( bootdef_value = strdup(strtok(NULL, ":")) ) != NULL ) {
+	  chomp(bootdef_value);
+	  if ( bootdef_value[0] == ' ' ) {
+	    bootdef_value++;
+	  }
+	  singularity_message(VERBOSE2, "Got bootstrap definition key %s(: '%s')\n", key, bootdef_value);
+	  return(bootdef_value);
+	}
+      }
+    }
+  }
+  free(line);
+
+  singularity_message(DEBUG, "No bootstrap definition file entry found for '%s'\n", key);
+  return(NULL);
+}
 
 singularity_bootstrap_keys_get()
+
 
 
 singularity_bootstrap_section_exists()
 
 
+
 singularity_bootstrap_section_args()
 
 
+//Can either directly call on get-section binary, or reimplement it here. Not sure what the best idea is?
 singularity_bootstrap_section_get()
 
 
