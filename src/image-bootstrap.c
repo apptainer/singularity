e77e7927f5f9ebd2bb9df5c1c452f8572da33104
Michael Bauer <bauerm@umich.edu>
2016-11-02 14:28:59 +0100

    image-bootstrap will now directly run driver-v1.sh when a v1 file is found

--- src/image-bootstrap.c
+++ src/image-bootstrap.c
@@ -21,10 +21,10 @@
 #include "util/file.h"
 #include "util/util.h"
 
-
 int main(int argc, char ** argv) {
 
   char *bootdef_path;
+  char *driver_v1_path = LIBEXECDIR "/singularity/bootstrap/driver-v1.sh";
   
   if ( argv[1] == NULL ) {
     fprintf(stderr, "USAGE: UPDATE USAGE HERE!! SINGULARITY_IMAGE=[image] %s [command...]\n", argv[0]);
@@ -40,13 +40,15 @@ int main(int argc, char ** argv) {
   
   //image-mount has finished, we are now inside a fork of image-mount running image-bootstrap binary instead of bootstrap.sh
 
-  //mktemp -d /tmp/singularity-bootstrap.XXXXXXX 
+  //mktemp -d /tmp/singularity-bootstrap.XXXXXXX ?? Unsure where this was used but was in the bootstrap shell scripts
   
   //Execute old driver-v1.sh if bootdef_ver = 1, else execute new bootstrap code
   if( ( bootstrap_ver = singularity_bootdef_get_version() ) == 1 ) {
-    singularity_message(DEBUG, "Running bootstrap driver v1\n");
-
+    singularity_message(INFO, "Running bootstrap driver v1,new non-privileged functionality requires use of v2 driver!\n");
     singularity_bootdef_close();
+
+    argv[0] = driver_v1_path; //pointer to some string containing path to driver-v1.sh script
+    singularity_fork_exec(argv); //Use singularity_fork_exec to directly call the v1 driver
     
     //Maybe directly use driver-v2.sh since it is outdated and we don't need to rewrite it for future use?
   }
