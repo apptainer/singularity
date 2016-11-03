dea36f5b53e83a992f1bdef5615d03f6981949c8
Michael Bauer <bauerm@umich.edu>
2016-11-03 11:14:00 +0100

    Finalized format of header files and functions in bootstrap/

--- src/lib/bootstrap/bootstrap.c
+++ src/lib/bootstrap/bootstrap.c
@@ -18,39 +18,40 @@
 #include "lib/bootstrap_parser.h"
 #include "lib/singularity.h"
 
-
+static char *module_name;
 void singularity_bootstrap_init() {
 
   //sanity check here?? Or maybe just change name of _run_module to _init
-  singularity_bootstrap_run_module();
+  if ( singularity_bootstrap_run_module() == -1 ) {
+    singularity_message(ERROR, "Problem running bootstrap module\n");
+    ABORT(255);
 }
 
 
-void singularity_bootstrap_run_module() {
-  char *module_name;
-
+int singularity_bootstrap_run_module() {
   singularity_bootdef_rewind();
+  
   if ( ( module_name = singularity_bootdef_get_value("Bootstrap") ) == NULL ) {
     singularity_message(ERROR, "Bootstrap definition file does not contain a Bootstrap: line");
     ABORT(255);
     
   } else {
-    singularity_message(INFO, "Bootstrap module %s found, running module builder\n", module_name);
+    singularity_message(INFO, "Running bootstrap module %s\n", module_name);
 
     if ( strcmp(module_name, "docker") ) { //Docker
-      singularity_bootstrap_docker_init();
+      return( singularity_bootstrap_docker_init() );
       
     } else if ( strcmp(module_name, "yum") ) { //Yum
-      singularity_bootstrap_yum_init();
+      return( singularity_bootstrap_yum_init() );
       
     } else if ( strcmp(module_name, "debootstrap") ) { //Debootstrap
-      singularity_bootstrap_debootstrap_init();
+      return( singularity_bootstrap_debootstrap_init() );
       
     } else if ( strcmp(module_name, "arch") ) { //Arch
-      singularity_bootstrap_arch_init();
+      return( singularity_bootstrap_arch_init() );
       
     } else if ( strcmp(module_name, "busybox") ) { //Busybox
-      singularity_bootstrap_busybox_init();
+      return( singularity_bootstrap_busybox_init() );
       
     } else {
       singularity_message(ERROR, "Could not parse bootstrap module of type: %s", module_name);
--- src/lib/bootstrap/bootstrap.h
+++ src/lib/bootstrap/bootstrap.h
@@ -2,3 +2,12 @@
 #define __SINGULARITY_BOOTSTRAP_H_
 
     
+    extern void singularity_bootstrap_init();
+
+    extern int singularity_bootstrap_docker_init();
+    extern int singularity_bootstrap_yum_init();
+    extern int singularity_bootstrap_debootstrap_init();
+    extern int singularity_bootstrap_arch_init();
+    extern int singularity_bootstrap_busybox_init();
+
+#endif
