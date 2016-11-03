b911f02feb8eb175c543aabffba20ae85a5e4a86
Michael Bauer <bauerm@umich.edu>
2016-11-03 16:05:09 +0100

    Made more descriptive filenames

--- src/lib/image/create/create.c
+++ src/lib/image/create/create.c
@@ -34,7 +34,7 @@
 #include "util/file.h"
 
 
-int main(int argc, char ** argv) {
+int singularity_image_create_test(int argc, char ** argv) {
     long int size;
 
     if ( argv[1] == NULL ) {
---
+++ src/lib/image/image.h
@@ -0,0 +1,9 @@
+#ifndef __SINGULARITY_IMAGE_H
+#define __SINGULARITY_IMAGE_H
+
+extern int singularity_image_create_test(int argc, char ** argv);
+extern int singularity_image_expand_test(int argc, char ** argv);
+extern int singularity_image_mount(int argc, char ** argv);
+extern int singularity_image_bind(int argc, char ** argv);
+
+#endif
