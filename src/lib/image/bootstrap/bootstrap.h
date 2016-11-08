1529628da7323e1d12dc69a69da4aa509916a40f
Michael Bauer <bauerm@umich.edu>
2016-11-08 13:42:11 +0100

    bootstrap/bootstrap.h now only exposes module_init()

--- src/lib/image/bootstrap/bootstrap.h
+++ src/lib/image/bootstrap/bootstrap.h
@@ -1,15 +1,10 @@
 #ifndef __SINGULARITY_BOOTSTRAP_H_
 #define __SINGULARITY_BOOTSTRAP_H_
-
     
-    extern void singularity_bootstrap_init();
-    extern void singularity_prebootstrap_init();
-    extern void singularity_postbootstrap_init();
+    int singularity_bootstrap_init();
 
-    extern int singularity_bootstrap_docker_init();
-    extern int singularity_bootstrap_yum_init();
-    extern int singularity_bootstrap_debootstrap_init();
-    extern int singularity_bootstrap_arch_init();
-    extern int singularity_bootstrap_busybox_init();
+    extern int singularity_bootstrap_pre_init();
+    extern int singularity_bootstrap_post_init();
+    extern int singularity_bootstrap_module_init();
 
 #endif
