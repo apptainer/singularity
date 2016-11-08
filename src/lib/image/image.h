51c00dc483e6f26ce65885d12ec992937c74a013
Michael Bauer <bauerm@umich.edu>
2016-11-08 10:49:44 +0100

    Moved image-util.c to lib/image

--- src/lib/image/image.h
+++ src/lib/image/image.h
@@ -6,4 +6,9 @@ extern int singularity_image_extern_expand(int argc, char ** argv);
 extern int singularity_image_mount(int argc, char ** argv);
 extern int singularity_image_bind(int argc, char ** argv);
 
+extern int singularity_image_check(FILE *image_fp);
+extern int singularity_image_offset(FILE *image_fp);
+extern int singularity_image_create(char *image, int size);
+extern int singularity_image_expand(char *image, int size);
+
 #endif
