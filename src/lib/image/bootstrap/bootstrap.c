17d77eef0cbf6afe5d8cc776dada5bf4de62d38a
Michael Bauer <bauerm@umich.edu>
2016-11-08 13:32:03 +0100

    Moved bootstrap_module_init() -> bootstrap/module/module.c

--- src/lib/image/bootstrap/bootstrap.c
+++ src/lib/image/bootstrap/bootstrap.c
@@ -68,35 +68,3 @@ int singularity_bootstrap_init(int argc, char ** argv) {
 
   return(0);
 }
-
-int singularity_bootstrap_module_init() {
-  singularity_bootdef_rewind();
-  
-  if ( ( module_name = singularity_bootdef_get_value("Bootstrap") ) == NULL ) {
-    singularity_message(ERROR, "Bootstrap definition file does not contain a Bootstrap: line");
-    ABORT(255);
-    
-  } else {
-    singularity_message(INFO, "Running bootstrap module %s\n", module_name);
-
-    if ( strcmp(module_name, "docker") ) { //Docker
-      return( singularity_bootstrap_docker_init() );
-      
-    } else if ( strcmp(module_name, "yum") ) { //Yum
-      return( singularity_bootstrap_yum_init() );
-      
-    } else if ( strcmp(module_name, "debootstrap") ) { //Debootstrap
-      return( singularity_bootstrap_debootstrap_init() );
-      
-    } else if ( strcmp(module_name, "arch") ) { //Arch
-      return( singularity_bootstrap_arch_init() );
-      
-    } else if ( strcmp(module_name, "busybox") ) { //Busybox
-      return( singularity_bootstrap_busybox_init() );
-      
-    } else {
-      singularity_message(ERROR, "Could not parse bootstrap module of type: %s", module_name);
-      ABORT(255);
-    }
-
-}
