55f2dfa4cfad2fb6b3737c61dc06b48f185671d0
vsoch <vsochat@stanford.edu>
2016-11-13 16:27:39 -0800

    finishing up dockerfile conversion - last detail working on is parsing multi-line commands for ADD and COPY, etc.

--- libexec/python/docker/converter.py
+++ libexec/python/docker/converter.py
@@ -23,6 +23,8 @@ perform publicly and display publicly, and to permit other to do so.
 
 '''
 
+import json
+import os
 import re
 import sys
 sys.path.append('..') # parent directory
@@ -61,7 +63,6 @@ def parse_env(env):
                     value = "%s %s" %(value,contender)
         exports.append(join_env(name,value))
         return "\n".join(exports)
-
     # otherwise, the rule is one per line
     else: 
         name,value = re.split(' ',env,1)
@@ -77,7 +78,14 @@ def join_env(name,value):
 
 def parse_cmd(cmd):
     '''parse_cmd will parse a Dockerfile CMD command to a singularity appropriate one
-    eg: CMD /code/run_uwsgi.sh --> exec /code/run_uwsgi.sh
+    eg: CMD /code/run_uwsgi.sh --> /code/run_uwsgi.sh.
+    '''
+    return "%s" %(cmd)
+
+
+def parse_entry(cmd):
+    '''parse_entry will parse a Dockerfile ENTRYPOINT command to a singularity appropriate one
+    eg: ENTRYPOINT /code/run_uwsgi.sh --> exec /code/run_uwsgi.sh.
     '''
     return "exec %s" %(cmd)
 
@@ -90,33 +98,59 @@ def parse_copy(copy_str):
     return "cp %s" %(copy_str)
 
 
-def parse_add(add)
+def parse_http(url,destination):
+    '''parse_http will get the filename of an http address, and return a statement
+    to download it to some location
+    '''
+    file_name = os.path.basename(url)
+    download_path = "%s/%s" %(to_thing,file_name)
+    return "curl %s -o %s" %(url,download_path)
+
+
+def parse_targz(targz,destination):
+    '''parse_targz will return a commnd to extract a targz file to a destination.
+    '''
+    return "tar -xzvf %s %s" %(targz,destination)
+
+
+def parse_zip(zipfile,destination):
+    '''parse_zipfile will return a commnd to unzip a file to a destination.
+    '''
+    return "unzip %s %s" %(targz,destination)
+
+
+def parse_add(add):
     '''parse_add will copy multiple files from one location to another. This likely will need
     tweaking, as the files might need to be mounted from some location before adding to
     the image. The add command is done for an entire directory.
     :param add: the command to parse
     '''
     from_thing,to_thing = add.split(" ")
-
-    # If it's a url or http address, then we need to use wget/curl to get it
-    if re.search("^http",from_thing):
-        return "curl %s -o %s" %(from_thing,to_thing)
-
     # People like to use dots for PWD.
     if from_thing == ".":
         from_thing = os.getcwd()
     if to_thing == ".":
         to_thing = os.getcwd()
-
+    # If it's a url or http address, then we need to use wget/curl to get it
+    if re.search("^http",from_thing):
+        return parse_http(url=from_thing,
+                          destination=to_thing)
+    # If it's a tar.gz, then we are supposed to uncompress
+    if re.search(".tar.gz$",from_thing):
+        return parse_targz(targz=from_thing,
+                           destination=to_thing)
+    # If it's .zip, then we are supposed to unzip it
+    if re.search(".zip$",from_thing):
+        return parse_zip(zipfile=from_thing,
+                         destination=to_thing)
     # Is from thing a directory or something else?
     if os.path.isdir(from_thing):
         return "cp -R %s %s" %(from_thing,to_thing)
+    else:
+        return "cp %s %s" %(from_thing,to_thing)
 
-    print("Cannot determine add command for %s, skipping" %(add))     
-    return ""
 
-
-def parse_workdir(workdir)
+def parse_workdir(workdir):
     '''parse_workdir will simply cd to the working directory
     '''
     return "cd %s" %(workdir)
@@ -125,35 +159,69 @@ def parse_workdir(workdir)
 def get_mapping():
     '''get_mapping returns a dictionary mapping from a Dockerfile command to a Singularity
     build spec section. Note - this currently ignores lines that we don't know what to do with
-    in the context of Singularity (eg, EXPOSE, LABEL, USER, VOLUME, STOPSIGNAL)
+    in the context of Singularity (eg, EXPOSE, LABEL, USER, VOLUME, STOPSIGNAL, escape,
+    MAINTAINER)
+
+    :: note
+    each KEY of the mapping should be a command start in the Dockerfile (eg, RUN)
+    for each corresponding value, there should be a dictionary with the following:
+    
+        - section: the Singularity build file section to write the new command to
+        - fun: any function to pass the output through before writing to the section (optional)
+        - json: Boolean, if the section can optionally have json (eg a list)
+
+    I'm not sure the subtle differences between add and copy, other than copy doesn't support
+    external files. It should suffice for our purposes (for now) to use the same function 
+    (parse_add) until evidence for a major difference is determined.
     '''
     #  Docker : Singularity
-    add_command = {"section": "%post", "fun": parse_copy }  
-    copy_command = {"section": "%post", "fun": parse_copy }  
-    cmd_command = {"section": "%runscript", "fun": parse_cmd }  
-    env_command = {"section": "%post", "fun": parse_env } 
-    from_command = {"section": "From"}
-    run_command = {"section": "%post"}       
-    workdir_command = {"section": "%post", "fun": parse_workdir }  
+    add_command = {"section": "%post","fun": parse_add, "json": True }
+    copy_command = {"section": "%post", "fun": parse_add, "json": True }  
+    cmd_command = {"section": "%runscript", "fun": parse_cmd, "json": True }  
+    env_command = {"section": "%post", "fun": parse_env, "json": False }
+    from_command = {"section": "From", "json": False }
+    run_command = {"section": "%post", "json": True}       
+    workdir_command = {"section": "%post","fun": parse_workdir, "json": False }  
+    entry_command = {"section": "%post", "fun": parse_entry, "json": True }
 
     return {"ADD": add_command,
+            "COPY":copy_command,
+            "CMD":cmd_command,
+            "ENTRYPOINT":entry_command,
             "ENV": env_command,
             "FROM": from_command,
+            "RUN":run_command,
             "WORKDIR":workdir_command}
            
-    # STOPPING FOR TODAY - remainder of parsing functions need to be written, tested,
-    # and the mapping finished, and then the mapping used in organize_sections!
+    
 
-
-def dockerfile_to_singularity(dockerfile_path, output_dir):
+def dockerfile_to_singularity(dockerfile_path, output_dir=None):
     '''dockerfile_to_singularity will return a Singularity build file based on
-    a provided Dockerfile
+    a provided Dockerfile. If output directory is not specified, the string
+    will be returned. Otherwise, a file called Singularity will be written to 
+    output_dir
     :param dockerfile_path: the path to the Dockerfile
     :param output_dir: the output directory to write the Singularity file to
     '''
     if os.path.basename(dockerfile_path) == "Dockerfile":
         spec = read_file(dockerfile_path)
+        # Use a common mapping
+        mapping = get_mapping()
+   
+        # Put into dict of keys (section titles) and list of commands (values)
+        sections = organize_sections(lines=spec,
+                                     mapping=mapping)
 
+        # We have to, by default, add the Docker bootstrap
+        sections["bootstrap"] = ["docker"]
+
+        # Put into one string based on "order" variable in mapping
+        build_file = print_sections(sections=sections,
+                                    mapping=mapping)
+        if output_dir != None:
+            write_file("%s/Singularity" %(output_dir),build_file)
+            print("Singularity spec written to %s" %(output_dir))
+        return build_file
 
     # If we make it here, something didn't work
     return sys.exit(1)
@@ -165,13 +233,83 @@ def organize_sections(lines,mapping=None):
     :param lines: the raw lines from the Dockerfile
     :mapping: a dictionary mapping Docker commands to Singularity sections
     '''
+    if mapping == None:
+        mapping = get_mapping()
+    sections = dict()
+    startre = "|".join(["^%s" %x for x in mapping.keys()])
+    command = None
+    name = None
+    while len(lines) > 0:
+        line = lines.pop(0)
+        # Do we have a new line/section?
+        if re.search(startre,line):
+            # Parse the last section, and start over
+            if command != None and name != None:
+                sections = parse_section(name=name,
+                                         command=command,
+                                         mapping=mapping,
+                                         sections=sections)
+                name,command = line.split(" ",1)
+        # We have a continuation of the last command or an empty line
+        else:
+            command = "%s %s" %(command,line)
 
-    #TODO: read in lines until we reach the next section.
-    # If section isn't in list, or is, parse to one or the other (FOR: RUN,CMD)
-    # send entire section to be parsed and put (as a unit) into an object
+    return sections
+
+def parse_section(sections,name,command,mapping=None):
+    '''parse_section will take a command that has lookup key "name" as a key in "mapping"
+    and add a line to the list of each in sections that will be rendered into a Singularity
+    build file.
+    :param sections: the current sections, a dictionary of keys (singularity section titles)
+    and a list of lines.
+    :param name: the name of the section to add
+    :param command: the command to parse:
+    :param mapping: the mapping object to use
+    '''
+    if mapping == None:
+        mapping = get_mapping()
+    if name in mapping:
+        build_section = mapping[name]['section']
+        # Can the command potentially be json (a list?)
+        if mapping[name]['json']:
+            try:
+                command = " ".join(json.loads(command))
+            except:
+                pass 
+        # Do we need to pass it through a function first?
+        if 'fun' in mapping[name]:
+            command = mapping[name]['fun'](command)
+        # Add to our dictionary of sections!
+        if build_section not in sections:
+            sections[build_section] = [command]
+        else:
+            sections[build_section].append(command)
+    return sections
 
 
-def sniff_command(line):
-    '''sniff_command will return the command type and command for one or more lines
-    :param line: the line to read
+def print_sections(sections,mapping=None):
+    '''print_sections will take a sections object (dict with section names and
+    list of commands) and parse into a common string, to output to file or return
+    to user.
+    :param sections: output from organize_sections
+    :mapping: a dictionary mapping Docker commands to Singularity sections
+    '''
+    if mapping == None:
+        mapping = get_mapping()
+    finished_spec = ""
+    ordering = ['bootstrap',"From","%runscript","%post"]
 
+    for section in ordering:
+
+        # Was the section found in the file?
+        if section in sections:
+            if not re.search("^%",section):
+                # A single command, intended to go after a colon (yaml)
+                content = "".join(sections[section])
+                finished_spec = "%s\n%s:%s" %(finished_spec,section,content)
+            else:
+                # A list of things to join, after the section header
+                content = "".join(sections[section])
+                finished_spec = "%s\n%s\n%s" %(finished_spec,section,content)
+
+    return finished_spec
