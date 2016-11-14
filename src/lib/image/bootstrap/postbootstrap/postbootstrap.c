7321f7493363204025a2fe98f4835d2a65f6cdc8
Michael Bauer <bauerm@umich.edu>
2016-11-14 10:58:45 +0100

    Added logic to copy etc/defaults/* into rootfs/*

--- src/lib/image/bootstrap/postbootstrap/postbootstrap.c
+++ src/lib/image/bootstrap/postbootstrap/postbootstrap.c
@@ -27,8 +27,16 @@ static char *rootfs_path = NULL;
 void singularity_postbootstrap_init() {
   rootfs_path = singularity_rootfs_dir();
   singularity_rootfs_check();
-  
 
+  if ( postbootstrap_rootfs_install() != 0 ) {
+    singularity_message(ERROR, "Failed to create container rootfs. Aborting...\n");
+    ABORT(255);
+  }
+  
+  if ( postbootstrap_copy_defaults() != 0 ) {
+    singularity_message(ERROR, "Failed to copy necessary default files to container rootfs. Aborting...\n");
+    ABORT(255);
+  }
   
   postbootstrap_script_run("setup");
 
@@ -36,27 +44,44 @@ void singularity_postbootstrap_init() {
   postbootstrap_script_run("post");
 }
 
-void postbootstrap_rootfs_install() {
-  s_mkpath(rootfs_path, 0755);
-  s_mkpath(joinpath(rootfs_path, "/bin"), 0755);
-  s_mkpath(joinpath(rootfs_path, "/dev"), 0755);
-  s_mkpath(joinpath(rootfs_path, "/home"), 0755);
-  s_mkpath(joinpath(rootfs_path, "/etc"), 0755);
-  s_mkpath(joinpath(rootfs_path, "/root"), 0750);
-  s_mkpath(joinpath(rootfs_path, "/proc"), 0755);
-  s_mkpath(joinpath(rootfs_path, "/sys"), 0755);
-  s_mkpath(joinpath(rootfs_path, "/tmp"), 1777);
-  s_mkpath(joinpath(rootfs_path, "/var/tmp"), 1777);
+int postbootstrap_rootfs_install() {
+  int retval = 0;
+  retval += s_mkpath(rootfs_path, 0755);
+  retval += s_mkpath(joinpath(rootfs_path, "/bin"), 0755);
+  retval += s_mkpath(joinpath(rootfs_path, "/dev"), 0755);
+  retval += s_mkpath(joinpath(rootfs_path, "/home"), 0755);
+  retval += s_mkpath(joinpath(rootfs_path, "/etc"), 0755);
+  retval += s_mkpath(joinpath(rootfs_path, "/root"), 0750);
+  retval += s_mkpath(joinpath(rootfs_path, "/proc"), 0755);
+  retval += s_mkpath(joinpath(rootfs_path, "/sys"), 0755);
+  retval += s_mkpath(joinpath(rootfs_path, "/tmp"), 1777);
+  retval += s_mkpath(joinpath(rootfs_path, "/var/tmp"), 1777);
+
+  return(retval);
   
 
   //Do this in C (Or maybe move this to yum.c since it was changed in upstream/master??)
   //cp -a /dev/null         "$SINGULARITY_ROOTFS/dev/null"      2>/dev/null || > "$SINGULARITY_ROOTFS/dev/null";
   //cp -a /dev/zero         "$SINGULARITY_ROOTFS/dev/zero"      2>/dev/null || > "$SINGULARITY_ROOTFS/dev/zero";
   //cp -a /dev/random       "$SINGULARITY_ROOTFS/dev/random"    2>/dev/null || > "$SINGULARITY_ROOTFS/dev/random";
-  //cp -a /dev/urandom      "$SINGULARITY_ROOTFS/dev/urandom"   2>/dev/null || > "$SINGULARITY_ROOTFS/dev/urandom";
-
-  
+  //cp -a /dev/urandom      "$SINGULARITY_ROOTFS/dev/urandom"   2>/dev/null || > "$SINGULARITY_ROOTFS/dev/urandom";  
 }
+
+int postbootstrap_copy_defaults() {
+  int retval = 0;
+
+  if ( is_file(joinpath(rootfs_path, "/environment")) ) {
+    singularity_message(INFO, "Skipping environment file, file already exists.\n");
+  } else {
+    retval += copy_file( LIBEXECDIR "/singularity/defaults/environment", joinpath(rootfs_path, "/environment") );
+  }
+  retval += copy_file( LIBEXECDIR "/singularity/defaults/exec", joinpath(rootfs_path, "/.exec") );
+  retval += copy_file( LIBEXECDIR "/singularity/defaults/shell", joinpath(rootfs_path, "/.shell") );
+  retval += copy_file( LIBEXECDIR "/singularity/defaults/run", joinpath(rootfs_path, "/.run") );
+
+  return(retval);
+}
+  
 
 void postbootstrap_script_run(char *section_name) {
   char ** script;
