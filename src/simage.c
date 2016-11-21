27ae4b11a37f7aeff03b29a143f30489aad1ba6e
Gregory M. Kurtzer <gmkurtzer@lbl.gov>
2016-11-21 09:43:07 -0800

    Cleanups to simage.c

--- src/simage.c
+++ src/simage.c
@@ -43,57 +43,56 @@
 #include "util/util.h"
 
 int main(int argc, char ** argv) {
-  if ( argv[1] == NULL ) {
-    fprintf(stderr, "USAGE: simage command args\n");
-    return(1);
+    if ( argv[1] == NULL ) {
+        fprintf(stderr, "USAGE: simage command args\n");
+        return(1);
     
-  } else {
+    }
 
     //Loop through argv, each time chopping off argv[0], until argv[1] is a relevant shell script or is empty
     singularity_priv_init(); //Make sure user is running as root before we add SUID code
     while ( 1 ) {
-      singularity_message(DEBUG, "Running %s %s workflow\n", argv[0], argv[1]);
+        singularity_message(DEBUG, "Running %s %s workflow\n", argv[0], argv[1]);
 
-      singularity_priv_escalate();
-      if ( argv[1] == NULL ) {
-	singularity_message(DEBUG, "Finished running simage command and returning\n");
-	return(0);
+        singularity_priv_escalate();
+        if ( argv[1] == NULL ) {
+            singularity_message(DEBUG, "Finished running simage command and returning\n");
+            return(0);
 
-      } else if ( strcmp(argv[1], "mount") == 0 ) {
-	if ( singularity_image_mount(argc - 1, &argv[1]) != 0 ) {
-	  singularity_priv_drop_perm();
-	  return(1);
-	}
-	
-      } else if ( strcmp(argv[1], "bind") == 0 ) {
-	if ( singularity_image_bind(argc - 1, &argv[1]) != 0 ) {
-	  singularity_priv_drop_perm();
-	  return(1);
-	}
-	
-      } else if ( strcmp(argv[1], "create") == 0 ) {
-	if ( singularity_image_extern_create(argc - 1, &argv[1]) != 0 ) {
-	  singularity_priv_drop_perm();
-	  return(1);
-	} else {
-	  singularity_priv_drop_perm();
-	  return(0);
-	}
-	
-      } else if ( strcmp(argv[1], "expand") == 0 ) {
-	if ( singularity_image_extern_expand(argc - 1, &argv[1]) != 0 ) {
-	  singularity_priv_drop_perm();
-	  return(1);
-	}
-	
-      } else {
-	singularity_priv_drop_perm(); //Drop all privs permanently and return to calling user
-	return(singularity_fork_exec(&argv[1])); //Can NOT run this with root privs
-      }
+        } else if ( strcmp(argv[1], "mount") == 0 ) {
+            if ( singularity_image_mount(argc - 1, &argv[1]) != 0 ) {
+                singularity_priv_drop_perm();
+                return(1);
+            }
+        
+        } else if ( strcmp(argv[1], "bind") == 0 ) {
+            if ( singularity_image_bind(argc - 1, &argv[1]) != 0 ) {
+                singularity_priv_drop_perm();
+                return(1);
+            }
+        
+        } else if ( strcmp(argv[1], "create") == 0 ) {
+            if ( singularity_image_extern_create(argc - 1, &argv[1]) != 0 ) {
+                singularity_priv_drop_perm();
+                return(1);
+            } else {
+                singularity_priv_drop_perm();
+                return(0);
+            }
+        
+        } else if ( strcmp(argv[1], "expand") == 0 ) {
+            if ( singularity_image_extern_expand(argc - 1, &argv[1]) != 0 ) {
+                singularity_priv_drop_perm();
+                return(1);
+            }
+        
+        } else {
+            singularity_priv_drop_perm(); //Drop all privs permanently and return to calling user
+            return(singularity_fork_exec(&argv[1])); //Can NOT run this with root privs
+        }
       
-      argv++;
-      argc--;
-      singularity_priv_drop();
+        argv++;
+        argc--;
+        singularity_priv_drop();
     }
-  }
 }
