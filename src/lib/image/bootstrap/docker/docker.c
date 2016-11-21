66bdf9b55439726a03af7a525d7e23e16aa8a1bf
Michael Bauer <bauerm@umich.edu>
2016-11-21 13:26:15 +0100

    Unified license/copyright header of all relevant files

--- src/lib/image/bootstrap/docker/docker.c
+++ src/lib/image/bootstrap/docker/docker.c
@@ -1,3 +1,23 @@
+/* 
+ * Copyright (c) 2016, Michael W. Bauer. All rights reserved.
+ * 
+ * “Singularity” Copyright (c) 2016, The Regents of the University of California,
+ * through Lawrence Berkeley National Laboratory (subject to receipt of any
+ * required approvals from the U.S. Dept. of Energy).  All rights reserved.
+ * 
+ * This software is licensed under a customized 3-clause BSD license.  Please
+ * consult LICENSE file distributed with the sources of this project regarding
+ * your rights to use or distribute this software.
+ * 
+ * NOTICE.  This Software was developed under funding from the U.S. Department of
+ * Energy and the U.S. Government consequently retains certain rights. As such,
+ * the U.S. Government has been granted for itself and others acting on its
+ * behalf a paid-up, nonexclusive, irrevocable, worldwide license in the Software
+ * to reproduce, distribute copies to the public, prepare derivative works, and
+ * perform publicly and display publicly, and to permit other to do so. 
+ * 
+ */
+
 #define _GNU_SOURCE
 #include <errno.h>
 #include <fcntl.h>
@@ -11,12 +31,10 @@
 #include <stdlib.h>
 #include <sched.h>
 
-
 #include "util/file.h"
 #include "util/util.h"
 #include "lib/message.h"
 #include "lib/singularity.h"
-
 
 //Return 0 if successful, return 1 otherwise.
 int singularity_bootstrap_docker() {
