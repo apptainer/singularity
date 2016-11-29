ce9e06db85e093a32b82671b8773f8f84be014f6
vsoch <vsochat@stanford.edu>
2016-11-29 10:51:38 -0800

    updating client tests to use temp directories, and print better output to tell user what is going on

--- libexec/python/tests/test_client.py
+++ libexec/python/tests/test_client.py
@@ -28,6 +28,8 @@ sys.path.append('..') # directory with client
 
 from unittest import TestCase
 from cli import get_parser, run
+import shutil
+import tempfile
 
 VERSION = sys.version_info[0]
 
@@ -63,7 +65,7 @@ class TestUtils(TestCase):
     def test_add_http(self):
         '''test_add_http ensures that http is added to a url
         '''
-        print("Testing utils.add_http...")
+        print("Case 1: adding https to url with nothing specified...")
 
         from utils import add_http
         url = 'registry.docker.io'
@@ -73,10 +75,12 @@ class TestUtils(TestCase):
         self.assertEqual("https://%s"%url,http)
 
         # http
+        print("Case 2: adding http to url with nothing specified...")
         http = add_http(url,use_https=False)
         self.assertEqual("http://%s"%url,http)
 
         # This should not change. Note - is url is http, stays http
+        print("Case 3: url already has https, should not change...")
         url = 'https://registry.docker.io'
         http = add_http(url)
         self.assertEqual(url,http)
@@ -93,15 +97,18 @@ class TestUtils(TestCase):
         from utils import basic_auth_header
         
         # If we don't give headers, and no default, should get {} 
+        print("Case 1: Don't give headers, return empty dictionary")
         empty_dict = parse_headers(default_header=False)
         self.assertEqual(empty_dict,dict())
 
         # If we ask for default, should get something back
+        print("Case 2: ask for default headers...")
         headers = parse_headers(default_header=True)
         for field in ["Accept","Content-Type"]:
             self.assertTrue(field in headers)
 
         # Can we add a header?
+        print("Case 3: add a custom header")
         new_header = {"cookies":"nom"}
         headers = parse_headers(default_header=True,
                                 headers=new_header)
@@ -110,6 +117,7 @@ class TestUtils(TestCase):
 
 
         # Basic auth header
+        print("Case 4: basic_auth_header - ask for custom authentication header")
         auth = basic_auth_header(username='vanessa',
                                  password='pancakes')
         self.assertEqual(auth['Authorization'],
@@ -125,10 +133,12 @@ class TestUtils(TestCase):
         from utils import run_command
         
         # An error should return None
+        print("Case 1: Command errors returns None ")
         none  = run_command(['exec','whaaczasd'])
         self.assertEqual(none,None)
 
         # A success should return console output
+        print("Case 2: Command success returns output")
         hello  = run_command(['echo','hello'])
         if not isinstance(hello,str): # python 3 support
             hello = hello.decode('utf-8')
@@ -144,29 +154,34 @@ class TestUtils(TestCase):
         from utils import get_cache
         
         # If there is no cache_base, we should get default
+        print("Case 1: No cache base returns default")
         home = os.environ['HOME']
         cache = get_cache()
         self.assertEqual("%s/.singularity" %home,cache)
         self.assertTrue(os.path.exists(cache))
 
         # If we give a base, we should get that base instead
+        print("Case 2: custom specification of cache base")
         cache_base = '%s/cache' %(home)
         cache = get_cache(cache_base=cache_base)
         self.assertEqual(cache_base,cache)
         self.assertTrue(os.path.exists(cache))
 
         # If we specify a subfolder, we should get that added
+        print("Case 3: Ask for subfolder in cache base")
         subfolder = 'docker'
         cache = get_cache(subfolder=subfolder)
         self.assertEqual("%s/.singularity/%s" %(home,subfolder),cache)
         self.assertTrue(os.path.exists(cache))
 
         # If we disable the cache, we should get temporary directory
+        print("Case 4: Disable the cache (uses /tmp)")
         cache = get_cache(disable_cache=True)
         self.assertTrue(os.path.exists(cache))
         self.assertTrue(re.search("tmp",cache)!=None)
 
         # If environmental variable set, should use that
+        print("Case 5: cache base obtained from environment")
         SINGULARITY_CACHEDIR = '%s/cache' %(home)
         os.environ['SINGULARITY_CACHEDIR'] = SINGULARITY_CACHEDIR
         cache = get_cache()
@@ -183,21 +198,23 @@ class TestUtils(TestCase):
         from utils import change_permissions
         from stat import ST_MODE
         
-        os.system('touch .mooza')
+        tmpdir = tempfile.mkdtemp()
+        tmpfile = '%s/.mooza' %(tmpdir)
+        os.system('touch %s' %(tmpfile))
 
         # 664
-        permissions = oct(os.stat(".mooza")[ST_MODE])[-3:]
+        permissions = oct(os.stat(tmpfile)[ST_MODE])[-3:]
         self.assertTrue(permissions,'664')
         # to 755
-        change_permissions(".mooza",permission="0755")  
-        new_permissions = oct(os.stat(".mooza")[ST_MODE])[-3:]
+        change_permissions(tmpfile,permission="0755")  
+        new_permissions = oct(os.stat(tmpfile)[ST_MODE])[-3:]
         self.assertTrue(new_permissions,'755')
         # and back
-        change_permissions(".mooza",permission="0644")  
-        new_permissions = oct(os.stat(".mooza")[ST_MODE])[-3:]
+        change_permissions(tmpfile,permission="0644")  
+        new_permissions = oct(os.stat(tmpfile)[ST_MODE])[-3:]
         self.assertTrue(new_permissions,'664')
 
-        os.remove('.mooza')
+        shutil.rmtree(tmpdir)
 
 
     #TODO: need to test api_get
