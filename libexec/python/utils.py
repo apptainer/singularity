9b3b03b5f9bf5c659b3939128aae1c7b784b794a
Felix Abecassis <fabecassis@nvidia.com>
2016-11-29 22:37:14 -0800

    Cleanup extract_tar() in utils.py
    Remove dependency on "re" module
    
    Signed-off-by: Felix Abecassis <fabecassis@nvidia.com>

--- libexec/python/utils.py
+++ libexec/python/utils.py
@@ -24,7 +24,6 @@ from defaults import SINGULARITY_CACHE
 from logman import logger
 import json
 import os
-import re
 import shutil
 import subprocess
 import sys
@@ -259,19 +258,19 @@ def change_permissions(path,permission="0755",recursive=True):
         return run_command(cmd)
 
 
-def extract_tar(targz,output_folder):
-    '''extract_tar will extract a tar.gz to a specified output folder
-    :param targz: the tar.gz file to extract
+def extract_tar(archive,output_folder):
+    '''extract_tar will extract a tar archive to a specified output folder
+    :param archive: the archive file to extract
     :param output_folder: the output folder to extract to
     '''
     # If extension is .tar.gz, use -xzf
     args = '-xf'
-    if re.search('.tar.gz$',targz):
+    if archive.endswith(".tar.gz"):
         args = '-xzf'
 
     # Just use command line, more succinct.
-    command = ["tar", args, targz, "-C", output_folder, "--exclude=dev/*"]
-    print("Extracting %s" %(targz))
+    command = ["tar", args, archive, "-C", output_folder, "--exclude=dev/*"]
+    print("Extracting %s" %(archive))
 
     # Should we return a list of extracted files? Current returns empty string
     return run_command(command)
