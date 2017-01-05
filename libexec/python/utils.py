0593989e683390594106492d7d4f85e2fe3051a2
vsoch <vsochat@stanford.edu>
2017-01-05 15:13:04 -0800

    removing username/password debug line, not a good idea

--- libexec/python/utils.py
+++ libexec/python/utils.py
@@ -180,7 +180,6 @@ def basic_auth_header(username, password):
     :param password: the password
     '''
     s = "%s:%s" % (username, password)
-    logger.debug("AUTH: Input username/password: %s", s)
     if sys.version_info[0] >= 3:
         s = bytes(s, 'utf-8')
         credentials = base64.b64encode(s).decode('utf-8')
