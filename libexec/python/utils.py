c90c544c46f48094c91204ffd48cca7af7d3a96e
vsoch <vsochat@stanford.edu>
2017-01-05 14:58:36 -0800

    teaching myself to correctly print things again... lol

--- libexec/python/utils.py
+++ libexec/python/utils.py
@@ -180,14 +180,14 @@ def basic_auth_header(username, password):
     :param password: the password
     '''
     s = "%s:%s" % (username, password)
-    logger.debug("AUTH: Input username/password:", s)
+    logger.debug("AUTH: Input username/password: %s", s)
     if sys.version_info[0] >= 3:
         s = bytes(s, 'utf-8')
         credentials = base64.b64encode(s).decode('utf-8')
     else:
         credentials = base64.b64encode(s)
     auth = {"Authorization": "Basic %s" % credentials}
-    logger.debug("AUTH Final", auth)
+    logger.debug("AUTH Final %s", auth)
     return auth
 
 
