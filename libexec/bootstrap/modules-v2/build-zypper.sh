b3086dfb1ddf16fca6b11093d63cceddd4a718f7
Dmitry Spodarets <d.spodarets@flyelephant.net>
2017-01-31 23:21:07 +0200

    Update build-zypper.sh

--- libexec/bootstrap/modules-v2/build-zypper.sh
+++ libexec/bootstrap/modules-v2/build-zypper.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 # 
-# Copyleft (c) 2017, FlyElephant Team. All wrongs reserved.
-# 
-# “Singularity” Copyright (c) 2016, The Regents of the University of California,
-# through Lawrence Berkeley National Laboratory (subject to receipt of any
-# required approvals from the U.S. Dept. of Energy).  All rights reserved.
-
-# NOTICE. This Software uses parts which was developed under funding
-# from the U.S. Department of  Energy and the U.S. Government consequently 
-# retains certain rights. As such, the U.S. Government has been granted 
-# for itself and others acting on its  behalf a paid-up, nonexclusive,
-# irrevocable, worldwide license in the Software to reproduce, distribute
-# copies to the public, prepare derivative works, and
-# perform publicly and display publicly, and to permit other to do so. 
+# Copyright (c) 2017, FlyElephant Team, MIT License.
 # 
 
 ## Basic sanity
