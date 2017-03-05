2343dc339b994bf31b59620bd701406bd7c0ead7
vsoch <vsochat@stanford.edu>
2017-03-04 19:23:53 -0500

    quick tweak just to fix this PR, but larger changes needed!

--- libexec/python/tests/test_shub_pull.py
+++ libexec/python/tests/test_shub_pull.py
@@ -49,6 +49,7 @@ class TestImport(TestCase):
         # Variables are obtained from environment
         os.environ["SINGULARITY_CONTAINER"] = "shub://vsoch/singularity-images" 
         os.environ["SINGULARITY_HUB_PULL_FOLDER"] = self.tmpdir
+        os.environ["SINGULARITY_LAYERFILE"] = "%s/.layers" %self.tmpdir
 
         print("\n---START----------------------------------------")
 
