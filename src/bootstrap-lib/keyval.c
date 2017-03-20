cbd0c5fb7984430a60563d098d5766501e8ac6f9
Satrajit Ghosh <satra@mit.edu>
2017-03-19 21:53:11 -0400

    fix: removed tabs

--- src/bootstrap-lib/keyval.c
+++ src/bootstrap-lib/keyval.c
@@ -57,28 +57,30 @@ int bootstrap_keyval_parse(char *path) {
         if ( line[0] == '%' ) { // We hit a section, stop parsing for keyword tags
             break;
         } else if ( ( bootdef_key = strtok(line, ":") ) != NULL ) {
-	  chomp(bootdef_key);
-	  char *bootdef_value;
 
-	  if ( ( bootdef_value = strtok(NULL, "\n") ) != NULL ) {
-	    chomp_comments(bootdef_value);
-	    
-	    singularity_message(VERBOSE2, "Got bootstrap definition key/val '%s' = '%s'\n", bootdef_key, bootdef_value);
-	    
-	    if ( strcasecmp(bootdef_key, "import") == 0 ) {
-	      bootstrap_keyval_parse(bootdef_value);
-	    }
+            chomp(bootdef_key);
 
-	    if ( strcasecmp(bootdef_key, "bootstrap") == 0 ) {
-	      singularity_registry_set("DRIVER", bootdef_value);
-	    }
-	    
-	    // Cool little feature, every key defined in def file is transposed
-	    // to environment
-	    setenv(uppercase(bootdef_key), bootdef_value, 1);
-	    setenv(strjoin("SINGULARITY_DEFFILE_", bootdef_key), bootdef_value, 1);
-	  }
-	}
+            char *bootdef_value;
+
+            if ( ( bootdef_value = strtok(NULL, "\n") ) != NULL ) {
+
+                chomp_comments(bootdef_value);
+                singularity_message(VERBOSE2, "Got bootstrap definition key/val '%s' = '%s'\n", bootdef_key, bootdef_value);
+
+                if ( strcasecmp(bootdef_key, "import") == 0 ) {
+                  bootstrap_keyval_parse(bootdef_value);
+                }
+
+                if ( strcasecmp(bootdef_key, "bootstrap") == 0 ) {
+                  singularity_registry_set("DRIVER", bootdef_value);
+                }
+
+                // Cool little feature, every key defined in def file is transposed
+                // to environment
+                setenv(uppercase(bootdef_key), bootdef_value, 1);
+                setenv(strjoin("SINGULARITY_DEFFILE_", bootdef_key), bootdef_value, 1);
+            }
+        }
     }
 
     free(line);
