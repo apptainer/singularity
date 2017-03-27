563d8bef835451d1cbf807159e28be1d8c981ace
Gregory M. Kurtzer <gmkurtzer@lbl.gov>
2017-03-27 15:56:40 +0000

    Add current working directory to container prompt

--- libexec/bootstrap-scripts/environment/01-base.sh
+++ libexec/bootstrap-scripts/environment/01-base.sh
@@ -22,7 +22,7 @@
 
 if test -z "$SINGULARITY_INIT"; then
     PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
-    PS1="Singularity $SINGULARITY_CONTAINER> "
+    PS1="Singularity $SINGULARITY_CONTAINER:\\w> "
     LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/lib:/usr/local/lib64"
     SINGULARITY_INIT=1
     export PATH PS1 SINGULARITY_INIT LD_LIBRARY_PATH
