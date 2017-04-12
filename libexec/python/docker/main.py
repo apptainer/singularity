ea5e0e7b26f6d3e4ef637cb4c8c66370b59f452e
vsoch <vsochat@stanford.edu>
2017-04-11 21:53:16 -0400

    modified:   libexec/python/docker/api.py 	modified:   libexec/python/docker/main.py

--- libexec/python/docker/main.py
+++ libexec/python/docker/main.py
@@ -98,7 +98,7 @@ def IMPORT(image,auth=None,layerfile=None):
 
         image_id = images[ii]
         targz = "%s/%s.tar.gz" %(cache_base,image_id)
-        prefix = "[" + str(ii+i) + "/" + str(len(images)) + "] Download"  
+        prefix = "[%s/%s]Download" %((ii+1),len(images))
         if not os.path.exists(targz):
             targz = client.get_layer(image_id=image_id,
                                      download_folder=cache_base,
