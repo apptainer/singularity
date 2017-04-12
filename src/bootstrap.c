23e5295cbd9ab5a56d89c79f26b8a636b92ad9a4
Gregory M. Kurtzer <gmkurtzer@gmail.com>
2017-04-12 15:53:56 -0700

    Set LANG=C (ref: #594)

--- src/bootstrap.c
+++ src/bootstrap.c
@@ -46,7 +46,6 @@
 
 int main(int argc, char **argv) {
     struct image_object image;
-    char *lang = envar_get("LANG", "_-=+:,.%", 128);
     char *term = envar_get("TERM", "-", 128);
 
     singularity_config_init(joinpath(SYSCONFDIR, "/singularity/singularity.conf"));
@@ -65,7 +64,7 @@ int main(int argc, char **argv) {
     envar_set("SINGULARITY_BUILDDEF", singularity_registry_get("BUILDDEF"), 1);
     envar_set("SINGULARITY_MESSAGELEVEL", singularity_registry_get("MESSAGELEVEL"), 1);
     envar_set("SINGULARITY_version", singularity_registry_get("VERSION"), 1);
-    envar_set("LANG", lang, 1);
+    envar_set("LANG", "C", 1);
     envar_set("TERM", term, 1);
 
     singularity_message(INFO, "Setting envar: 'HOME' = '%s'\n", singularity_priv_home());
