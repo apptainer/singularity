2d7a9ae2f2adecea22e4774588d8023cb72975b0
Gregory M. Kurtzer <gmkurtzer@gmail.com>
2017-04-28 15:39:32 -0700

    Added @godlovedc's Nvidia library recommendations

--- libexec/cli/action_argparser.sh
+++ libexec/cli/action_argparser.sh
@@ -109,7 +109,7 @@ while true; do
         ;;
         -n|--nv)
             shift
-            for i in `ldconfig -p | grep -E "/libnv|/libcuda"`; do
+            for i in `ldconfig -p | grep -E "/libnv|/libcuda|/libEGL|/libGL|/libnvcu|/libvdpau|/libOpenCL|/libOpenGL"`; do
                 if [ -f "$i" ]; then
                     message 2 "Found NV library: $i\n"
                     if [ -z "${SINGULARITY_CONTAINLIBS:-}" ]; then
