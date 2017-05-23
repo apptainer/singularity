399c786e7ed945b9d6a8050bd0c0b04467f56aab
Gregory M. Kurtzer <gmkurtzer@gmail.com>
2017-05-23 14:01:13 -0700

    Properly handle commands to run within the mounted environment

--- src/mount.c
+++ src/mount.c
@@ -71,12 +71,22 @@ int main(int argc, char **argv) {
         ABORT(255);
     }
 
-    if ( singularity_priv_getuid() != 0 ) {
+    if ( ( argc > 1 ) || ( singularity_priv_getuid() != 0 ) ) {
         singularity_runtime_ns(SR_NS_MNT);
     }
 
     singularity_image_bind(&image);
     singularity_image_mount(&image, singularity_registry_get("MOUNTPOINT"));
+
+    if ( argc > 1 ) {
+        singularity_priv_drop_perm();
+        singularity_message(VERBOSE, "Running command: %s\n", argv[1]);
+        singularity_message(DEBUG, "Calling exec...\n");
+        execvp(argv[1], &argv[1]); // Flawfinder: ignore (Yes flawfinder, we are exec'ing)
+
+        singularity_message(ERROR, "Exec failed: %s: %s\n", argv[1], strerror(errno));
+        ABORT(255);
+    }
 
     singularity_message(INFO, "%s is mounted at: %s\n\n", singularity_image_name(&image), singularity_registry_get("MOUNTPOINT"));
 
