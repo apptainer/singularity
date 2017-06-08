19edbd9b4ba11b9a705ac88ddc34f808cbebce8c
Michael Bauer <bauerm@umich.edu>
2017-06-08 16:30:53 -0700

    Add sinit file for running PID 1 inside PID NS

---
+++ src/sinit.c
@@ -0,0 +1,64 @@
+/* 
+ * Copyright (c) 2017, SingularityWare, LLC. All rights reserved.
+ *
+ * Copyright (c) 2015-2017, Gregory M. Kurtzer. All rights reserved.
+ * 
+ * Copyright (c) 2016-2017, The Regents of the University of California,
+ * through Lawrence Berkeley National Laboratory (subject to receipt of any
+ * required approvals from the U.S. Dept. of Energy).  All rights reserved.
+ * 
+ * This software is licensed under a customized 3-clause BSD license.  Please
+ * consult LICENSE file distributed with the sources of this project regarding
+ * your rights to use or distribute this software.
+ * 
+ * NOTICE.  This Software was developed under funding from the U.S. Department of
+ * Energy and the U.S. Government consequently retains certain rights. As such,
+ * the U.S. Government has been granted for itself and others acting on its
+ * behalf a paid-up, nonexclusive, irrevocable, worldwide license in the Software
+ * to reproduce, distribute copies to the public, prepare derivative works, and
+ * perform publicly and display publicly, and to permit other to do so. 
+ * 
+ */
+
+
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <fcntl.h>
+
+#include "config.h"
+#include "util/file.h"
+#include "util/util.h"
+#include "util/registry.h"
+#include "lib/image/image.h"
+#include "lib/runtime/runtime.h"
+#include "util/config_parser.h"
+#include "util/fork.h"
+#include "util/privilege.h"
+#include "util/suid.h"
+#include "util/sessiondir.h"
+#include "util/cleanupd.h"
+
+#include "./action-lib/include.h"
+
+#ifndef SYSCONFDIR
+#error SYSCONFDIR not defined
+#endif
+
+
+
+int main(int argc, char **argv) {
+    struct image_object image;
+    char *pwd = get_current_dir_name();
+    char *target_pwd = NULL;
+    char *command = NULL;
+
+    singularity_config_init(joinpath(SYSCONFDIR, "/singularity/singularity.conf"));
+
+    singularity_registry_init();
+
+    singularity_fork_daemonize();
+}
