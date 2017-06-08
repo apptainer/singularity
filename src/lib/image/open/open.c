987900a4e7895b538ec7d1729a380fc0564fe8d9
Gregory M. Kurtzer <gmkurtzer@gmail.com>
2017-06-08 10:51:50 -0700

    Remove FD_CLOEXEC option on image (ref: #714)
    Note: this means the file descriptor will now be purposefully leaked
    into the container environment. But there are potential benefits to
    that.

--- src/lib/image/open/open.c
+++ src/lib/image/open/open.c
@@ -65,11 +65,6 @@ int _singularity_image_open(struct image_object *image, int open_flags) {
         ABORT(255);
     }
 
-    if ( fcntl(image->fd, F_SETFD, FD_CLOEXEC) != 0 ) {
-        singularity_message(ERROR, "Could not set file descriptor flag to close on exit: %s\n", strerror(errno));
-        ABORT(255);
-    }
-
     if ( fstat(image->fd, &imagestat) < 0 ) {
         singularity_message(ERROR, "Failed calling fstat() on %s (fd: %d): %s\n", image->path, image->fd, strerror(errno));
         ABORT(255);
