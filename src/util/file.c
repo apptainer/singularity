9c7e3159bd5b2df9191aa995d000c3fd70d7cf6a
Michael Bauer <bauerm@umich.edu>
2017-06-08 10:55:50 -0700

    Add mask support to chk_mode()

--- src/util/file.c
+++ src/util/file.c
@@ -99,7 +99,7 @@ int chk_perms(char *path, mode_t mode) {
     return(-1);
 }
 
-int chk_mode(char *path, mode_t mode) {
+int chk_mode(char *path, mode_t mode, mode_t mask) {
     struct stat filestat;
 
     singularity_message(DEBUG, "Checking exact mode (%o) on: %s\n", mode, path);
@@ -109,7 +109,7 @@ int chk_mode(char *path, mode_t mode) {
         return(-1);
     }
 
-    if ( filestat.st_mode == mode ) {
+    if ( ( filestat.st_mode | mask )  == ( mode | mask ) ) {
         singularity_message(DEBUG, "Found appropriate mode on file: %s\n", path);
         return(0);
     } else {
