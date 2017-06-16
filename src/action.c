f79b393acea8301c0919d841eca29dad68996813
Michael Bauer <bauerm@umich.edu>
2017-06-15 17:30:27 -0700

    Call daemon_rootfs to set the rootfs to what is given in daemon file

--- src/action.c
+++ src/action.c
@@ -67,7 +67,8 @@ int main(int argc, char **argv) {
 
     daemon_join();
     singularity_runtime_ns(SR_NS_ALL);
-
+    daemon_rootfs();
+    
     singularity_sessiondir();
     singularity_cleanupd();
     
