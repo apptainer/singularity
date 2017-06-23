7efaaf6af68ddaab86eac48ce34f2bb97ec08c05
Gregory M. Kurtzer <gmkurtzer@gmail.com>
2017-06-23 10:17:39 -0700

    Add MS_NODEV to the tmpfs portion of the overlay mount

--- src/lib/runtime/overlayfs/overlayfs.c
+++ src/lib/runtime/overlayfs/overlayfs.c
@@ -75,7 +75,7 @@ int _singularity_runtime_overlayfs(void) {
 
         singularity_priv_escalate();
         singularity_message(DEBUG, "Mounting overlay tmpfs: %s\n", overlay_mount);
-        if ( mount("tmpfs", overlay_mount, "tmpfs", MS_NOSUID, "size=1m") < 0 ){
+        if ( mount("tmpfs", overlay_mount, "tmpfs", MS_NOSUID | MS_NODEV, "size=1m") < 0 ){
             singularity_message(ERROR, "Failed to mount overlay tmpfs %s: %s\n", overlay_mount, strerror(errno));
             ABORT(255);
         }
