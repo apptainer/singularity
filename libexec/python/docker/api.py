d7a98cb26a6f6fa88ba6316cd2d2966716560f19
vsoch <vsochat@stanford.edu>
2017-07-06 11:25:25 -0700

    update to docker api to give error message before attempting bootstrap when errors present (and image doesn't exist)

--- libexec/python/docker/api.py
+++ libexec/python/docker/api.py
@@ -107,6 +107,21 @@ class DockerApiConnection(ApiConnection):
                'Content-Type':'application/json; charset=utf-8'}
 
 
+    def check_errors(self,response,exit=True):
+        '''take a response with errors key, iterate through errors in expected format
+        and exit upon completion
+        '''
+        if "errors" in response:
+            for error in response['errors']:
+                bot.error("%s: %s" %(error['code'],error['message']))
+                if error['code'] == "UNAUTHORIZED":
+                    bot.error("Check image existence, capitalization, and permissions.")
+            if exit:
+                sys.exit(1)
+        return response
+
+
+
     def load_image(self,image):
         '''load_image parses the image uri, and loads the different image parameters into
         the client. The image should be a docker uri (eg docker://) or name of docker image.
@@ -261,7 +276,8 @@ class DockerApiConnection(ApiConnection):
             bot.error("Error getting manifest for %s, exiting." %repo_uri)
             sys.exit(1)
 
-        return response
+        # If we have errors, don't continue
+        return self.check_errors(response)
 
 
     def get_layer(self,image_id,download_folder=None,change_perms=False,return_tmp=False):
