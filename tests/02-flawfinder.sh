4061bed2e2c365bda9ef1220dd5f0ee0c6fa0401
Jason Stover <jason.stover@gmail.com>
2017-07-19 15:44:53 -0500

    Exit if flawfinder isn't found.

--- tests/02-flawfinder.sh
+++ tests/02-flawfinder.sh
@@ -24,7 +24,11 @@
 
 test_init "Running Flawfinder"
 
-stest 0 which flawfinder
+if ! which flawfinder 2>/dev/null; then
+    echo "Not testing with flawfinder, not installed"
+    exit 0
+fi
+
 stest 0 sh -c "flawfinder -SQ --minlevel=3 ../src > '$SINGULARITY_TESTDIR/ff.out'"
 if ! grep -q 'No hits found.' "$SINGULARITY_TESTDIR/ff.out"; then
     cat "$SINGULARITY_TESTDIR/ff.out"
