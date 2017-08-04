610986b4768728479d6bcd64091e65270e5c087b
GodloveD <davidgodlove@gmail.com>
2017-08-04 16:28:29 -0400

    when will I learn to test before commit?

--- libexec/bootstrap-scripts/deffile-driver-localimage.sh
+++ libexec/bootstrap-scripts/deffile-driver-localimage.sh
@@ -35,7 +35,7 @@ fi
 if [ -z "${FROM:-}" ]; then
     message ERROR "Required Definition tag 'From:' not defined.\n"
     exit 1
-else if [ ! -f "${FROM:-}" ]; then
+elif [ ! -f "${FROM:-}" ]; then
     message ERROR "${FROM} does not exist\n"
     exit 1
 fi
