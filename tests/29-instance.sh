a5c22e99db5497ddc4864f7a34ee894eecdd4882
Gregory M. Kurtzer <gmkurtzer@gmail.com>
2017-09-17 15:09:59 -0700

    Fixing up tests

--- tests/29-instance.sh
+++ tests/29-instance.sh
@@ -23,20 +23,17 @@ CONTAINER="$SINGULARITY_TESTDIR/container"
 stest 0 sudo singularity build "$CONTAINER" "../examples/busybox/Singularity"
 stest 0 singularity instance.start "$CONTAINER" service1
 stest 0 sh -c "ps -ef | grep -q sinit"
-stest 0 sh -c "singularity exec --join service1 ${CONTAINER} ps -ef | grep -q sinit"
-export PID=`singularity exec --join service1 ${CONTAINER} ps -ef | grep sinit | awk '{print $1}'`
+stest 0 sh -c "singularity exec instance://service1 ps -ef | grep -q sinit"
+export PID=`singularity exec instance://service1 ps -ef | grep sinit | awk '{print $1}'`
 stest 0 sh -c "echo $PID | grep -q 1" 
 stest 1 singularity instance.start "$CONTAINER" service1
 stest 0 singularity instance.start "$CONTAINER" service2
 stest 0 singularity instance.start "$CONTAINER" service3
-stest 0 sh -c "ps -ef | grep sinit | wc -l | grep -q 4" # 3 instances + grep process
-stest 0 singularity instance.list
-stest 0 sh -c "singularity instance.list | grep service | wc -l | grep -q 3"
-stest 0 singularity instance.stop "$CONTAINER" service1
-stest 0 sh -c "ps -ef | grep sinit | wc -l | grep -q 4" # 2 instances + grep process + subshell
-stest 0 singularity instance.stop-all
-stest 0 sh -c "ps -ef | grep sinit | wc -l | grep -q 2" # just grep process and subshell
-stest 0 sh -c "singularity instance.list | wc -l | grep -q 0"
+stest 0 singularity instance.list service1
+stest 0 singularity instance.stop service1
+stest 1 singularity instance.list service1
+stest 0 singularity instance.stop service\*
+stest 1 singularity instance.list service\*
 
 stest 0 sudo rm -rf "$CONTAINER"
 test_cleanup
