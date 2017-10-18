01c0e2acaa5a7ba75ced93bb94e8b51cd44bc32a
vsoch <vsochat@stanford.edu>
2017-10-18 14:39:15 -0700

    modified:   libexec/python/tests/test_helpers.py

--- libexec/python/tests/test_helpers.py
+++ libexec/python/tests/test_helpers.py
@@ -91,6 +91,7 @@ class TestJson(TestCase):
         shub_cont = "shub://vsoch/singularity-hello-world"
         os.environ['SINGULARITY_CONTAINER'] = shub_cont
         os.environ['SINGULARITY_CONTENTS'] = self.file
+        os.environ['SINGULARITY_MESSAGELEVEL'] = 5
 
         script_path = "%s/size.py" % self.here
         if VERSION == 2:
@@ -98,6 +99,7 @@ class TestJson(TestCase):
         else:
             testing_command = ["python3", script_path]
 
+        print(' '.join(testing_command))
         output = Popen(testing_command,
                        stderr=STDOUT,
                        stdout=PIPE)
@@ -105,6 +107,7 @@ class TestJson(TestCase):
         t = output.communicate()[0], output.returncode
         result = {'message': t[0],
                   'return_code': t[1]}
+        print(result['message'])
         self.assertEqual(result['return_code'], 0)
         result = read_file(self.file)[0]
         self.assertEqual('260', result)
