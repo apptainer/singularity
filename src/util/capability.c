818b648257e32968e4f8501a22309dc68a61de62
Cedric Clerget <cedric.clerget@univ-fcomte.fr>
2017-10-20 19:45:51 +0200

    Add CAP_AUDIT_WRITE as default to fix PAM issue during build

--- src/util/capability.c
+++ src/util/capability.c
@@ -74,6 +74,7 @@ static __u32 default_capabilities[] = {
     CAP_SYS_PTRACE,
     CAP_DAC_READ_SEARCH,
     CAP_DAC_OVERRIDE,
+    CAP_AUDIT_WRITE,
     NO_CAP
 };
 
@@ -86,6 +87,7 @@ static __u32 minimal_capabilities[] = {
     CAP_SYS_CHROOT,
     CAP_DAC_READ_SEARCH,
     CAP_DAC_OVERRIDE,
+    CAP_AUDIT_WRITE,
     NO_CAP
 };
 
