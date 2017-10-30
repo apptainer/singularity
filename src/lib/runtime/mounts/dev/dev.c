769204688b61d2638de74498b6de3492927210f4
Cedric Clerget <cedric.clerget@univ-fcomte.fr>
2017-10-30 22:55:05 +0100

    Add mount wrapper called singularity_mount

--- src/lib/runtime/mounts/dev/dev.c
+++ src/lib/runtime/mounts/dev/dev.c
@@ -38,8 +38,8 @@
 #include "util/privilege.h"
 #include "util/config_parser.h"
 #include "util/registry.h"
+#include "util/mount.h"
 
-#include "../mount-util.h"
 #include "../../runtime.h"
 
 static int bind_dev(char *tmpdir, char *dev);
@@ -104,7 +104,7 @@ int _singularity_runtime_mount_dev(void) {
 
         singularity_priv_escalate();
         singularity_message(DEBUG, "Mounting tmpfs for staged /dev/shm\n");
-        if ( mount("/dev/shm", joinpath(devdir, "/shm"), "tmpfs", MS_NOSUID, "") < 0 ) {
+        if ( singularity_mount("/dev/shm", joinpath(devdir, "/shm"), "tmpfs", MS_NOSUID, "") < 0 ) {
             singularity_message(ERROR, "Failed to mount %s: %s\n", joinpath(devdir, "/shm"), strerror(errno));
             ABORT(255);
         }
@@ -143,7 +143,7 @@ int _singularity_runtime_mount_dev(void) {
             snprintf(devpts_opts, max_sz-1, "%s%d", devpts_opts_base, ttygid->gr_gid);
 
             singularity_message(DEBUG, "Mounting devpts for staged /dev/pts\n");
-            if ( mount("devpts", joinpath(devdir, "/pts"), "devpts", MS_NOSUID|MS_NOEXEC, devpts_opts) < 0 ) {
+            if ( singularity_mount("devpts", joinpath(devdir, "/pts"), "devpts", MS_NOSUID|MS_NOEXEC, devpts_opts) < 0 ) {
                 if (errno == EINVAL) {
                     // This is the error when unprivileged on RHEL7.4
                     singularity_message(VERBOSE, "Couldn't mount %s, continuing\n", joinpath(devdir, "/pts"));
@@ -162,7 +162,7 @@ int _singularity_runtime_mount_dev(void) {
         }
 
         singularity_message(DEBUG, "Mounting minimal staged /dev into container\n");
-        if ( mount(devdir, joinpath(container_dir, "/dev"), NULL, MS_BIND|MS_REC, NULL) < 0 ) {
+        if ( singularity_mount(devdir, joinpath(container_dir, "/dev"), NULL, MS_BIND|MS_REC, NULL) < 0 ) {
             singularity_priv_drop();
             singularity_message(WARNING, "Could not stage dev tree: '%s' -> '%s': %s\n", devdir, joinpath(container_dir, "/dev"), strerror(errno));
             free(sessiondir);
@@ -182,7 +182,7 @@ int _singularity_runtime_mount_dev(void) {
         if ( is_dir(joinpath(container_dir, "/dev")) == 0 ) {
                 singularity_priv_escalate();
                 singularity_message(VERBOSE, "Bind mounting /dev\n");
-                if ( mount("/dev", joinpath(container_dir, "/dev"), NULL, MS_BIND|MS_NOSUID|MS_REC, NULL) < 0 ) {
+                if ( singularity_mount("/dev", joinpath(container_dir, "/dev"), NULL, MS_BIND|MS_NOSUID|MS_REC, NULL) < 0 ) {
                     singularity_message(ERROR, "Could not bind mount container's /dev: %s\n", strerror(errno));
                     ABORT(255);
                 }
@@ -223,7 +223,7 @@ static int bind_dev(char *tmpdir, char *dev) {
 
     singularity_priv_escalate();
     singularity_message(DEBUG, "Mounting device %s at %s\n", dev, path);
-    if ( mount(dev, path, NULL, MS_BIND, NULL) < 0 ) {
+    if ( singularity_mount(dev, path, NULL, MS_BIND, NULL) < 0 ) {
         singularity_priv_drop();
         singularity_message(WARNING, "Could not mount %s: %s\n", dev, strerror(errno));
         free(path);
--- src/lib/runtime/mounts/kernelfs/kernelfs.c
+++ src/lib/runtime/mounts/kernelfs/kernelfs.c
@@ -36,8 +36,8 @@
 #include "util/privilege.h"
 #include "util/config_parser.h"
 #include "util/registry.h"
+#include "util/mount.h"
 
-#include "../mount-util.h"
 #include "../../runtime.h"
 #include "../../ns/ns.h"
 
@@ -52,7 +52,7 @@ int _singularity_runtime_mount_kernelfs(void) {
             if ( singularity_registry_get("PIDNS_ENABLED") == NULL ) {
                 singularity_priv_escalate();
                 singularity_message(VERBOSE, "Bind-mounting host /proc\n");
-                if ( mount("/proc", joinpath(container_dir, "/proc"), NULL, MS_BIND | MS_NOSUID | MS_REC, NULL) < 0 ) {
+                if ( singularity_mount("/proc", joinpath(container_dir, "/proc"), NULL, MS_BIND | MS_NOSUID | MS_REC, NULL) < 0 ) {
                     singularity_message(ERROR, "Could not bind-mount host /proc into container: %s\n", strerror(errno));
                     ABORT(255);
                 }
@@ -60,7 +60,7 @@ int _singularity_runtime_mount_kernelfs(void) {
             } else {
                 singularity_priv_escalate();
                 singularity_message(VERBOSE, "Mounting new procfs\n");
-                if ( mount("proc", joinpath(container_dir, "/proc"), "proc", MS_NOSUID, NULL) < 0 ) {
+                if ( singularity_mount("proc", joinpath(container_dir, "/proc"), "proc", MS_NOSUID, NULL) < 0 ) {
                     singularity_message(ERROR, "Could not mount new procfs into container: %s\n", strerror(errno));
                     ABORT(255);
                 }
@@ -81,7 +81,7 @@ int _singularity_runtime_mount_kernelfs(void) {
             if ( singularity_priv_userns_enabled() == 1 ) {
                 singularity_priv_escalate();
                 singularity_message(VERBOSE, "Mounting /sys\n");
-                if ( mount("/sys", joinpath(container_dir, "/sys"), NULL, MS_BIND | MS_NOSUID | MS_REC, NULL) < 0 ) {
+                if ( singularity_mount("/sys", joinpath(container_dir, "/sys"), NULL, MS_BIND | MS_NOSUID | MS_REC, NULL) < 0 ) {
                     singularity_message(ERROR, "Could not mount /sys into container: %s\n", strerror(errno));
                     ABORT(255);
                 }
@@ -89,7 +89,7 @@ int _singularity_runtime_mount_kernelfs(void) {
             } else {
                 singularity_priv_escalate();
                 singularity_message(VERBOSE, "Mounting /sys\n");
-                if ( mount("sysfs", joinpath(container_dir, "/sys"), "sysfs", MS_NOSUID, NULL) < 0 ) {
+                if ( singularity_mount("sysfs", joinpath(container_dir, "/sys"), "sysfs", MS_NOSUID, NULL) < 0 ) {
                     singularity_message(ERROR, "Could not mount /sys into container: %s\n", strerror(errno));
                     ABORT(255);
                 }
