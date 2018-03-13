9945f35c9f965c23e761bc1cf0a0d02ef82a632b
Yannick Cote <yhcote@gmail.com>
2018-03-13 02:47:04 -0700

    Make sure warning flags works on older compilers.
    Signed-off-by: Yannick Cote <yhcote@gmail.com>

--- core/mlocal/frags/common_opts.mk
+++ core/mlocal/frags/common_opts.mk
@@ -6,11 +6,9 @@ AFLAGS := -g
 
 CFLAGS := -Wall -Werror -Wfatal-errors  -Wno-unknown-warning-option
 CFLAGS += -Wstrict-prototypes -Wshadow -Wpointer-arith -Wbad-function-cast
-CFLAGS += -Wwrite-strings -Woverlength-strings -Wstrict-prototypes
-CFLAGS += -Wunreachable-code -Wframe-larger-than=2047 -Wno-sign-compare
-CFLAGS += -Wclobbered -Wempty-body -Wimplicit-fallthrough=3
-CFLAGS += -Wmissing-field-initializers -Wmissing-parameter-type -Wtype-limits
-CFLAGS += -Wshift-negative-value -Wunused-parameter -Wunused-but-set-parameter
+CFLAGS += -Woverlength-strings -Wunreachable-code -Wframe-larger-than=2047
+CFLAGS += -Wno-sign-compare -Wclobbered -Wempty-body -Wmissing-parameter-type
+CFLAGS += -Wtype-limits -Wunused-parameter -Wunused-but-set-parameter
 CFLAGS += -Wno-discarded-qualifiers -Wno-incompatible-pointer-types
 CFLAGS += -pipe -fmessage-length=0
 
