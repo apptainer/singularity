a4cee6c5e88555afc5928d2769c8a74a22db0830
GodloveD <davidgodlove@gmail.com>
2018-03-31 02:48:50 +0000

    fixing tests/25-environment.sh

--- tests/25-environment.sh
+++ tests/25-environment.sh
@@ -13,7 +13,6 @@
 #
 #
 
-
 . ./functions
 
 test_init "Environment tests"
@@ -22,32 +21,35 @@ test_init "Environment tests"
 stest 0 singularity exec docker://alpine env | grep -q \
     PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-# No Dockerfile custom path, Set SINGULARITYENV_PATH
-stest 0 SINGULARITYENV_PATH=/usr/bin:/bin singularity exec docker://alpine env | grep -q \
-    PATH=/usr/bin:/bin
-
-# No Dockerfile custom path, Set SINGULARITYENV_APPEND_PATH
-stest 0 SINGULARITYENV_APPEND_PATH=/opt singularity exec docker://alpine env | grep -q \
-    PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt
-
-# No Dockerfile custom path, Set SINGULARITYENV_PREPEND_PATH
-stest 0 SINGULARITYENV_PREPEND_PATH=/opt singularity exec docker://alpine env | grep -q \
-    PATH=/opt:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-
 # Dockerfile custom path, No SINGULARITYENV_* variables 
 stest 0 singularity exec docker://godlovedc/lolcow env | grep -q \
     PATH=/usr/games:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-# Dockerfile custom path, Set SINGULARITYENV_PATH
-stest 0 SINGULARITYENV_PATH=/usr/bin:/bin singularity exec docker://godlovedc/lolcow env | grep -q \
-    PATH=/usr/bin:/bin
+# No Dockerfile custom path, Set SINGULARITYENV_APPEND_PATH
+export SINGULARITYENV_PREPEND_PATH=/foo
+stest 0 singularity exec docker://alpine env | grep -q \
+    PATH=/foo:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
 # Dockerfile custom path, Set SINGULARITYENV_APPEND_PATH
-stest 0 SINGULARITYENV_APPEND_PATH=/testpath singularity exec docker://godlovedc/lolcow env | grep -q \
-    PATH=/usr/games:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/testpath
+stest 0 singularity exec docker://godlovedc/lolcow env | grep -q \
+    PATH=/foo:/usr/games:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+# No Dockerfile custom path, Set SINGULARITYENV_PREPEND_PATH
+export SINGULARITYENV_APPEND_PATH=/bar
+stest 0 singularity exec docker://alpine env | grep -q \
+    PATH=/foo:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/bar
 
 # Dockerfile custom path, Set SINGULARITYENV_PREPEND_PATH
-stest 0 SINGULARITYENV_PREPEND_PATH=/testpath singularity exec docker://godlovedc/lolcow env | grep -q \
-    PATH=/testpath:/usr/games:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+stest 0 singularity exec docker://godlovedc/lolcow env | grep -q \
+    PATH=/foo:/usr/games:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/bar
+
+# No Dockerfile custom path, Set SINGULARITYENV_PATH
+export SINGULARITYENV_PATH=/usr/bin:/bin
+stest 0 singularity exec docker://alpine env | grep -q \
+    PATH=/usr/bin:/bin
+
+# Dockerfile custom path, Set SINGULARITYENV_PATH
+stest 0 singularity exec docker://godlovedc/lolcow env | grep -q \
+    PATH=/usr/bin:/bin
 
 test_cleanup
