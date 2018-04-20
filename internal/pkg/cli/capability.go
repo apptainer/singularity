02889bfd5b0e938d1420f9f4adde658de6f2ddfe
GodloveD <davidgodlove@gmail.com>
2018-04-20 11:00:17 -0400

    still adding meat for man pages

--- internal/pkg/cli/capability.go
+++ internal/pkg/cli/capability.go
@@ -5,21 +5,50 @@
   consult LICENSE file distributed with the sources of this project regarding
   your rights to use or distribute this software.
 */
+
 package cli
 
 import (
 	"github.com/spf13/cobra"
+    "github.com/singularityware/singularity/docs"
 )
 
+var capabilityUse string = `capability <subcommands>`
+
+var capabilityShort string = `
+command group supporting the management of Linux security capabilities on 
+containers`
+
+var capabilityLong string = `
+Capabilities allow you to have fine grained control over the permissions that 
+your containers need to run. For instance, if you need to `
+
+var capabilityExample string = `
+All group commands have their own help output:
+
+$ singularity help capability add
+$ singularity capability list --help
+`
+
 func init() {
+
+    manHelp := func(c *cobra.Command, args []string) {
+        docs.DispManPg("singularity-capability")
+    }
+
 	SingularityCmd.AddCommand(CapabilityCmd)
+    CapabilityCmd.SetHelpFunc(manHelp)
 	CapabilityCmd.AddCommand(CapabilityAddCmd)
 	CapabilityCmd.AddCommand(CapabilityDropCmd)
 	CapabilityCmd.AddCommand(CapabilityListCmd)
 }
 
 var CapabilityCmd = &cobra.Command{
-	Use: "capability <subcommand>",
 	Run: nil,
 	DisableFlagsInUseLine: true,
+
+    Use: capabilityUse,
+    Short: capabilityShort,
+    Long: capabilityLong,
+    Example: capabilityExample,
 }
--- internal/pkg/cli/capability_add.go
+++ internal/pkg/cli/capability_add.go
@@ -11,46 +11,69 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
+    "github.com/singularityware/singularity/docs"
 )
 
-var capabilityAddExamples string = `
-      $ singularity capability.add /tmp/my-sql.img mysql
+var capabilityAddUse string = `add [add options...] <capabilities>`
 
-      $ singularity shell capability://mysql
-      Singularity my-sql.img> pwd
-      /home/mibauer/mysql
-      Singularity my-sql.img> ps
-      PID TTY          TIME CMD
-        1 pts/0    00:00:00 sinit
-        2 pts/0    00:00:00 bash
-        3 pts/0    00:00:00 ps
-      Singularity my-sql.img> 
-    
-      $ singularity capability.stop /tmp/my-sql.img mysql
-      Stopping /tmp/my-sql.img mysql`
+var capabilityAddShort string = `
+add Linux capabilities to a container at runtime`
+
+var capabilityAddLong string = `
+The capability add command allows you to grant fine grained Linux capabilities 
+to your container at runtime. For instance, `
+
+var capabilityAddExample string = `
+$ singularity capability.add /tmp/my-sql.img mysql
+
+$ singularity shell capability://mysql
+Singularity my-sql.img> pwd
+/home/mibauer/mysql
+Singularity my-sql.img> ps
+PID TTY          TIME CMD
+1 pts/0    00:00:00 sinit
+2 pts/0    00:00:00 bash
+3 pts/0    00:00:00 ps
+Singularity my-sql.img> 
+
+$ singularity capability.stop /tmp/my-sql.img mysql
+Stopping /tmp/my-sql.img mysql`
 
 func init() {
+
+    manHelp := func(c *cobra.Command, args []string) {
+        docs.DispManPg("singularity-capability-add")
+    }
+
+
 	capabilityAddCmds := []*cobra.Command{
 		CapabilityAddCmd,
-		capabilityDotAddCmd,
+		// capabilityDotAddCmd,
 	}
 
 	for _, cmd := range capabilityAddCmds {
 		cmd.Flags().SetInterspersed(false)
+        cmd.SetHelpFunc(manHelp)
 	}
 
-	SingularityCmd.AddCommand(capabilityDotAddCmd)
+	// SingularityCmd.AddCommand(capabilityDotAddCmd)
 }
 
 var CapabilityAddCmd = &cobra.Command{
-	Use:  "add [add options...] <capabilities>",
 	Args: cobra.MinimumNArgs(2),
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("adding capability")
 	},
-	Example: capabilityAddExamples,
+
+    Use: capabilityAddUse,
+    Short: capabilityAddShort,
+    Long: capabilityAddLong,
+	Example: capabilityAddExample,
 }
 
+/* I'd like to have a discussion about dropping the dot version of command
+groups.  Don't really want to support duplicate code that doesn't serve a real
+purpose and if we are going to drop them major release is the time. 
 var capabilityDotAddCmd = &cobra.Command{
 	Use:  "capability.add [options...] <capabilities>",
 	Args: cobra.MinimumNArgs(2),
@@ -60,3 +83,4 @@ var capabilityDotAddCmd = &cobra.Command{
 	Example: capabilityAddExamples,
 	Hidden:  true,
 }
+*/
