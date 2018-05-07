0613804555239a5b7395f05c494a63b8af31d5b2
Vanessa Sochat <vsochat@stanford.edu>
2018-05-07 10:35:43 -0400

    still errors, but adding WIP!

---
+++ src/pkg/build/provisioners/provisioner_shub.go
@@ -0,0 +1,147 @@
+/*
+  Copyright (c) 2018, Sylabs, Inc. All rights reserved.
+
+  This software is licensed under a 3-clause BSD license.  Please
+  consult LICENSE file distributed with the sources of this project regarding
+  your rights to use or distribute this software.
+*/
+
+package provisioners
+
+import (
+	"encoding/json"
+        "errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+//        "log"
+	"os"
+        "time"
+
+	"github.com/containers/image/docker"
+	"github.com/containers/image/types"
+	oci "github.com/containers/image/oci/layout"
+	"github.com/singularityware/singularity/src/pkg/image"
+	"github.com/singularityware/singularity/src/pkg/sylog"
+)
+
+
+// ShubClient contains the uri and client
+type ShubClient struct {
+	Client   http.Client
+	ImageUri string
+	HTTPAddr string
+}
+
+// NewRemoteBuilder creates a RemoteBuilder with the specified details.
+func NewShubClient(uri string) (shubClient *ShubClient) {
+
+	sylog.Infof("%v", uri)
+	httpAddr := fmt.Sprintf("www.singularity-hub.org")
+	shubClient = &ShubClient{
+		Client: http.Client{
+			Timeout: 30 * time.Second,
+		},
+		HTTPAddr: httpAddr,
+		ImageUri: uri,
+	}
+
+	return
+}
+
+func (p *ShubProvisioner) getManifest() (err error) {
+
+	// Create a new Singularity Hub client
+	sc := &ShubClient{}
+
+	// Format the http address, coinciding with the image uri
+	httpAddr := fmt.Sprintf("/container/%s", p.srcRef)
+	sylog.Infof("%v", httpAddr)
+
+	// Create the request, add headers context
+	url := url.URL{Scheme: "http", Host: sc.HTTPAddr, Path: httpAddr}
+	req, err := http.NewRequest(http.MethodPost, url.String(), nil)
+	if err != nil {
+		return
+	}
+
+	// Do the request, if status isn't success, return error
+	res, err := sc.Client.Do(req)
+	if err != nil {
+		return
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		err = errors.New(res.Status)
+		return
+	}
+
+	err = json.NewDecoder(res.Body).Decode(&p)
+	return
+
+}
+
+// NewShubProvisioner returns a provisioner that can dump a previously
+// build Singularity image into a sandbox based on a URI
+func NewShubProvisioner(src string) (p *ShubProvisioner, err error) {
+
+	// Question for bauerm - where does this get called?
+	p = &ShubProvisioner{}
+
+	// Shub URI largely follows same namespace convention
+	p.srcRef, err = docker.ParseReference(src)
+	if err != nil {
+		return &ShubProvisioner{}, err
+	}
+
+	p.tmpfs, err = ioutil.TempDir("", "temp-oci-")
+	if err != nil {
+		return &ShubProvisioner{}, err
+	}
+
+	p.tmpfsRef, err = oci.ParseReference(p.tmpfs + ":" + "tmp")
+	if err != nil {
+		return &ShubProvisioner{}, err
+	}
+
+	return p, nil
+}
+
+// ShubProvisioner provisions a sandbox environment from a shub URI.
+type ShubProvisioner struct {
+	src      string
+	srcRef   types.ImageReference
+	tmpfs    string
+	tmpfsRef types.ImageReference
+}
+
+// Provision provisions a sandbox from the Shub source URI into the location
+// specified by i.
+func (p *ShubProvisioner) Provision(i *image.Sandbox) (err error) {
+
+	defer os.RemoveAll(p.tmpfs)
+
+	// Get the image manifest
+	manifest := p.getManifest()
+	println(manifest)
+	sylog.Infof("%v", manifest)
+
+	// retrieve the image
+	//err = p.fetch(i)
+	//if err != nil {
+	//	log.Fatal(err)
+	//	return
+	//}
+
+	//err = p.unpackTmpfs(i)
+	//if err != nil {
+	//	log.Fatal(err)
+	//	return
+	//}
+
+	return nil
+
+	return fmt.Errorf("Shub provisioner not implemented yet")
+}
