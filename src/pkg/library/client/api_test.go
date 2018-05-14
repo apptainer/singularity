201d01c1a97ea3ced77e08e5febd008cadb82bb7
David Trudgian <dave@trudgian.net>
2018-05-14 15:57:12 -0500

    Library - further api client outline tests

--- src/pkg/library/client/api_test.go
+++ src/pkg/library/client/api_test.go
@@ -15,6 +15,7 @@ import (
 	"net/http/httptest"
 	"reflect"
 
+	"github.com/globalsign/mgo/bson"
 )
 
 type mockService struct {
@@ -41,6 +42,7 @@ func (m *mockService) Stop() {
 }
 
 func (m *mockService) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
 	if m.reqCallback != nil {
 		m.reqCallback(r, m.t)
 	}
@@ -363,6 +365,316 @@ func Test_getImage(t *testing.T) {
 			}
 			if !reflect.DeepEqual(image, test.expectImage) {
 				t.Errorf("Got image %v - expected %v", image, test.expectImage)
+			}
+
+			m.Stop()
+
+		})
+
+	}
+}
+
+
+func Test_createEntity(t *testing.T) {
+
+	tests := []struct {
+		description  string
+		code         int
+		body         interface{}
+		reqCallback  func(*http.Request, *testing.T)
+		entityRef    string
+		expectEntity Entity
+		expectError  bool
+	}{
+		{
+			description:  "Valid Request",
+			code:         http.StatusOK,
+			body:         EntityResponse{Data: Entity{Name: "test"}, Error: JSONError{}},
+			entityRef:    "test",
+			expectEntity: Entity{Name: "test"},
+			expectError:  false,
+		},
+		{
+			description:  "Error response",
+			code:         http.StatusInternalServerError,
+			body:         Entity{},
+			entityRef:    "test",
+			expectEntity: Entity{},
+			expectError:  true,
+		},
+	}
+
+	// Loop over test cases
+	for _, test := range tests {
+		t.Run(test.description, func(t *testing.T) {
+
+			m := mockService{
+				t:           t,
+				code:        test.code,
+				body:        test.body,
+				reqCallback: test.reqCallback,
+				httpPath:    "/v1/entities/",
+			}
+
+			m.Run()
+
+			entity, err := createEntity(m.baseURI, test.entityRef)
+
+			if err != nil && !test.expectError {
+				t.Errorf("Unexpected error: %v", err)
+			}
+			if err == nil && test.expectError {
+				t.Errorf("Unexpected success. Expected error.")
+			}
+			if !reflect.DeepEqual(entity, test.expectEntity) {
+				t.Errorf("Got created entity %v - expected %v", entity, test.expectEntity)
+			}
+
+			m.Stop()
+
+		})
+
+	}
+}
+
+
+func Test_createCollection(t *testing.T) {
+
+	tests := []struct {
+		description  string
+		code         int
+		body         interface{}
+		reqCallback  func(*http.Request, *testing.T)
+		collectionRef    string
+		expectCollection Collection
+		expectError  bool
+	}{
+		{
+			description:  "Valid Request",
+			code:         http.StatusOK,
+			body:         CollectionResponse{Data: Collection{Name: "test"}, Error: JSONError{}},
+			collectionRef:    "test",
+			expectCollection: Collection{Name: "test"},
+			expectError:  false,
+		},
+		{
+			description:  "Error response",
+			code:         http.StatusInternalServerError,
+			body:         Collection{},
+			collectionRef:    "test",
+			expectCollection: Collection{},
+			expectError:  true,
+		},
+	}
+
+	// Loop over test cases
+	for _, test := range tests {
+		t.Run(test.description, func(t *testing.T) {
+
+			m := mockService{
+				t:           t,
+				code:        test.code,
+				body:        test.body,
+				reqCallback: test.reqCallback,
+				httpPath:    "/v1/collections/",
+			}
+
+			m.Run()
+
+			collection, err := createCollection(m.baseURI, test.collectionRef, bson.NewObjectId().Hex())
+
+			if err != nil && !test.expectError {
+				t.Errorf("Unexpected error: %v", err)
+			}
+			if err == nil && test.expectError {
+				t.Errorf("Unexpected success. Expected error.")
+			}
+			if !reflect.DeepEqual(collection, test.expectCollection) {
+				t.Errorf("Got created collection %v - expected %v", collection, test.expectCollection)
+			}
+
+			m.Stop()
+
+		})
+
+	}
+}
+
+
+func Test_createContainer(t *testing.T) {
+
+	tests := []struct {
+		description  string
+		code         int
+		body         interface{}
+		reqCallback  func(*http.Request, *testing.T)
+		containerRef    string
+		expectContainer Container
+		expectError  bool
+	}{
+		{
+			description:  "Valid Request",
+			code:         http.StatusOK,
+			body:         ContainerResponse{Data: Container{Name: "test"}, Error: JSONError{}},
+			containerRef:    "test",
+			expectContainer: Container{Name: "test"},
+			expectError:  false,
+		},
+		{
+			description:  "Error response",
+			code:         http.StatusInternalServerError,
+			body:         Container{},
+			containerRef:    "test",
+			expectContainer: Container{},
+			expectError:  true,
+		},
+	}
+
+	// Loop over test cases
+	for _, test := range tests {
+		t.Run(test.description, func(t *testing.T) {
+
+			m := mockService{
+				t:           t,
+				code:        test.code,
+				body:        test.body,
+				reqCallback: test.reqCallback,
+				httpPath:    "/v1/containers/",
+			}
+
+			m.Run()
+
+			container, err := createContainer(m.baseURI, test.containerRef, bson.NewObjectId().Hex())
+
+			if err != nil && !test.expectError {
+				t.Errorf("Unexpected error: %v", err)
+			}
+			if err == nil && test.expectError {
+				t.Errorf("Unexpected success. Expected error.")
+			}
+			if !reflect.DeepEqual(container, test.expectContainer) {
+				t.Errorf("Got created collection %v - expected %v", container, test.expectContainer)
+			}
+
+			m.Stop()
+
+		})
+
+	}
+}
+
+func Test_createImage(t *testing.T) {
+
+	tests := []struct {
+		description  string
+		code         int
+		body         interface{}
+		reqCallback  func(*http.Request, *testing.T)
+		imageRef    string
+		expectImage Image
+		expectError  bool
+	}{
+		{
+			description:  "Valid Request",
+			code:         http.StatusOK,
+			body:         ImageResponse{Data: Image{Hash: "sha256.e50a30881ace3d5944f5661d222db7bee5296be9e4dc7c1fcb7604bcae926e88"}, Error: JSONError{}},
+			imageRef:    "test",
+			expectImage: Image{Hash: "sha256.e50a30881ace3d5944f5661d222db7bee5296be9e4dc7c1fcb7604bcae926e88"},
+			expectError:  false,
+		},
+		{
+			description:  "Error response",
+			code:         http.StatusInternalServerError,
+			body:         Image{},
+			imageRef:    "test",
+			expectImage: Image{},
+			expectError:  true,
+		},
+	}
+
+	// Loop over test cases
+	for _, test := range tests {
+		t.Run(test.description, func(t *testing.T) {
+
+			m := mockService{
+				t:           t,
+				code:        test.code,
+				body:        test.body,
+				reqCallback: test.reqCallback,
+				httpPath:    "/v1/images/",
+			}
+
+			m.Run()
+
+			image, err := createImage(m.baseURI, test.imageRef, bson.NewObjectId().Hex())
+
+			if err != nil && !test.expectError {
+				t.Errorf("Unexpected error: %v", err)
+			}
+			if err == nil && test.expectError {
+				t.Errorf("Unexpected success. Expected error.")
+			}
+			if !reflect.DeepEqual(image, test.expectImage) {
+				t.Errorf("Got created collection %v - expected %v", image, test.expectImage)
+			}
+
+			m.Stop()
+
+		})
+
+	}
+}
+
+
+func Test_setTags(t *testing.T) {
+
+	tests := []struct {
+		description  string
+		code         int
+		reqCallback  func(*http.Request, *testing.T)
+		containerRef string
+		imageRef    string
+		tags		[]string
+		expectError  bool
+	}{
+		{
+			description:  "Valid Request",
+			code:         http.StatusOK,
+			containerRef: "test",
+			imageRef:    bson.NewObjectId().Hex(),
+			tags:	[]string{"tag1", "tag2", "tag3"},
+			expectError:  false,
+		},
+		{
+			description:  "Error response",
+			code:         http.StatusInternalServerError,
+			containerRef: "test",
+			imageRef:    bson.NewObjectId().Hex(),
+			tags:	[]string{"tag1", "tag2", "tag3"},
+			expectError:  true,
+		},
+	}
+
+	// Loop over test cases
+	for _, test := range tests {
+		t.Run(test.description, func(t *testing.T) {
+
+			m := mockService{
+				t:           t,
+				code:        test.code,
+				reqCallback: test.reqCallback,
+				httpPath:    "/v1/tags/" + test.containerRef,
+			}
+
+			m.Run()
+
+			err := setTags(m.baseURI, test.containerRef, test.imageRef, test.tags)
+
+			if err != nil && !test.expectError {
+				t.Errorf("Unexpected error: %v", err)
+			}
+			if err == nil && test.expectError {
+				t.Errorf("Unexpected success. Expected error.")
 			}
 
 			m.Stop()
