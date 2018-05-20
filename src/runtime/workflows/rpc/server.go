5a985c67de325ebf32212c93e0d104ab0e58aa7d
Adam Hughes <stickmanica@gmail.com>
2018-05-19 20:28:39 -0400

    Remove lint in RPC package

--- src/runtime/workflows/rpc/server.go
+++ src/runtime/workflows/rpc/server.go
@@ -1,22 +1,24 @@
 package rpc
 
 import (
-	singularityConfig "github.com/singularityware/singularity/src/runtime/workflows/workflows/singularity/config"
-	singularityRpcServer "github.com/singularityware/singularity/src/runtime/workflows/workflows/singularity/rpc/server"
 	"net"
 	"net/rpc"
+
+	singularityConfig "github.com/singularityware/singularity/src/runtime/workflows/workflows/singularity/config"
+	singularityRpcServer "github.com/singularityware/singularity/src/runtime/workflows/workflows/singularity/rpc/server"
 )
 
 var rpcServerMethods map[string]interface{}
 
 // Register RPC server methods for runtime engine
-func registerRuntimeRpcServerMethods(methods interface{}, name string) {
+func registerRuntimeRPCServerMethods(methods interface{}, name string) {
 	if rpcServerMethods == nil {
 		rpcServerMethods = make(map[string]interface{})
 	}
 	rpcServerMethods[name] = methods
 }
 
+// ServeRuntimeEngineRequests services runtime engine requests
 func ServeRuntimeEngineRequests(name string, conn net.Conn) {
 	methods := rpcServerMethods[name]
 	rpc.RegisterName(name, methods)
@@ -26,5 +28,5 @@ func ServeRuntimeEngineRequests(name string, conn net.Conn) {
 func init() {
 	// register singularity RPC methods
 	methods := new(singularityRpcServer.Methods)
-	registerRuntimeRpcServerMethods(methods, singularityConfig.Name)
+	registerRuntimeRPCServerMethods(methods, singularityConfig.Name)
 }
