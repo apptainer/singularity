67703f158b7b96e7c99dcdaeb3aec2fe15f73ec3
ArangoGutierrez <eduardo@sylabs.io>
2018-05-21 08:27:25 -0500

    Line comments are the norm

--- src/pkg/sylog/sylog.go
+++ src/pkg/sylog/sylog.go
@@ -1,10 +1,7 @@
-/*
-  Copyright (c) 2018, Sylabs, Inc. All rights reserved.
-
-  This software is licensed under a 3-clause BSD license.  Please
-  consult LICENSE file distributed with the sources of this project regarding
-  your rights to use or distribute this software.
-*/
+// Copyright (c) 2018, Sylabs Inc. All rights reserved.
+// This software is licensed under a 3-clause BSD license. Please consult the
+// LICENSE file distributed with the sources of this project regarding your
+// rights to use or distribute this software.
 
 // Package sylog implements a basic logger for Singularity Go code to log
 // messages in the same format as singularity_message() from C code
--- src/pkg/workflows/oci/config/linux.go
+++ src/pkg/workflows/oci/config/linux.go
@@ -1,12 +1,9 @@
 // +build linux
 
-/*
-  Copyright (c) 2018, Sylabs, Inc. All rights reserved.
-
-  This software is licensed under a 3-clause BSD license.  Please
-  consult LICENSE file distributed with the sources of this project regarding
-  your rights to use or distribute this software.
-*/
+// Copyright (c) 2018, Sylabs Inc. All rights reserved.
+// This software is licensed under a 3-clause BSD license. Please consult the
+// LICENSE file distributed with the sources of this project regarding your
+// rights to use or distribute this software.
 
 package config
 
