9624559be025fcfce22f64355885df159039d71a
Michael Bauer <bauerm@umich.edu>
2018-05-25 11:20:06 -0600

    Remove code graveyard from builder_sif.go

--- src/pkg/build/builder_sif.go
+++ src/pkg/build/builder_sif.go
@@ -39,19 +39,6 @@ func NewSIFBuilderJSON(imagePath string, r io.Reader) (b *SIFBuilder, err error)
 	}
 
 	return NewSIFBuilder(imagePath, d)
-
-	/*
-		b.Def = d
-		b.ImagePath = imagePath
-		b.tmpfs, err = image.TempSandbox(path.Base(imagePath) + "-")
-		if err != nil {
-			return b, err
-		}
-
-		uri := d.Header["bootstrap"] + "://" + d.Header["from"]
-		b.p, err = NewProvisionerFromURI(uri)
-
-		return b, err*/
 }
 
 // NewSIFBuilder creates a new SIFBuilder from the supplied Definition struct
@@ -84,72 +71,3 @@ func (b *SIFBuilder) Build(ctx context.Context) (err error) {
 
 	return nil
 }
-
-/*
-func NewSifBuilder(imagePath string, d Definition) (b *SifBuilder, err error) {
-	r, w, _ := os.Pipe()
-	er, ew, _ := os.Pipe()
-
-	b = &SifBuilder{
-		Definition: d,
-		path:       imagePath,
-		Out:        io.MultiReader(er, r),
-		outWrite:   w,
-		errWrite:   ew,
-		outRead:    r,
-		errRead:    er,
-	}
-
-	b.tmpfs, err = image.TempSandbox(path.Base(imagePath) + "-")
-	if err != nil {
-		return b, err
-	}
-
-	uri := d.Header["bootstrap"] + "://" + d.Header["from"]
-	b.p, err = NewProvisionerFromURI(uri)
-
-	return b, err
-}*/
-
-/*
-// Build provisions a temporaly file system from a definition file
-// and build a SIF afterwards.
-func (b *SifBuilder) Build() (err error) {
-	oldstdout := os.Stdout
-	oldstderr := os.Stderr
-
-	os.Stdout = b.outWrite
-	os.Stderr = b.errWrite
-
-	defer func() {
-		os.Stdout = oldstdout
-		os.Stderr = oldstderr
-		//b.outRead.Close()
-		b.outWrite.Close()
-		//b.errRead.Close()
-		b.errWrite.Close()
-	}()
-
-	err := b.p.Provision(b.tmpfs)
-	if err != nil {
-		return err
-	}
-
-	img, err := image.SIFFromSandbox(b.tmpfs, b.path)
-	if err != nil {
-		return err
-	}
-
-	b.Image = img
-
-	return nil
-}
-
-func (b *SifBuilder) createSifFile() (err error) {
-	return nil
-}
-
-func (b *SifBuilder) mksquashfs() (err error) {
-	return nil
-}
-*/
