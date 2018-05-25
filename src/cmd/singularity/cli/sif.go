33c603aad4e2541a10ff17f01e01729d6e8031d7
ArangoGutierrez <eduardo@sylabs.io>
2018-05-25 17:02:13 -0500

    cobra long help

--- src/cmd/singularity/cli/sif.go
+++ src/cmd/singularity/cli/sif.go
@@ -24,6 +24,23 @@ sif create -P /tmp/fs.squash -f "SQUASHFS" -p "SYSTEM" -c "Linux" /tmp/container
 
 var sifCmd = &cobra.Command{
 	Use: "sif COMMAND OPTION FILE",
+	Long: `
+	create --  Create a new sif file with input data objects
+	del    id  Delete a specified set of descriptor+object
+	dump   id  Display data object content
+	header --  Display SIF header
+	info   id  Print data object descriptor info
+	list   --  List SIF data descriptors from an input SIF file
+
+
+	create options:
+        -D deffile : include definitions file 'deffile'
+        -E : include environment variables
+        -P partfile : include file system partition 'partfile'
+                -c CONTENT : freeform partition content string
+                -f FSTYPE : filesystem type: EXT3, SQUASHFS
+                -p PARTTYPE : filesystem partition type: SYSTEM, DATA, OVERLAY
+                -u uuid : pass a uuid to use instead of generating a new one`,
 	Run: func(cmd *cobra.Command, args []string) {
 		var sif = buildcfg.SBINDIR + "/sif"
 
