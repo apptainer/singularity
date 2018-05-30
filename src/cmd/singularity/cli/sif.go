8c24843e5af96851b6744f4ad2e22af29a9a212a
ArangoGutierrez <eduardo@sylabs.io>
2018-05-30 09:56:47 -0500

    fix unexported names

--- src/cmd/singularity/cli/sif.go
+++ src/cmd/singularity/cli/sif.go
@@ -142,7 +142,7 @@ var SifInfo = &cobra.Command{
 var SifDump = &cobra.Command{
 	Use:     docs.SifDumpUse,
 	Short:   docs.SifDumpShort,
-	Example: docs.sifInfoExample,
+	Example: docs.SifInfoExample,
 	Args:    cobra.MinimumNArgs(2),
 	Run: func(cmd *cobra.Command, args []string) {
 
@@ -164,7 +164,7 @@ var SifDump = &cobra.Command{
 var SifDel = &cobra.Command{
 	Use:     docs.SifDelUse,
 	Short:   docs.SifDelShort,
-	Example: docs.sifInfoExample,
+	Example: docs.SifInfoExample,
 	Args:    cobra.MinimumNArgs(2),
 	Run: func(cmd *cobra.Command, args []string) {
 
@@ -186,7 +186,7 @@ var SifDel = &cobra.Command{
 var SifHeader = &cobra.Command{
 	Use:     docs.SifHeaderUse,
 	Short:   docs.SifHeaderShort,
-	Example: docs.sifHeaderExample,
+	Example: docs.SifHeaderExample,
 	Args:    cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 
