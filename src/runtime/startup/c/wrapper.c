88db26d529ddb57a08520135fea14d72ab72cc02
Cedric Clerget <cedric.clerget@gmail.com>
2018-06-02 15:00:29 +0200

    Revert wrapper changes

--- src/runtime/startup/c/wrapper.c
+++ src/runtime/startup/c/wrapper.c
@@ -32,14 +32,6 @@
 #include <setjmp.h>
 #include <sys/syscall.h>
 
-#ifndef PR_SET_NO_NEW_PRIVS
-#define PR_SET_NO_NEW_PRIVS 38
-#endif
-
-#ifndef PR_GET_NO_NEW_PRIVS
-#define PR_GET_NO_NEW_PRIVS 39
-#endif
-
 #ifdef SINGULARITY_SECUREBITS
 #  include <linux/securebits.h>
 #else
@@ -495,11 +487,6 @@ __attribute__((constructor)) static void init(void) {
     char *runtime;
     int output[2];
 
-#ifndef SINGULARITY_NO_NEW_PRIVS
-    singularity_message(ERROR, "Host kernel is outdated and does not support PR_SET_NO_NEW_PRIVS!\n");
-    exit(1);
-#endif
-
     loglevel = getenv("SINGULARITY_MESSAGELEVEL");
     if ( loglevel != NULL ) {
         loglevel = strdup(loglevel);
@@ -537,6 +524,14 @@ __attribute__((constructor)) static void init(void) {
         setenv("SINGULARITY_MESSAGELEVEL", loglevel, 1);
         free(loglevel);
     }
+
+#ifdef SINGULARITY_NO_NEW_PRIVS
+    singularity_message(DEBUG, "PR_SET_NO_NEW_PRIVS supported\n");
+    config.hasNoNewPrivs = 1;
+#else
+    singularity_message(DEBUG, "PR_SET_NO_NEW_PRIVS not supported\n");
+    config.hasNoNewPrivs = 0;
+#endif
 
     /* read json configuration from stdin */
     singularity_message(DEBUG, "Read json configuration from stdin\n");
@@ -711,6 +706,12 @@ __attribute__((constructor)) static void init(void) {
         if ( socketpair(AF_UNIX, SOCK_STREAM, 0, stage_socket) < 0 ) {
             singularity_message(ERROR, "Failed to create communication socket: %s\n", strerror(errno));
             exit(1);
+        }
+
+        /* enforce PID namespace if NO_NEW_PRIVS not supported  */
+        if ( config.hasNoNewPrivs == 0 ) {
+            singularity_message(VERBOSE, "No PR_SET_NO_NEW_PRIVS support, enforcing PID namespace\n");
+            config.nsFlags |= CLONE_NEWPID;
         }
 
         if ( config.mntPid ) {
