64a96b7c3aefef56260f9ed3854142989c9552d5
ArangoGutierrez <eduardo@sylabs.io>
2018-06-14 10:26:47 -0500

    more printf formating

--- src/pkg/build/builder_remote.go
+++ src/pkg/build/builder_remote.go
@@ -102,8 +102,8 @@ func (rb *RemoteBuilder) Build(ctx context.Context) (err error) {
 	// If we're doing an detached build, print help on how to download the image
 	if rb.IsDetached {
 		fmt.Printf("Build submitted! Once it is complete, the image can be retrieved by running:\n\n")
-		fmt.Printf("\tsingularity pull --library-uri %v library://%v/build/image\n\n", rd.LibraryURL, rd.LibraryRef)
-		fmt.Printf("Alternatively, you can download it from a browser at %v/library/%v\n", rd.LibraryURL, rd.LibraryRef)
+		fmt.Printf("\tsingularity pull --library-uri %v %v\n\n", rd.LibraryURL, rd.LibraryRef)
+		fmt.Printf("Alternatively, you can download it from a browser at %v/%v/build/image\n", rd.LibraryURL, rd.ID.Hex())
 	}
 
 	// If we're doing an attached build, stream output and then download the resulting file
