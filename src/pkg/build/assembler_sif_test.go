60de1820963953e8151fa0f49d0323e9fca1b2b8
Ian <iankane@umich.edu>
2018-06-14 15:50:17 -0700

    Merge branch '3.0-pullFurnisher' into shub_dev

--- src/pkg/build/assembler_sif_test.go
+++ src/pkg/build/assembler_sif_test.go
@@ -1,39 +1,44 @@
 // Copyright (c) 2018, Sylabs Inc. All rights reserved.
 // This software is licensed under a 3-clause BSD license. Please consult the
-// LICENSE file distributed with the sources of this project regarding your
+// LICENSE file distributed with the URIs of this project regarding your
 // rights to use or distribute this software.
 
 package build
 
 import (
+	"os"
 	"testing"
-
-	"github.com/singularityware/singularity/src/pkg/sylog"
 )
 
-// TestChef sees if we can build a SIF image from a docker://ubuntu:18.04 based kitchen to /tmp
+const (
+	assemblerDockerURI  = "docker://alpine"
+	assemblerDockerDest = "/tmp/docker_alpine_assemble_test.sif"
+)
+
+// TestAssembler sees if we can build a SIF image from a docke based kitchen to /tmp
 func TestAssembler(t *testing.T) {
-	def, err := NewDefinitionFromURI("docker://ubuntu:18.04")
+	def, err := NewDefinitionFromURI(assemblerDockerURI)
 	if err != nil {
-		sylog.Fatalf("unable to parse URI docker://ubuntu:18.04: %v\n", err)
+		t.Fatalf("unable to parse URI %s: %v\n", assemblerDockerURI, err)
 	}
 
 	dcp := &DockerConveyorPacker{}
 
 	if err := dcp.Get(def); err != nil {
-		t.Fatal("failed to pull:", err)
+		t.Fatalf("failed to Get from %s: %v\n", assemblerDockerURI, err)
 	}
 
 	b, err := dcp.Pack()
-
 	if err != nil {
-		t.Fatal("failed to furnish:", err)
+		t.Fatalf("failed to Pack from %s: %v\n", assemblerDockerURI, err)
 	}
 
 	a := &SIFAssembler{}
 
-	err = a.Assemble(b, "/tmp/docker_assemble_test.sif")
+	err = a.Assemble(b, assemblerDockerDest)
 	if err != nil {
-		t.Fatal("Assembler failed to assemble:", err)
+		t.Fatalf("failed to assemble from %s: %v\n", assemblerDockerURI, err)
 	}
+
+	defer os.Remove(assemblerDockerDest)
 }
