c867a735623067d13e4cf33367fb8765ee1b5608
Michael Bauer <bauerm@umich.edu>
2018-06-20 23:24:18 -0700

    Engine runs SIF file with .singularity.d as DATA partition

--- src/runtime/engines/singularity/create.go
+++ src/runtime/engines/singularity/create.go
@@ -6,9 +6,36 @@
 package singularity
 
 /*
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 #include <unistd.h>
-#include "image/image.h"
+#include <uuid/uuid.h>
+
 #include "util/config_parser.h"
+
+#include "image/image.h"
+#include "util/message.h"
+#include "sif/list.h"
+#include "sif/sif.h"
+#include "sif/sifaccess.h"
+
+// TODO return struct Sifpartition instead of Sifcommon
+Sifcommon sif_getpart(struct image_object *image, int id) {
+	Sifinfo sif;
+	//Sifpartition *partdesc;
+
+	if (sif_load(image->path, &sif, 1) < 0) {
+		singularity_message(ERROR, "Unable to open sif\n");
+	}
+
+	Sifdescriptor *desc = sif_getdescid(&sif, id);
+
+	return desc->cm;
+}
+
 */
 // #cgo CFLAGS: -I../../c/lib
 // #cgo LDFLAGS: -L../../../../builddir/lib -lruntime -luuid
@@ -28,6 +55,14 @@ import (
 	"github.com/singularityware/singularity/src/pkg/util/loop"
 	"github.com/singularityware/singularity/src/runtime/engines/singularity/rpc/client"
 )
+
+// loopDevices is a mapping of loop device id -> associated dest in image. The
+// dest should be given relative to the image root
+var loopDevices map[int]string
+
+func init() {
+	loopDevices = make(map[int]string)
+}
 
 // CreateContainer creates a container
 func (engine *EngineOperations) CreateContainer(rpcConn net.Conn) error {
@@ -82,6 +117,7 @@ func (engine *EngineOperations) CreateContainer(rpcConn net.Conn) error {
 		info.SizeLimit = uint64(C.uint(imageObject.size))
 	}
 
+	// Mount SIF default system partition
 	if st.IsDir() == false && !userNS {
 		var number int
 		info.Flags = loop.FlagsAutoClear
@@ -155,6 +191,17 @@ func (engine *EngineOperations) CreateContainer(rpcConn net.Conn) error {
 		return fmt.Errorf("mount /etc/group failed: %s", err)
 	}
 
+	// Mount SINGULARITY.D partition
+	id, err := loopSifPartition(rpcOps, rootfs, imageObject, 2)
+	if err != nil {
+		return fmt.Errorf("mount filesystem failed: %v", err)
+	}
+
+	loopDevices[id] = ".singularity.d"
+	if err := mountLoops(rpcOps, loopDevices); err != nil {
+		return fmt.Errorf("loop mounts failed: %v", err)
+	}
+
 	sylog.Debugf("Mounting staging dir %s into final dir %s\n", buildcfg.CONTAINER_FINALDIR, buildcfg.SESSIONDIR)
 	_, err = rpcOps.Mount(buildcfg.CONTAINER_FINALDIR, buildcfg.SESSIONDIR, "", syscall.MS_BIND|syscall.MS_REC, "")
 	if err != nil {
@@ -180,6 +227,53 @@ func (engine *EngineOperations) CreateContainer(rpcConn net.Conn) error {
 	}
 	if err := rpcOps.Client.Close(); err != nil {
 		return fmt.Errorf("can't close connection with rpc server: %s", err)
+	}
+
+	return nil
+}
+
+/* Most of the following work will be able to be replace by PR #1625 once that's merged */
+
+// loopSifPartition opens a loop device and mounts the partition described
+// by id of the SIF file at imageObject->path [c struct]
+func loopSifPartition(rpcOps *client.RPC, rootfs string, imageObject C.struct_image_object, id int) (int, error) {
+	info := new(loop.Info64)
+
+	// get sif common struct from sif[id] partition
+	sifCommon := C.sif_getpart(&imageObject, C.int(id))
+
+	info.Offset = uint64(C.uint(sifCommon.fileoff))
+	info.SizeLimit = uint64(C.uint(sifCommon.filelen))
+	info.Flags = loop.FlagsAutoClear
+
+	// ask RPC server to open loop device
+	return rpcOps.LoopDevice(rootfs, os.O_RDONLY, *info)
+}
+
+// mountSifLoop takes a loop dev, destination path (in img), mount type, and flags; and
+// performs the mount operation
+func mountSifLoop(rpcOps *client.RPC, id int, rdest, mountType string, flags uintptr) error {
+	path := fmt.Sprintf("/dev/loop%d", id)
+	sylog.Debugf("Mounting loop device %s\n", path)
+
+	dest := filepath.Join(buildcfg.CONTAINER_FINALDIR, rdest)
+
+	_, err := rpcOps.Mount(path, dest, mountType, flags, "errors=remount-ro")
+	if err != nil {
+		return fmt.Errorf("failed to mount %s filesystem: %s", mountType, err)
+	}
+
+	return nil
+}
+
+// mountLoops takes the map of loop devices and mounts at their respective destinations
+func mountLoops(rpcOps *client.RPC, loops map[int]string) error {
+	mountType := "squashfs" //assuming squashfs partition for now
+	var mountFlags uintptr = syscall.MS_NOSUID | syscall.MS_RDONLY | syscall.MS_NODEV
+	for id, dest := range loops {
+		if err := mountSifLoop(rpcOps, id, dest, mountType, mountFlags); err != nil {
+			return err
+		}
 	}
 
 	return nil
