11d4d88e3e86345464340eec27edc0390422f5b6
Ian <iankane@umich.edu>
2018-06-21 12:40:20 -0700

    restructured some code, build fails to make valid images though

---
+++ src/pkg/build/conveyorPacker_local.go
@@ -0,0 +1,104 @@
+// Copyright (c) 2018, Sylabs Inc. All rights reserved.
+// Copyright (c) 2018, Vanessa Sochat. All rights reserved.
+// This software is licensed under a 3-clause BSD license. Please consult the
+// LICENSE file distributed with the sources of this project regarding your
+// rights to use or distribute this software.
+
+package build
+
+/*
+#include <unistd.h>
+#include "image/image.h"
+#include "util/config_parser.h"
+*/
+// #cgo CFLAGS: -I../../runtime/c/lib
+// #cgo LDFLAGS: -L../../../builddir/lib -lruntime -luuid
+import "C"
+import (
+	"io/ioutil"
+	"log"
+
+	"github.com/singularityware/singularity/src/pkg/sylog"
+	"github.com/singularityware/singularity/src/pkg/util/loop"
+)
+
+// LocalConveyor only needs to hold the conveyor to have the needed data to pack
+type LocalConveyor struct {
+	src string
+}
+
+// LocalConveyorPacker only needs to hold the conveyor to have the needed data to pack
+type LocalConveyorPacker struct {
+	LocalConveyor
+
+	tmpfs string
+}
+
+// Get just stores the source
+func (c *LocalConveyor) Get(recipe Definition) (err error) {
+	c.src = recipe.Header["from"]
+	return nil
+}
+
+// Pack puts relevant objects in a Bundle!
+func (cp *LocalConveyorPacker) Pack() (b *Bundle, err error) {
+
+	cp.tmpfs, err = ioutil.TempDir("", "temp-local-")
+	if err != nil {
+		return
+	}
+
+	b, err = NewBundle()
+
+	err = cp.unpackTmpfs(b)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	b.Recipe = Definition{}
+
+	return b, nil
+}
+
+func (cp *LocalConveyorPacker) unpackTmpfs(b *Bundle) (err error) {
+	var p Packer
+
+	rootfs := cp.src
+
+	//leverage C code to properly mount squashfs image
+	C.singularity_config_init()
+
+	imageObject := C.singularity_image_init(C.CString(rootfs), 0)
+
+	info := new(loop.Info64)
+
+	switch C.singularity_image_type(&imageObject) {
+	case 1:
+		//squashfs
+		info.Offset = uint64(C.uint(imageObject.offset))
+		info.SizeLimit = uint64(C.uint(imageObject.size))
+
+		p = &SquashfsPacker{
+			srcfile: rootfs,
+			tmpfs:   cp.tmpfs,
+			info:    info,
+		}
+	case 2:
+		//ext3
+		info.Offset = uint64(C.uint(imageObject.offset))
+		info.SizeLimit = uint64(C.uint(imageObject.size))
+
+		p = &Ext3Packer{
+			srcfile: rootfs,
+			tmpfs:   cp.tmpfs,
+			info:    info,
+		}
+	default:
+		sylog.Fatalf("Invalid image format from shub")
+	}
+
+	b, err = p.Pack()
+
+	return err
+}
---
+++ src/pkg/build/packer_squashfs.go
@@ -0,0 +1,71 @@
+// Copyright (c) 2018, Sylabs Inc. All rights reserved.
+// Copyright (c) 2018, Vanessa Sochat. All rights reserved.
+// This software is licensed under a 3-clause BSD license. Please consult the
+// LICENSE file distributed with the sources of this project regarding your
+// rights to use or distribute this software.
+
+package build
+
+/*
+#include <unistd.h>
+#include "image/image.h"
+#include "util/config_parser.h"
+*/
+// #cgo CFLAGS: -I../../runtime/c/lib
+// #cgo LDFLAGS: -L../../../builddir/lib -lruntime -luuid
+import "C"
+
+import (
+	"io/ioutil"
+	"os/exec"
+	"strconv"
+
+	"github.com/singularityware/singularity/src/pkg/sylog"
+	"github.com/singularityware/singularity/src/pkg/util/loop"
+)
+
+// SquashfsPacker only needs to hold the conveyor to have the needed data to pack
+type SquashfsPacker struct {
+	srcfile string
+	tmpfs   string
+	info    *loop.Info64
+}
+
+// Pack puts relevant objects in a Bundle!
+func (p *SquashfsPacker) Pack() (b *Bundle, err error) {
+	rootfs := p.srcfile
+
+	b, err = NewBundle()
+	if err != nil {
+		return
+	}
+	err = p.unpackSquashfs(b, p.info, rootfs)
+	if err != nil {
+		sylog.Fatalf("unpackSquashfs Failed", err.Error())
+		return nil, err
+	}
+
+	return
+}
+
+// unpackSquashfs removes the image header with dd and then unpackes image into bundle directories with unsquashfs
+func (p *SquashfsPacker) unpackSquashfs(b *Bundle, info *loop.Info64, rootfs string) (err error) {
+	trimfile, err := ioutil.TempFile(p.tmpfs, "trim.squashfs")
+	//trim header
+	cmd := exec.Command("dd", "bs="+strconv.Itoa(int(info.Offset)), "skip=1", "if="+rootfs, "of="+trimfile.Name())
+	err = cmd.Run()
+	if err != nil {
+		sylog.Errorf("dd Failed", err.Error())
+		return err
+	}
+
+	//copy filesystem into bundle rootfs
+	cmd = exec.Command("unsquashfs", "-f", "-d", b.Rootfs(), trimfile.Name())
+	err = cmd.Run()
+	if err != nil {
+		sylog.Errorf("unsquashfs Failed", err.Error())
+		return err
+	}
+
+	return err
+}
