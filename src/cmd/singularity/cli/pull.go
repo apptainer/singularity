c759419eb945c57b0f34bd5e4fce47d5005da5e6
ArangoGutierrez <eduardo@sylabs.io>
2018-08-16 12:46:29 -0500

    WARN about shub not supported YET

--- src/cmd/singularity/cli/pull.go
+++ src/cmd/singularity/cli/pull.go
@@ -6,9 +6,21 @@
 package cli
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/singularityware/singularity/src/docs"
 	"github.com/singularityware/singularity/src/pkg/libexec"
 	"github.com/spf13/cobra"
+)
+
+const (
+	// SyCloudLibrary holds sylabs cloud library base URI
+	// for more info refer to https://cloud.sylabs.io/library
+	SyCloudLibrary = "library"
+	// Shub holds singularity hub base URI
+	// for more info refer to https://singularity-hub.org/
+	Shub = "shub"
 )
 
 var (
@@ -31,11 +43,25 @@ var PullCmd = &cobra.Command{
 	Args:   cobra.RangeArgs(1, 2),
 	PreRun: sylabsToken,
 	Run: func(cmd *cobra.Command, args []string) {
+		var uri string
 		if len(args) == 2 {
-			libexec.PullImage(args[0], args[1], PullLibraryURI, force, authToken)
-			return
+			uri = args[1]
+		} else {
+			uri = args[0]
 		}
-		libexec.PullImage("", args[0], PullLibraryURI, force, authToken)
+		BaseURI := strings.Split(uri, "://")
+		switch BaseURI[0] {
+		case SyCloudLibrary:
+			if len(args) == 2 {
+				libexec.PullImage(args[0], uri, PullLibraryURI, force, authToken)
+				return
+			}
+			libexec.PullImage("", args[0], PullLibraryURI, force, authToken)
+		case Shub:
+			fmt.Println("Shub not yet supported")
+		default:
+			fmt.Println(BaseURI[0], "Not a supported URI")
+		}
 	},
 
 	Use:     docs.PullUse,
