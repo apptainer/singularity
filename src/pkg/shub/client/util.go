710ed8000909545236c0dfb3c88c36cfc24f424c
ArangoGutierrez <eduardo@sylabs.io>
2018-08-23 16:16:40 -0500

    split shubParseReference into 2 funcs

--- src/pkg/shub/client/util.go
+++ src/pkg/shub/client/util.go
@@ -6,75 +6,77 @@
 package client
 
 import (
-	"fmt"
+	"errors"
 	"regexp"
 	"strings"
 )
 
-// ShubParseReference accepts a Shub reference string and parses its content
-// It will return an error if the given URI is not valid,
-// otherwise it will parse the contents into a ShubURI struct
-func ShubParseReference(src string) (uri ShubURI, err error) {
-
+// isShubPullRef returns true if the provided string is a valid Shub
+// reference for a pull operation.
+func isShubPullRef(shubRef string) bool {
 	//define regex for each URI component
-	registryRegexp := `([-a-zA-Z0-9/]{1,64}\/)?` //target is very open, outside registry
-	nameRegexp := `([-a-zA-Z0-9]{1,39}\/)`       //target valid github usernames
-	containerRegexp := `([-_.a-zA-Z0-9]{1,64})`  //target valid github repo names
-	tagRegexp := `(:[-_.a-zA-Z0-9]{1,64})?`      //target is very open, file extensions or branch names
-	digestRegexp := `(\@[a-f0-9]{32})?`          //target md5 sum hash
+	registryRegexp := `([-.a-zA-Z0-9/]{1,64}\/)?` //target is very open, outside registry
+	nameRegexp := `([-a-zA-Z0-9]{1,39}\/)`        //target valid github usernames
+	containerRegexp := `([-_.a-zA-Z0-9]{1,64})`   //target valid github repo names
+	tagRegexp := `(:[-_.a-zA-Z0-9]{1,64})?`       //target is very open, file extensions or branch names
+	digestRegexp := `(\@[a-f0-9]{32})?`           //target file md5 has, git commit hash, git branch
 
 	//expression is anchored
-	shubRegex, err := regexp.Compile(`^\/\/` + registryRegexp + nameRegexp + containerRegexp + tagRegexp + digestRegexp + `$`)
+	shubRegex, err := regexp.Compile(`^(shub://)` + registryRegexp + nameRegexp + containerRegexp + tagRegexp + digestRegexp + `$`)
 	if err != nil {
-		return uri, err
+		return false
 	}
 
-	found := shubRegex.FindString(src)
+	found := shubRegex.FindString(shubRef)
 
 	//sanity check
 	//if found string is not equal to the input, input isn't a valid URI
-	if strings.Compare(src, found) != 0 {
-		return uri, fmt.Errorf("Source string is not a valid URI: %s", src)
+	if strings.Compare(shubRef, found) != 0 {
+		return false
 	}
 
-	//strip `//` from start of src
-	src = src[2:]
+	return true
+}
 
-	pieces := strings.SplitAfterN(src, `/`, -1)
-	if l := len(pieces); l > 2 {
+// shubParseReference accepts a valid Shub reference string and parses its content
+// It will return an error if the given URI is not valid,
+// otherwise it will parse the contents into a ShubURI struct
+func shubParseReference(src string) (uri ShubURI, err error) {
+	ShubRef := strings.TrimPrefix(src, "shub://")
+	refParts := strings.Split(ShubRef, "/")
+
+	if l := len(refParts); l > 2 {
 		//more than two pieces indicates a custom registry
 		uri.defaultReg = false
-		uri.registry = strings.Join(pieces[:l-2], "")
-		uri.user = pieces[l-2]
-		src = pieces[l-1]
+		uri.registry = strings.Join(refParts[:l-2], "") + shubAPIRoute
+		uri.user = refParts[l-2]
+		src = refParts[l-1]
 	} else if l == 2 {
 		//two pieces means default registry
 		uri.defaultReg = true
 		uri.registry = defaultRegistry
-		uri.user = pieces[l-2]
-		src = pieces[l-1]
+		uri.user = refParts[l-2]
+		src = refParts[l-1]
+	} else if l < 2 {
+		return ShubURI{}, errors.New("Not a valid Shub reference")
 	}
 
 	//look for an @ and split if it exists
 	if strings.Contains(src, `@`) {
-		pieces = strings.Split(src, `@`)
-		uri.digest = `@` + pieces[1]
-		src = pieces[0]
+		refParts = strings.Split(src, `@`)
+		uri.digest = `@` + refParts[1]
+		src = refParts[0]
 	}
 
 	//look for a : and split if it exists
 	if strings.Contains(src, `:`) {
-		pieces = strings.Split(src, `:`)
-		uri.tag = `:` + pieces[1]
-		src = pieces[0]
+		refParts = strings.Split(src, `:`)
+		uri.tag = `:` + refParts[1]
+		src = refParts[0]
 	}
 
 	//container name is left over after other parts are split from it
 	uri.container = src
 
 	return uri, nil
-}
-
-func (s *ShubURI) String() string {
-	return s.registry + s.user + s.container + s.tag + s.digest
 }
