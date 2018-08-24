172600cd597ffe7405a1ae3758e1e77bc6608c09
ArangoGutierrez <eduardo@sylabs.io>
2018-08-24 09:15:58 -0500

    Img -> Image on function names

--- src/pkg/libexec/pull.go
+++ src/pkg/libexec/pull.go
@@ -1,6 +1,6 @@
 // Copyright (c) 2018, Sylabs Inc. All rights reserved.
 // This software is licensed under a 3-clause BSD license. Please consult the
-// LICENSE file distributed with the sources of this project regarding your
+// LICENSE.md file distributed with the sources of this project regarding your
 // rights to use or distribute this software.
 
 package libexec
@@ -11,16 +11,16 @@ import (
 	"github.com/singularityware/singularity/src/pkg/sylog"
 )
 
-// PullLibraryImg is the function that is responsible for pulling an image from a Sylabs library.
-func PullLibraryImg(image string, libraryRef string, libraryURL string, force bool, authToken string) {
+// PullLibraryImage is the function that is responsible for pulling an image from a Sylabs library.
+func PullLibraryImage(image string, libraryRef string, libraryURL string, force bool, authToken string) {
 	err := library.DownloadImage(image, libraryRef, libraryURL, force, authToken)
 	if err != nil {
 		sylog.Fatalf("%v\n", err)
 	}
 }
 
-// PullShubImg is the function that is responsible for pulling an image from a Singularity Hub.
-func PullShubImg(filePath string, shubRef string, force bool) {
+// PullShubImage is the function that is responsible for pulling an image from a Singularity Hub.
+func PullShubImage(filePath string, shubRef string, force bool) {
 	err := shub.DownloadImage(filePath, shubRef, force)
 	if err != nil {
 		sylog.Fatalf("%v\n", err)
