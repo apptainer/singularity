508ae0ac561f267c45b7c9ee14f2ad329efd1211
ArangoGutierrez <eduardo@sylabs.io>
2018-09-04 17:56:49 -0500

    Create a CleanEnv func under util pkg
    Func CleanEnv cleans environment variables before running the container
    it recieves  a pointer to the generator, the host environment and
    the --cleanenv flag value.

---
+++ src/pkg/util/env/clean.go
@@ -0,0 +1,41 @@
+// Copyright (c) 2018, Sylabs Inc. All rights reserved.
+// This software is licensed under a 3-clause BSD license. Please consult the
+// LICENSE.md file distributed with the sources of this project regarding your
+// rights to use or distribute this software.
+
+package env
+
+import (
+	"strings"
+
+	"github.com/opencontainers/runtime-tools/generate"
+	"github.com/singularityware/singularity/src/pkg/sylog"
+)
+
+// CleanEnv cleans environment variables before running the container
+func CleanEnv(g *generate.Generator, IsCleanEnv bool, environment []string) {
+
+	for _, env := range environment {
+		e := strings.SplitN(env, "=", 2)
+		if len(e) != 2 {
+			sylog.Verbosef("can't process environment variable %s", env)
+			continue
+		}
+
+		// Transpose environment
+		if strings.HasPrefix(e[0], "SINGULARITYENV_") {
+			e[0] = strings.TrimPrefix(e[0], "SINGULARITYENV_")
+		} else if IsCleanEnv && (e[0] != "HOME" &&
+			e[0] != "TERM" &&
+			e[0] != "LANG" &&
+			e[0] != "http_proxy" &&
+			e[0] != "https_proxy" &&
+			e[0] != "no_proxy" &&
+			e[0] != "all_proxy" &&
+			e[0] != "ftp_proxy") {
+			continue
+		}
+
+		g.AddProcessEnv(e[0], e[1])
+	}
+}
