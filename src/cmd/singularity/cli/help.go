61c3fd36b57bc694e743f3fb7c65f779f1faceea
ArangoGutierrez <eduardo@sylabs.io>
2018-09-13 17:13:16 -0500

    singularity help my_container.sif display help for container if available
    Help should be stated on the definition file
    ```
    Bootstrap: docker
    From: busybox
    
    %help
        Some help for this container
    ```

---
+++ src/cmd/singularity/cli/help.go
@@ -0,0 +1,46 @@
+// Copyright (c) 2018, Sylabs Inc. All rights reserved.
+// This software is licensed under a 3-clause BSD license. Please consult the
+// LICENSE.md file distributed with the sources of this project regarding your
+// rights to use or distribute this software.
+
+package cli
+
+import (
+	"os"
+
+	"github.com/singularityware/singularity/src/docs"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	HelpCmd.Flags().SetInterspersed(false)
+	SingularityCmd.SetHelpCommand(HelpCmd)
+
+	SingularityCmd.AddCommand(HelpCmd)
+}
+
+// HelpCmd singularity help
+var HelpCmd = &cobra.Command{
+	DisableFlagsInUseLine: true,
+	Args:   cobra.ExactArgs(1),
+	PreRun: sylabsToken,
+	Run: func(cmd *cobra.Command, args []string) {
+		c, _, e := cmd.Root().Find(args)
+		if _, err := os.Stat(args[0]); err == nil {
+			// Help prints (if set) the sourced %help section on the definition file
+			a := []string{"/bin/cat", "/.singularity.d/runscript.help"}
+			execStarter(cmd, args[0], a, "")
+		} else if c == nil || e != nil {
+			c.Printf("Unknown help topic %#q\n", args)
+			c.Root().Usage()
+		} else {
+			c.InitDefaultHelpFlag() // make possible 'help' flag to be shown
+			c.Help()
+		}
+	},
+
+	Use:     docs.HelpUse,
+	Short:   docs.HelpShort,
+	Long:    docs.HelpLong,
+	Example: docs.HelpExample,
+}
