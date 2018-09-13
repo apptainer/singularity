b359d14c756a921ea0a791baf0d59a68e4743159
ArangoGutierrez <eduardo@sylabs.io>
2018-09-13 17:16:18 -0500

    LICENCE => LICENSE.md

--- src/cmd/singularity/cli/keys_push.go
+++ src/cmd/singularity/cli/keys_push.go
@@ -1,12 +1,13 @@
 // Copyright (c) 2018, Sylabs Inc. All rights reserved.
 // This software is licensed under a 3-clause BSD license. Please consult the
-// LICENSE file distributed with the sources of this project regarding your
+// LICENSE.md file distributed with the sources of this project regarding your
 // rights to use or distribute this software.
 
 package cli
 
 import (
 	"fmt"
+
 	"github.com/singularityware/singularity/src/docs"
 	"github.com/singularityware/singularity/src/pkg/sylog"
 	"github.com/singularityware/singularity/src/pkg/sypgp"
--- src/cmd/singularity/cli/verify.go
+++ src/cmd/singularity/cli/verify.go
@@ -1,7 +1,7 @@
-//   Copyright (c) 2018, Sylabs, Inc. All rights reserved.
-//   This software is licensed under a 3-clause BSD license.  Please
-//   consult LICENSE.md file distributed with the sources of this project regarding
-//   your rights to use or distribute this software.
+// Copyright (c) 2018, Sylabs Inc. All rights reserved.
+// This software is licensed under a 3-clause BSD license. Please consult the
+// LICENSE.md file distributed with the sources of this project regarding your
+// rights to use or distribute this software.
 
 package cli
 
