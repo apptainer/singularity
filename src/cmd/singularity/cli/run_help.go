27eb1a55457c496409b3e137c16cc858f6e67312
ArangoGutierrez <eduardo@sylabs.io>
2018-09-15 06:42:48 -0500

    better err handling
    Instead of having the entire Run section wrapped in a big "if"
    do a better err check and the the rest of the code:
    
    ```
    if _, err := os.Stat(args[0]); err != nil {
            sylog.Fatalf("container not found: %s", err)
    	}
    	<rest of code>
    ```

--- src/cmd/singularity/cli/run_help.go
+++ src/cmd/singularity/cli/run_help.go
@@ -33,41 +33,41 @@ var RunHelpCmd = &cobra.Command{
 	PreRun:                sylabsToken,
 	Args:                  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		if _, err := os.Stat(args[0]); err == nil {
-			// Help prints (if set) the sourced %help section on the definition file
-			abspath, err := filepath.Abs(args[0])
-			name := filepath.Base(abspath)
-			a := []string{"/bin/cat", "/.singularity.d/runscript.help"}
-			starter := buildcfg.SBINDIR + "/starter-suid"
-			procname := "Singularity help"
-			Env := []string{sylog.GetEnvVar(), "SRUNTIME=singularity"}
-
-			engineConfig := singularity.NewConfig()
-			ociConfig := &oci.Config{}
-			generator := generate.Generator{Config: &ociConfig.Spec}
-			engineConfig.OciConfig = ociConfig
-
-			generator.SetProcessArgs(a)
-			engineConfig.SetImage(abspath)
-
-			cfg := &config.Common{
-				EngineName:   singularity.Name,
-				ContainerID:  name,
-				EngineConfig: engineConfig,
-			}
-
-			configData, err := json.Marshal(cfg)
-			if err != nil {
-				sylog.Fatalf("CLI Failed to marshal CommonEngineConfig: %s\n", err)
-			}
-
-			if err := exec.Pipe(starter, []string{procname}, Env, configData); err != nil {
-				sylog.Fatalf("%s", err)
-			}
-		} else {
+		// Sanity check
+		if _, err := os.Stat(args[0]); err != nil {
 			sylog.Fatalf("container not found: %s", err)
 		}
 
+		// Help prints (if set) the sourced %help section on the definition file
+		abspath, err := filepath.Abs(args[0])
+		name := filepath.Base(abspath)
+		a := []string{"/bin/cat", "/.singularity.d/runscript.help"}
+		starter := buildcfg.SBINDIR + "/starter-suid"
+		procname := "Singularity help"
+		Env := []string{sylog.GetEnvVar(), "SRUNTIME=singularity"}
+
+		engineConfig := singularity.NewConfig()
+		ociConfig := &oci.Config{}
+		generator := generate.Generator{Config: &ociConfig.Spec}
+		engineConfig.OciConfig = ociConfig
+
+		generator.SetProcessArgs(a)
+		engineConfig.SetImage(abspath)
+
+		cfg := &config.Common{
+			EngineName:   singularity.Name,
+			ContainerID:  name,
+			EngineConfig: engineConfig,
+		}
+
+		configData, err := json.Marshal(cfg)
+		if err != nil {
+			sylog.Fatalf("CLI Failed to marshal CommonEngineConfig: %s\n", err)
+		}
+
+		if err := exec.Pipe(starter, []string{procname}, Env, configData); err != nil {
+			sylog.Fatalf("%s", err)
+		}
 	},
 
 	Use:     docs.RunHelpUse,
