0c63883838c9210efa66eeb2a2179b983d81c6bb
Ian <iankane@umich.edu>
2018-09-20 00:16:37 -0400

    singularityware -> sylabs

--- src/cmd/singularity/cli/inspect.go
+++ src/cmd/singularity/cli/inspect.go
@@ -11,15 +11,15 @@ import (
 	"path/filepath"
 
 	"github.com/opencontainers/runtime-tools/generate"
-	"github.com/singularityware/singularity/src/docs"
-	"github.com/singularityware/singularity/src/pkg/buildcfg"
-	"github.com/singularityware/singularity/src/pkg/sylog"
-	"github.com/singularityware/singularity/src/pkg/util/exec"
 	"github.com/spf13/cobra"
+	"github.com/sylabs/singularity/src/docs"
+	"github.com/sylabs/singularity/src/pkg/buildcfg"
+	"github.com/sylabs/singularity/src/pkg/sylog"
+	"github.com/sylabs/singularity/src/pkg/util/exec"
 
-	"github.com/singularityware/singularity/src/runtime/engines/config"
-	"github.com/singularityware/singularity/src/runtime/engines/config/oci"
-	"github.com/singularityware/singularity/src/runtime/engines/singularity"
+	"github.com/sylabs/singularity/src/runtime/engines/config"
+	"github.com/sylabs/singularity/src/runtime/engines/config/oci"
+	"github.com/sylabs/singularity/src/runtime/engines/singularity"
 )
 
 var (
