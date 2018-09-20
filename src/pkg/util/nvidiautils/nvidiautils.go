a125945575847a56d825fac9d6aef1c39dece3ea
Carl Madison <carl@sylabs.io>
2018-09-20 14:49:01 -0600

    nvidia --nv refactor for using either nvidia-container-cli OR nvliblist.conf

--- src/pkg/util/nvidiautils/nvidiautils.go
+++ src/pkg/util/nvidiautils/nvidiautils.go
@@ -16,39 +16,85 @@ import (
 	"github.com/singularityware/singularity/src/pkg/sylog"
 )
 
+// generate bind list using nvidia-container-cli
 func nvidiaContainerCli() ([]string, []string, error) {
 	var strArray []string
 	var bindArray []string
-	var soID = regexp.MustCompile(".so")
 
 	// use nvidia-container-cli (if present)
 	command, err := exec.LookPath("nvidia-container-cli")
 	if err != nil {
 		return nil, nil, fmt.Errorf("no nvidia-container-cli present: %v\n", err)
 	}
-	cmd := exec.Command(command, "list", "--binaries", "--ipcs", "--libraries")
+
+	// process the binaries first
+	cmd := exec.Command(command, "list", "--binaries")
 	out, err := cmd.Output()
 	if err != nil {
 		return nil, nil, fmt.Errorf("Unable to execute nvidia-container-cli: %v\n", err)
 	}
-	for _, line := range strings.Split(string(out), "\n") {
-		val := soID.FindString(line) // this will disallow binaries (non .so files)
-		if val != "" {               // contains .so
-			if line != "" {
-				// extract the filename from the path
-				fileNames := strings.SplitAfter(line, "/")
-				fileName := fileNames[len(fileNames)-1]
 
-				testString := line + ":/.singularity.d/libs/" + fileName
-				bindArray = append(bindArray, testString)
-				strArray = append(strArray, fileName)
+	for _, line := range strings.Split(string(out), "\n") {
+		if line != "" {
+			// add this in to the bind list here so we don't need to special case it
+			// when the libraries are processed later in GetNvidiaBindPath
+			// (i.e. thiese will never show up in ldconfig output, hence they would not be
+			// added in later without adding a lot of special case code)
+			bindString := line + ":" + line
+			bindArray = append(bindArray, bindString)
+		}
+	}
+
+	cmd = exec.Command(command, "list", "--ipcs", "--libraries")
+	out, err = cmd.Output()
+	if err != nil {
+		return nil, nil, fmt.Errorf("Unable to execute nvidia-container-cli: %v\n", err)
+	}
+
+	for _, line := range strings.Split(string(out), "\n") {
+		if line != "" {
+
+			// strip off the filepath info as we are using
+			// the filepath returned by "ldconfig -p"
+			filePaths := strings.Split(line, "/")
+			fileName := ""
+			if len(filePaths) > 1 {
+				fileName = filePaths[len(filePaths)-1]
 			}
-		} else { // binary executable
-			bindArray = append(bindArray, line)
+
+			if strings.Contains(fileName, ".so") {
+				strArray = append(strArray, fileName) // add entry to list to be bound
+				// strip off .xxx.xx prefix and add so and so.1 entries as well
+				newentry := strings.SplitAfter(fileName, ".so")
+				strArray = append(strArray, newentry[0])      // add prefix (filepath.so)
+				strArray = append(strArray, newentry[0]+".1") // add prefix +".1" (filepath.so.1)
+			}
+		}
+	}
+	return strArray, bindArray, nil
+}
+
+// generate bind list using contents of nvliblist.conf
+func nvidiaLiblist(abspath string) ([]string, error) {
+	var strArray []string
+	var commentID = regexp.MustCompile(`^#`)
+
+	// grab the entries in nvliblist.conf file
+	file, err := os.Open(abspath + "/nvliblist.conf")
+	if err != nil {
+		return nil, fmt.Errorf("%v", err)
+	}
+
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		val := commentID.FindString(line)
+		if val == "" && line != "" {
 			strArray = append(strArray, line)
 		}
 	}
-	return bindArray, strArray, nil
+	return strArray, nil
 }
 
 // GetNvidiaBindPath returns a string array consisting of filepaths of nvidia
@@ -56,51 +102,40 @@ func nvidiaContainerCli() ([]string, []string, error) {
 func GetNvidiaBindPath(abspath string) ([]string, error) {
 	var strArray []string
 	var bindArray []string
-	var searchArray []string
-	var commentID = regexp.MustCompile(`^#`)
 
-	bindArray, strArray, err := nvidiaContainerCli()
+	// use nvidia-container-cli if presenet
+	strArray, bindArray, err := nvidiaContainerCli()
 	if err != nil {
-		sylog.Warningf("nvidia-container-cli returned: %v", err)
-	}
-	cliEntries := strings.Join(strArray, " ") // save away for later comparison check (disallow duplicates)
+		sylog.Warningf("nvidiaContainercli returned: %v", err)
 
-	// grab the entries in nvliblist.conf file
-	// use ldconfig to pattern match from ld.so.cache
-	file, err := os.Open(abspath + "/nvliblist.conf")
-	if err != nil {
-		sylog.Warningf("failed to open file %v:", err)
-	} else {
-		defer file.Close()
-		scanner := bufio.NewScanner(file)
-		for scanner.Scan() {
-			line := scanner.Text()
-			val := commentID.FindString(line)
-			if val == "" && line != "" {
-				searchArray = append(searchArray, line)
-			}
+		// nvidia-container-cli not present or errored out
+		// fallback is to use nvliblist.conf
+		strArray, err = nvidiaLiblist(abspath)
+		if err != nil {
+			sylog.Warningf("nvidiaLiblist returned: %v", err)
+			return nil, err
 		}
-
 	}
 
-	// walk thru the ldconfig output and add entries which contain the filenames located in
-	// the nvliblist.conf file (ldconfig filenames are full filepaths)
-	var searchFileName string
+	// walk thru the ldconfig output and add entries which contain the filenames
+	// returned by nvidia-container-cli OR the nvliblist.conf file contents
 	command, err := exec.LookPath("ldconfig")
 	if err != nil {
 		sylog.Warningf("ldconfig not found: %v", err)
-		return bindArray, nil
+		return nil, nil
 	}
+
 	cmd := exec.Command(command, "-p")
 	out, err := cmd.Output()
 	if err != nil {
 		sylog.Warningf("ldconfig execution error: %v", err)
-		return bindArray, nil
+		return nil, nil
 	}
+
 	lastadd := ""
 	for _, ldconfigOutputline := range strings.Split(strings.TrimSuffix(string(out), "\n"), "\n") {
 		if ldconfigOutputline != "" {
-			for _, nvidiaConfFileline := range searchArray {
+			for _, nvidiaFileName := range strArray {
 
 				// sample ldconfig -p output (ldconfigOutputline)
 				// 	libnvidia-ml.so.1 (libc6,x86-64) => /usr/lib64/nvidia/libnvidia-ml.so.1
@@ -111,29 +146,17 @@ func GetNvidiaBindPath(abspath string) ([]string, error) {
 
 					// ldconfigOutputSplitline[0] is the "libnvidia-ml.so[.1] (libc6,x86-64)"" (from the above example)
 					// ldconfigOutputSplitline[1] is the "/usr/lib64/nvidia/libnvidia-ml.so[.1]" (from the above example)
+					// these next 2 lines extract the "libnvdia-ml.so[.1]" (from the above example)
 
-					if !strings.Contains(cliEntries, ldconfigOutputSplitline[1]) { // skip if nvidia-container-cli found it
+					// ldconfigFileName is "libnvidia-ml.so[.1]" (from the above example)
+					ldconfigFileNames := strings.Split(ldconfigOutputSplitline[0], " ")
+					ldconfigFileName := strings.TrimSpace(string(ldconfigFileNames[0]))
 
-						// these 2 lines extract the "libnvdia-ml.so[.1]" (from the example above) - fileName
-						ldconfigFileNames := strings.Split(ldconfigOutputSplitline[0], " ")
-						ldconfigFileName := strings.TrimSpace(string(ldconfigFileNames[0]))
-
-						// this code block adds in foo.so.1 if there is a foo.so found in the config file
-						if strings.HasSuffix(ldconfigFileName, ".1") {
-							// remove the .1 from the search param (but will bind the actual name)
-							searchFileName = strings.TrimSuffix(ldconfigFileName, ".1")
-						} else {
-							searchFileName = ldconfigFileName
-						}
-
-						if searchFileName == nvidiaConfFileline {
-							if ldconfigFileName != lastadd { // add if not duplicate
-								// this is binding the actual name found above...
-								bindString := ldconfigOutputSplitline[1] + ":/.singularity.d/libs/" + ldconfigFileName
-								bindArray = append(bindArray, bindString)
-								lastadd = ldconfigFileName
-							}
-						}
+					if ldconfigFileName == nvidiaFileName && ldconfigFileName != lastadd { // add if not duplicate
+						// this is binding the actual name found above...
+						bindString := ldconfigOutputSplitline[1] + ":/.singularity.d/libs/" + ldconfigFileName
+						bindArray = append(bindArray, bindString)
+						lastadd = ldconfigFileName
 					}
 				}
 			}
