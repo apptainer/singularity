950229922218d323d2f0b14937277e85f04e1046
Ian <iankane@umich.edu>
2018-09-21 13:53:03 -0400

    code style changes

--- src/pkg/build/sources/conveyorPacker_zypper.go
+++ src/pkg/build/sources/conveyorPacker_zypper.go
@@ -32,25 +32,25 @@ type ZypperConveyorPacker struct {
 func (cp *ZypperConveyorPacker) Get(b *types.Bundle) (err error) {
 	cp.b = b
 
-	//check for zypper on system
+	// check for zypper on system
 	zypperPath, err := exec.LookPath("zypper")
 	if err != nil {
 		return fmt.Errorf("zypper is not in PATH: %v", err)
 	}
 
-	// //check for rpm on system
+	// check for rpm on system
 	err = rpmPathCheck()
 	if err != nil {
 		return
 	}
 
-	//get mirrorURL, OSVerison, and Includes components to definition
+	// get mirrorURL, OSVerison, and Includes components to definition
 	mirrorurl, ok := cp.b.Recipe.Header["mirrorurl"]
 	if !ok {
 		return fmt.Errorf("Invalid zypper header, no MirrorURL specified")
 	}
 
-	//look for an OS version if the mirror specifies it
+	// look for an OS version if the mirror specifies it
 	osversion := ""
 	if strings.Contains(mirrorurl, `%{OSVERSION}`) {
 		osversion, ok = cp.b.Recipe.Header["osversion"]
@@ -62,16 +62,16 @@ func (cp *ZypperConveyorPacker) Get(b *types.Bundle) (err error) {
 
 	include, _ := cp.b.Recipe.Header["include"]
 
-	//check for include environment variable and add it to requires string
+	// check for include environment variable and add it to requires string
 	include += ` ` + os.Getenv("INCLUDE")
 
-	//trim leading and trailing whitespace
+	// trim leading and trailing whitespace
 	include = strings.TrimSpace(include)
 
-	//add aaa_base to start of include list by default
+	// add aaa_base to start of include list by default
 	include = `aaa_base ` + include
 
-	//Create the main portion of zypper config
+	// Create the main portion of zypper config
 	err = cp.genZypperConfig()
 	if err != nil {
 		return fmt.Errorf("While generating Zypper config: %v", err)
@@ -82,7 +82,7 @@ func (cp *ZypperConveyorPacker) Get(b *types.Bundle) (err error) {
 		return fmt.Errorf("While copying pseudo devices: %v", err)
 	}
 
-	//Add mirrorURL as repo
+	// Add mirrorURL as repo
 	cmd := exec.Command(zypperPath, `--root`, cp.b.Rootfs(), `ar`, mirrorurl, `repo-oss`)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
@@ -90,7 +90,7 @@ func (cp *ZypperConveyorPacker) Get(b *types.Bundle) (err error) {
 		return fmt.Errorf("While adding zypper mirror: %v", err)
 	}
 
-	//Refreshing gpg keys
+	// Refreshing gpg keys
 	cmd = exec.Command(zypperPath, `--root`, cp.b.Rootfs(), `--gpg-auto-import-keys`, `refresh`)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
@@ -101,14 +101,14 @@ func (cp *ZypperConveyorPacker) Get(b *types.Bundle) (err error) {
 	args := []string{`--non-interactive`, `-c`, filepath.Join(cp.b.Rootfs(), zypperConf), `--root`, cp.b.Rootfs(), `--releasever=` + osversion, `-n`, `install`, `--auto-agree-with-licenses`}
 	args = append(args, strings.Fields(include)...)
 
-	//Zypper install command
+	// Zypper install command
 	cmd = exec.Command(zypperPath, args...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
 	sylog.Debugf("\n\tZypper Path: %s\n\tDetected Arch: %s\n\tOSVersion: %s\n\tMirrorURL: %s\n\tIncludes: %s\n", zypperPath, runtime.GOARCH, osversion, mirrorurl, include)
 
-	//run zypper
+	// run zypper
 	if err = cmd.Run(); err.Error() == "exit status 107" {
 		sylog.Errorf("*107* - *ZYPPER_EXIT_INF_RPM_SCRIPT_FAILED*::\n\t\tInstallation basically succeeded, but some of the packages %%post install scripts returned an error. These packages were successfully unpacked to disk and are registered in the rpm database, but due to the failed install script they may not work as expected. The failed scripts output might reveal what actually went wrong. Any scripts output is also logged to */var/log/zypp/history*.")
 	} else if err != nil {
@@ -120,7 +120,6 @@ func (cp *ZypperConveyorPacker) Get(b *types.Bundle) (err error) {
 
 // Pack puts relevant objects in a Bundle!
 func (cp *ZypperConveyorPacker) Pack() (b *types.Bundle, err error) {
-
 	err = cp.insertBaseEnv()
 	if err != nil {
 		return nil, fmt.Errorf("While inserting base environment: %v", err)
@@ -169,7 +168,6 @@ func (cp *ZypperConveyorPacker) insertRunScript() (err error) {
 }
 
 func (cp *ZypperConveyorPacker) genZypperConfig() (err error) {
-
 	err = os.MkdirAll(filepath.Join(cp.b.Rootfs(), "/etc/zypp"), 0775)
 	if err != nil {
 		return fmt.Errorf("While creating %v: %v", filepath.Join(cp.b.Rootfs(), "/etc/zypp"), err)
@@ -184,7 +182,6 @@ func (cp *ZypperConveyorPacker) genZypperConfig() (err error) {
 }
 
 func (cp *ZypperConveyorPacker) copyPseudoDevices() (err error) {
-
 	err = os.Mkdir(filepath.Join(cp.b.Rootfs(), "/dev"), 0775)
 	if err != nil {
 		return fmt.Errorf("While creating %v: %v", filepath.Join(cp.b.Rootfs(), "/dev"), err)
