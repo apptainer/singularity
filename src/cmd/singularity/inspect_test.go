7765f5683ebebe1fbd84b073575ce0ff2c71f9e0
Justin Cook <justin@sylabs.io>
2018-09-28 00:13:12 -0500

    Add a basic inspect test

--- src/cmd/singularity/inspect_test.go
+++ src/cmd/singularity/inspect_test.go
@@ -6,9 +6,30 @@
 package main
 
 import (
+	"os/exec"
 	"testing"
+
+	"github.com/sylabs/singularity/src/pkg/test"
 )
 
+func inspectImage(image string) ([]byte, error) {
+	args := []string{"inspect", image}
+	cmd := exec.Command(cmdPath, args...)
+
+	return cmd.CombinedOutput()
+}
+
 func TestInspect(t *testing.T) {
-	t.Skip("Not yet implemented")
+	var definition = "../../../examples/busybox/Singularity"
+	var image = "Default.sif"
+
+	if b, err := buildImage(definition, image); err != nil {
+		t.Fatalf("Error building an image from a definition: %v. Output follows.\n%s", err, string(b))
+	}
+
+	t.Run("InspectImage", test.WithoutPrivilege(func(t *testing.T) {
+		if b, err := inspectImage(image); err != nil {
+			t.Fatalf("Error inspecting an image: %v. Output follows.\n%s", err, string(b))
+		}
+	}))
 }
