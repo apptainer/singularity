d712c2957fdc2f5ae396e87f61bbcf40fa746a05
Dave Dykstra <2129743+DrDaveD@users.noreply.github.com>
2018-10-23 17:14:54 -0500

    read Version and Release from singularity.spec instead of recalculating defaults, make VERSION file non-empty

--- scripts/make-dist.sh
+++ scripts/make-dist.sh
@@ -4,9 +4,13 @@
 # in the LICENSE file.
 set -e
 
+# spec file needs to be at the root of the project
+cp dist/rpm/singularity.spec .
+git add singularity.spec
+
 package_name=singularity
-package_version=`(git describe --match 'v[0-9]*' --always 2>/dev/null || cat VERSION 2>/dev/null || echo "") | sed -e "s/^v//;s/-/_/g;s/_/-/;s/_/./g"`
-package_version_short=`(git describe --abbrev=0 --match 'v[0-9]*' --always 2>/dev/null || cat VERSION 2>/dev/null || echo "") | sed -e "s/^v//;s/-/_/g;s/_/-/;s/_/./g"`
+package_version_short="`sed -n 's/^Version: //p' singularity.spec`"
+tree_version="$package_version_short-`sed -n 's/^Release: \([^%]*\).*/\1/p' singularity.spec`"
 
 # Remove Dist tarball if it exists
 if [ -f $package_name-$package_version_short.tar.gz ]; then
@@ -16,9 +20,6 @@ fi
 echo " DIST setup VERSION: $tree_version"
 echo $tree_version > VERSION
 git add VERSION
-# spec file needs to be at the root of the project
-cp dist/rpm/singularity.spec .
-git add singularity.spec
 echo " DIST create tarball: $package_name-$package_version_short.tar.gz"
 git archive --format=tar --prefix=$package_name/ `git stash create` -o $package_name-$package_version_short.tar
 git reset VERSION singularity.spec || true
