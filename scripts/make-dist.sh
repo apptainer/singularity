b811abd9dcd2ed18e26c86de0f25bb0a7c312a5a
Dave Dykstra <2129743+DrDaveD@users.noreply.github.com>
2018-10-24 14:50:47 -0500

    use git ls-files instead of git-archive

--- scripts/make-dist.sh
+++ scripts/make-dist.sh
@@ -6,22 +6,16 @@ set -e
 
 # spec file needs to be at the root of the project
 cp dist/rpm/singularity.spec .
-git add singularity.spec
 
 package_name=singularity
 package_version_short="`sed -n 's/^Version: //p' singularity.spec`"
 tree_version="$package_version_short-`sed -n 's/^Release: \([^%]*\).*/\1/p' singularity.spec`"
 
-# Remove Dist tarball if it exists
-if [ -f $package_name-$package_version_short.tar.gz ]; then
-    rm -f $package_name-$package_version_short.tar.gz
-fi
-
 echo " DIST setup VERSION: $tree_version"
 echo $tree_version > VERSION
-git add VERSION
-echo " DIST create tarball: $package_name-$package_version_short.tar.gz"
-git archive --format=tar --prefix=$package_name/ `git stash create` -o $package_name-$package_version_short.tar
-git reset VERSION singularity.spec || true
-gzip $package_name-$package_version_short.tar
-
+tarball="$package_name-$package_version_short.tar.gz"
+echo " DIST create tarball: $tarball"
+rm -f $tarball
+(echo VERSION; echo singularity.spec; git ls-files) | \
+        tar -T - --xform "s,^,$package_name/," -czf $tarball
+rm -f VERSION singularity.spec
