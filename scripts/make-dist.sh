50b9ead30b36db51a13a33f3279f7417dbdd5ac5
Dave Dykstra <2129743+DrDaveD@users.noreply.github.com>
2018-10-29 15:33:09 -0500

    do not depend on GNU tar's --xform

--- scripts/make-dist.sh
+++ scripts/make-dist.sh
@@ -10,9 +10,16 @@ tree_version="$package_version_short-`sed -n 's/^Release: \([^%]*\).*/\1/p' sing
 
 echo " DIST setup VERSION: $tree_version"
 echo $tree_version > VERSION
+rmfiles="VERSION"
 tarball="$package_name-$package_version_short.tar.gz"
 echo " DIST create tarball: $tarball"
 rm -f $tarball
+pathtop="$package_name"
+thisdir="`basename $PWD`"
+if [ "$thisdir" != "$pathtop" ]; then
+    ln -fs $thisdir ../$pathtop
+    rmfiles="$rmfiles ../$pathtop"
+fi
+trap "rm -f $rmfiles" 0
 (echo VERSION; echo singularity.spec; git ls-files) | \
-        tar -T - --xform "s,^,$package_name/," -czf $tarball
-rm -f VERSION
+    sed "s,^,$pathtop/," | tar -C .. -T - -czf $tarball
