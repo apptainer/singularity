cc7d78cfada2f2661fb1a8eb35ff0cfeb51b9618
Michael Bauer <bauerm@umich.edu>
2018-12-05 12:11:21 -0700

    Add test plugin

---
+++ src/plugins/test-plugin/main.go
@@ -0,0 +1,51 @@
+// Copyright (c) 2018, Sylabs Inc. All rights reserved.
+// This software is licensed under a 3-clause BSD license. Please consult the
+// LICENSE.md file distributed with the URIs of this project regarding your
+// rights to use or distribute this software.
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	pluginapi "github.com/sylabs/singularity/pkg/plugin"
+)
+
+var Plugin = pluginapi.Plugin{
+	Manifest: pluginapi.Manifest{
+		Name:        "TestPlugin",
+		Author:      "Michael Bauer",
+		Version:     "0.0.1",
+		Description: "This is a short test plugin for Singularity",
+	},
+
+	Initializer: Impl,
+}
+
+type PluginImplementation struct {
+}
+
+var Impl = PluginImplementation{}
+
+func (p PluginImplementation) Init() {
+}
+
+func (p PluginImplementation) CommandAdd() []*cobra.Command {
+	ret := []*cobra.Command{}
+
+	ret = append(ret, &cobra.Command{
+		DisableFlagsInUseLine: true,
+		Args:                  cobra.MinimumNArgs(1),
+		Use:                   "plugin [args ...]",
+		Short:                 "Test test test",
+		Long:                  "Long test long test long test",
+		Example:               "singularity plugin my test",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("test-plugin is printing args:", args)
+		},
+		TraverseChildren: true,
+	})
+
+	return ret
+}
