1fd26cc7cf32061095e6dd548ca32f003cd9dac9
WestleyK <westley@sylabs.io>
2019-01-22 17:32:31 -0800

    Updated decimal point + range thrught Kb, Mb, & Gb

--- internal/pkg/cacheCli/cache_list.go
+++ internal/pkg/cacheCli/cache_list.go
@@ -7,6 +7,10 @@ import (
 	"strings"
 	"io/ioutil"
 	"os"
+//	"math"
+
+//	"strconv"
+//	"time"
 
 	"github.com/sylabs/singularity/internal/pkg/sylog"
 	"github.com/sylabs/singularity/internal/pkg/client/cache"
@@ -28,14 +32,29 @@ func visit(path string, f os.FileInfo, err error) error {
 	return nil
 }
 
+func find_size(size int64) string {
+	var size_f float64
+	if size <= 10000 {
+		size_f = float64(size) / 1000
+		return join(fmt.Sprintf("%.2f", size_f), " Kb")
+	} else if size <= 1000000000 {
+		size_f = float64(size) / 1000000
+		return join(fmt.Sprintf("%.2f", size_f), " Mb")
+	} else if size >= 1000000000 {
+		size_f = float64(size) / 1000000000
+		return join(fmt.Sprintf("%.2f", size_f), " Gb")
+	}
+	return "ERROR: failed to detect file size."
+}
+
 func ListSingularityCache() error {
 
-	fmt.Println("HELLO WORLD!!!!!!!!!")
+//	fmt.Println("HELLO WORLD!!!!!!!!!")
 	sylog.Debugf("Starting list...")
 
-	where := join(cache.Library(), "/")
+//	where := join(cache.Library(), "/")
 
-	fmt.Println("WHERE: ", where)
+//	fmt.Println("WHERE: ", where)
 
 
 
@@ -59,8 +78,26 @@ func ListSingularityCache() error {
 			os.Exit(255)
 		}
 		for _, c := range cont {
-			fmt.Printf("%-20s %-14s %s\n", c.Name(), "bar", "size")
-//			fmt.Println("Container: ", c.Name())
+			// get last modified time
+//			file, err := os.Stat(join(cache.Library(), "/", f.Name()))
+			file, err := os.Stat(join(cache.Library(), "/", f.Name(), "/", c.Name()))
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(100)
+			}
+
+//			fi, err := os.Stat(join(cache.Library(), "/", f.Name()))
+//			if err != nil {
+//				fmt.Println(err)
+//				os.Exit(100)
+//			}
+			// get the size
+
+//			fmt.Println("INFO:  ", find_size(file.Size()))
+
+//			fmt.Printf("The file is: %v bytes long:  %v\n", size, join(cache.Library(), "/", f.Name(), "/", c.Name()))
+
+			fmt.Printf("%-20s %-14s %s\n", c.Name(), file.ModTime().Format("2006-01-02"), find_size(file.Size()))
 		}		
 
 	}
