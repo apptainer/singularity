2e0696bb661c0467099e404970fb8e1a4290ded0
WestleyK <westley@sylabs.io>
2019-01-22 17:53:27 -0800

    Cleaned up code

--- internal/pkg/cacheCli/cache_list.go
+++ internal/pkg/cacheCli/cache_list.go
@@ -1,4 +1,7 @@
-
+// Copyright (c) 2019, Sylabs Inc. All rights reserved.
+// This software is licensed under a 3-clause BSD license. Please consult the
+// LICENSE.md file distributed with the sources of this project regarding your
+// rights to use or distribute this software.
 
 package cacheCli
 
@@ -7,10 +10,6 @@ import (
 	"strings"
 	"io/ioutil"
 	"os"
-//	"math"
-
-//	"strconv"
-//	"time"
 
 	"github.com/sylabs/singularity/internal/pkg/sylog"
 	"github.com/sylabs/singularity/internal/pkg/client/cache"
@@ -49,13 +48,7 @@ func find_size(size int64) string {
 
 func ListSingularityCache() error {
 
-//	fmt.Println("HELLO WORLD!!!!!!!!!")
 	sylog.Debugf("Starting list...")
-
-//	where := join(cache.Library(), "/")
-
-//	fmt.Println("WHERE: ", where)
-
 
 
 	files, err := ioutil.ReadDir(cache.Library())
@@ -64,44 +57,25 @@ func ListSingularityCache() error {
 		os.Exit(255)
 	}
 
-	fmt.Printf("%-20s %-14s %s\n", "NAME", "DATE CREATED", "SIZE")
-//	for _, file := range files {
-//		fmt.Printf("%-16s %-8d %s\n", file.Name, file.Pid, file.Image)
-//	}
-
+	fmt.Printf("%-22s %-22s %-16s %s\n", "NAME", "DATE CREATED", "SIZE", "TYPE")
 
 	for _, f := range files {
-//		fmt.Println("dir", f.Name())
 		cont, err := ioutil.ReadDir(join(cache.Library(), "/", f.Name()))
 		if err != nil {
 			sylog.Fatalf("%v", err)
 			os.Exit(255)
 		}
 		for _, c := range cont {
-			// get last modified time
 //			file, err := os.Stat(join(cache.Library(), "/", f.Name()))
 			file, err := os.Stat(join(cache.Library(), "/", f.Name(), "/", c.Name()))
 			if err != nil {
 				fmt.Println(err)
 				os.Exit(100)
 			}
-
-//			fi, err := os.Stat(join(cache.Library(), "/", f.Name()))
-//			if err != nil {
-//				fmt.Println(err)
-//				os.Exit(100)
-//			}
-			// get the size
-
-//			fmt.Println("INFO:  ", find_size(file.Size()))
-
-//			fmt.Printf("The file is: %v bytes long:  %v\n", size, join(cache.Library(), "/", f.Name(), "/", c.Name()))
-
-			fmt.Printf("%-20s %-14s %s\n", c.Name(), file.ModTime().Format("2006-01-02"), find_size(file.Size()))
+//			fmt.Printf("%-20s %-14s %-14s %s\n", c.Name(), file.ModTime().Format("2006-01-02"), find_size(file.Size()), "Library")
+			fmt.Printf("%-22s %-22s %-16s %s\n", c.Name(), file.ModTime().Format("2006-01-02 15:04:05"), find_size(file.Size()), "Library")
 		}		
-
 	}
-
 
 	return nil
 }
