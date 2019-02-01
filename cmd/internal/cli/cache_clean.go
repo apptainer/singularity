2e7c0cdc86cb2aa3072282b23b7cd726c9823c07
WestleyK <westley@sylabs.io>
2019-01-31 23:36:10 -0800

    2018-2019

--- cmd/internal/cli/cache_clean.go
+++ cmd/internal/cli/cache_clean.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2019, Sylabs Inc. All rights reserved.
+// Copyright (c) 2018-2019, Sylabs Inc. All rights reserved.
 // This software is licensed under a 3-clause BSD license. Please consult the
 // LICENSE.md file distributed with the sources of this project regarding your
 // rights to use or distribute this software.
