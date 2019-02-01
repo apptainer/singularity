7823ce06c1e427ae98450b2a1fce776d43a44420
SDkie <kumarsukhani@gmail.com>
2019-02-01 23:11:25 +0530

    Added check to verify cached file hash
    Any interupt in pull command will create invalid cached file,
    Added check to verify cached file hash

--- internal/pkg/client/cache/library.go
+++ internal/pkg/client/cache/library.go
@@ -8,6 +8,9 @@ package cache
 import (
 	"os"
 	"path/filepath"
+
+	"github.com/sylabs/singularity/internal/pkg/sylog"
+	client "github.com/sylabs/singularity/pkg/client/library"
 )
 
 const (
@@ -29,11 +32,24 @@ func LibraryImage(sum, name string) string {
 
 // LibraryImageExists returns whether the image with the SHA sum exists in the LibraryImage cache
 func LibraryImageExists(sum, name string) (bool, error) {
-	_, err := os.Stat(LibraryImage(sum, name))
+	imagePath := LibraryImage(sum, name)
+	_, err := os.Stat(imagePath)
 	if os.IsNotExist(err) {
 		return false, nil
 	} else if err != nil {
 		return false, err
+	}
+
+	cacheSum, err := client.ImageHash(imagePath)
+	if err != nil {
+		sylog.Debugf("Error getting ImageHash - Deleting Cached File: %s", err)
+		os.Remove(imagePath)
+		return false, nil
+	}
+	if cacheSum != sum {
+		sylog.Debugf("Cached File Sum(%s) and Expected Sum(%s) does not match - Deleting Cached File", cacheSum, sum)
+		os.Remove(imagePath)
+		return false, nil
 	}
 
 	return true, nil
