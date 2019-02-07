81590417956e1955e6f68572163ae0993f0793d1
Eduardo Arango <eduardo@sylabs.io>
2019-02-07 10:06:28 -0500

    fix test table order
    Signed-off-by: Eduardo Arango <eduardo@sylabs.io>

--- cmd/singularity/pull_test.go
+++ cmd/singularity/pull_test.go
@@ -33,7 +33,7 @@ func imagePull(library string, imagePath string, sourceSpec string, force bool) 
 func TestPull(t *testing.T) {
 	test.EnsurePrivilege(t)
 
-	imagePath := "./test_image.sif"
+	imagePath := "./test_pull.sif"
 
 	tests := []struct {
 		name       string
@@ -44,10 +44,10 @@ func TestPull(t *testing.T) {
 		success    bool
 	}{
 		{"Pull_Library", "library://alpine:3.7", false, "", imagePath, true}, // https://cloud.sylabs.io/library
-		{"Pull_Docker", "docker://alpine:3.7", true, "", imagePath, true},    // https://hub.docker.com/
-		{"Pull_Shub", "shub://GodloveD/busybox", true, "", imagePath, true},  // https://singularity-hub.org/
+		{"Force", "library://alpine:3.7", true, "", imagePath, true},
+		{"Pull_Docker", "docker://alpine:3.7", true, "", imagePath, true},   // https://hub.docker.com/
+		{"Pull_Shub", "shub://GodloveD/busybox", true, "", imagePath, true}, // https://singularity-hub.org/
 		{"PullWithHash", "library://alpine:sha256.af5e6b93dcfd08b7b46d6158c31ae5b6c2b4241c169b65595659ad9feac6f761", true, "", imagePath, true},
-		{"Force", "library://alpine:3.7", false, "", "./force_img.sif", true},
 	}
 	defer os.Remove(imagePath)
 	for _, tt := range tests {
