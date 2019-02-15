d82f254570a5189242bc3fd50511e76be9cb2354
WestleyK <westley@sylabs.io>
2019-02-14 16:05:24 -0800

    Cleaned up comments

--- pkg/signing/signing.go
+++ pkg/signing/signing.go
@@ -312,62 +312,7 @@ func IsSigned(cpath string) bool {
 			sylog.Fatalf("could not get the signing entity fingerprint: %s", err)
 			os.Exit(100)
 		}
-
-/*		// try to verify with local OpenPGP store first
-		signer, err := openpgp.CheckDetachedSignature(elist, bytes.NewBuffer(block.Bytes), block.ArmoredSignature.Body)
-		if err != nil {
-			// verification with local keyring failed, try to fetch from key server
-			sylog.Infof("key missing, searching key server for KeyID: %s...", fingerprint[24:])
-			netlist, err := sypgp.FetchPubkey(fingerprint, url, authToken, noPrompt)
-			if err != nil {
-				return fmt.Errorf("could not fetch public key from server: %s", err)
-			}
-			sylog.Infof("key retrieved successfully!")
-
-			block, _ := clearsign.Decode(data)
-			if block == nil {
-				return fmt.Errorf("failed to parse signature block")
-			}
-
-			// try verification again with downloaded key
-			signer, err = openpgp.CheckDetachedSignature(netlist, bytes.NewBuffer(block.Bytes), block.ArmoredSignature.Body)
-			if err != nil {
-				return fmt.Errorf("signature verification failed: %s", err)
-			}
-
-			if noPrompt {
-				// always store key when prompts disabled
-				if err = sypgp.StorePubKey(netlist[0]); err != nil {
-					return fmt.Errorf("could not store public key: %s", err)
-				}
-			} else {
-				// Ask to store new public key
-				resp, err := sypgp.AskQuestion("Store new public key %X? [Y/n] ", signer.PrimaryKey.Fingerprint)
-				if err != nil {
-					return err
-				}
-				if resp == "" || resp == "y" || resp == "Y" {
-					if err = sypgp.StorePubKey(netlist[0]); err != nil {
-						return fmt.Errorf("could not store public key: %s", err)
-					}
-				}
-			}
-		}*/
-
-		// Get first Identity data for convenience
-
-//		var name string
-//		for _, i := range signer.Identities {
-//			name = i.Name
-//			break
-//		}
-//		authok += fmt.Sprintf("\t%s, KeyID %X\n", name, signer.PrimaryKey.KeyId)
 	}
-//	fmt.Printf("Data integrity checked, authentic and signed by:\n")
-//	fmt.Print(authok)
-
-
-//	signedStatus = true
 
 	return true
 }
