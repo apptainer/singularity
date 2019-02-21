5d1fab94f9d84f7c20861169e4cb297f7e48ea15
Cedric Clerget <cedric.clerget@gmail.com>
2019-02-21 18:11:39 +0100

    Add NewSetup/SetArgs tests

--- pkg/network/network_test.go
+++ pkg/network/network_test.go
@@ -116,12 +116,12 @@ func TestGetAllNetworkConfigList(t *testing.T) {
 		validationFunc func([]*libcni.NetworkConfigList) error
 	}{
 		{
-			name:    "'nil CNIPath'",
+			name:    "nil CNIPath",
 			cniPath: nil,
 			success: false,
 		},
 		{
-			name: "'empty configuration path'",
+			name: "empty configuration path",
 			cniPath: &CNIPath{
 				Conf:   "",
 				Plugin: "",
@@ -129,7 +129,7 @@ func TestGetAllNetworkConfigList(t *testing.T) {
 			success: false,
 		},
 		{
-			name: "'empty configuration directory'",
+			name: "empty configuration directory",
 			cniPath: &CNIPath{
 				Conf:   emptyDir,
 				Plugin: "",
@@ -137,7 +137,7 @@ func TestGetAllNetworkConfigList(t *testing.T) {
 			success: false,
 		},
 		{
-			name: "'default configuration/plugin path'",
+			name: "default configuration/plugin path",
 			cniPath: &CNIPath{
 				Conf:   defaultCNIConfPath,
 				Plugin: defaultCNIPluginPath,
@@ -159,13 +159,178 @@ func TestGetAllNetworkConfigList(t *testing.T) {
 	for _, c := range testCNIPath {
 		networkList, err := GetAllNetworkConfigList(c.cniPath)
 		if err != nil && c.success {
-			t.Errorf("unexpected failure for %s test: %s", c.name, err)
+			t.Errorf("unexpected failure for %q test: %s", c.name, err)
 		} else if err == nil && !c.success {
-			t.Errorf("unexpected success for %s test", c.name)
+			t.Errorf("unexpected success for %q test", c.name)
 		} else if c.validationFunc != nil {
 			if err := c.validationFunc(networkList); err != nil {
 				t.Error(err)
 			}
+		}
+	}
+}
+
+func testSetArgs(setup *Setup, t *testing.T) {
+	var testArgs = []struct {
+		desc    string
+		args    []string
+		success bool
+	}{
+		{
+			desc:    "empty arg",
+			args:    []string{""},
+			success: false,
+		},
+		{
+			desc:    "badly formatted arg #1",
+			args:    []string{"test-bridge:"},
+			success: false,
+		},
+		{
+			desc:    "badly formatted arg #2",
+			args:    []string{":portmap=80/tcp"},
+			success: false,
+		},
+		{
+			desc:    "badly formatted arg #3",
+			args:    []string{"portmap=80/tcp;portmap="},
+			success: false,
+		},
+		{
+			desc:    "empty portmap",
+			args:    []string{"test-bridge:portmap="},
+			success: false,
+		},
+		{
+			desc:    "unknown portmap protocol",
+			args:    []string{"test-bridge:portmap=80/icmp"},
+			success: false,
+		},
+		{
+			desc:    "portmap 0",
+			args:    []string{"test-bridge:portmap=0/tcp"},
+			success: false,
+		},
+		{
+			desc:    "good portmap arg #1",
+			args:    []string{"test-bridge:portmap=80:80/tcp", "portmap=80:80/tcp"},
+			success: true,
+		},
+		{
+			desc:    "good portmap arg #2",
+			args:    []string{"portmap=80:80/tcp;portmap=8080/udp"},
+			success: true,
+		},
+		{
+			desc:    "good 1-1 portmap arg",
+			args:    []string{"test-bridge:portmap=80/udp", "test-bridge-iprange:portmap=8080/tcp"},
+			success: true,
+		},
+		{
+			desc:    "ipRange not supported arg",
+			args:    []string{"test-bridge:ipRange=10.1.1.0/16"},
+			success: false,
+		},
+		{
+			desc:    "good ipRange arg",
+			args:    []string{"test-bridge-iprange:ipRange=10.1.1.0/16"},
+			success: true,
+		},
+		{
+			desc:    "bad ipRange arg",
+			args:    []string{"test-bridge-iprange:ipRange=1024.1.1.0/16"},
+			success: false,
+		},
+		{
+			desc:    "IP arg",
+			args:    []string{"test-bridge:IP=10.1.1.1"},
+			success: true,
+		},
+		{
+			desc:    "Any arg",
+			args:    []string{"test-bridge:any=test"},
+			success: true,
+		},
+	}
+	for _, a := range testArgs {
+		err := setup.SetArgs(a.args)
+		if err != nil && a.success {
+			t.Errorf("unexpected failure for %q test: %s", a.desc, err)
+		} else if err == nil && !a.success {
+			t.Errorf("unexpected success for %q test", a.desc)
+		}
+	}
+}
+
+func TestNewSetup(t *testing.T) {
+	var cniPath = &CNIPath{
+		Conf:   defaultCNIConfPath,
+		Plugin: defaultCNIPluginPath,
+	}
+	var testSetup = []struct {
+		desc     string
+		networks []string
+		id       string
+		nspath   string
+		cniPath  *CNIPath
+		success  bool
+		subTest  func(*Setup, *testing.T)
+	}{
+		{
+			desc:     "no name network",
+			networks: []string{""},
+			id:       "testing",
+			nspath:   "/proc/self/net/ns",
+			cniPath:  cniPath,
+			success:  false,
+		},
+		{
+			desc:     "bad network",
+			networks: []string{"fake-network"},
+			id:       "testing",
+			nspath:   "/proc/self/net/ns",
+			cniPath:  cniPath,
+			success:  false,
+		},
+		{
+			desc:     "bad networks",
+			networks: []string{"test-bridge", "fake-network"},
+			id:       "testing",
+			nspath:   "/proc/self/net/ns",
+			cniPath:  cniPath,
+			success:  false,
+		},
+		{
+			desc:     "good network",
+			networks: []string{"test-bridge"},
+			nspath:   "/proc/self/net/ns",
+			cniPath:  cniPath,
+			success:  true,
+		},
+		{
+			desc:     "good networks",
+			networks: []string{"test-bridge", "test-bridge-iprange"},
+			nspath:   "/proc/self/net/ns",
+			cniPath:  cniPath,
+			success:  true,
+			subTest:  testSetArgs,
+		},
+		{
+			desc:     "nil cni path",
+			networks: []string{""},
+			id:       "testing",
+			success:  false,
+		},
+	}
+
+	for _, s := range testSetup {
+		setup, err := NewSetup(s.networks, s.id, s.nspath, s.cniPath)
+		if err != nil && s.success {
+			t.Errorf("unexpected failure for %q test: %s", s.desc, err)
+		} else if err == nil && !s.success {
+			t.Errorf("unexpected success for %q test", s.desc)
+		} else if s.subTest != nil {
+			s.subTest(setup, t)
 		}
 	}
 }
