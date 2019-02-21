232e5a8fa6bca17c735a37a0ab35962c5e0efdd6
WestleyK <westley@sylabs.io>
2019-02-21 11:01:33 -0800

    Removed commented code

--- pkg/signing/signing.go
+++ pkg/signing/signing.go
@@ -260,126 +260,15 @@ func getSigsForSelection(fimg *sif.FileImage, id uint32, isGroup bool) (sigs []*
 	return getSigsDescr(fimg, id)
 }
 
-// IsSigned : will return false if the givin container (cpath) is
-// not signed. Likewise, will return true if the container is signed.
-// will return a error if one occures.
+// IsSigned : will return false if the givin container (cpath) is not signed.
+// Likewise, will return true if the container is signed and print who signed
+// it. will return a error if one occures.
 func IsSigned(cpath, url string, id uint32, isGroup bool, authToken string, noPrompt bool) (bool, error) {
-
 	err := Verify(cpath, url, id, isGroup, authToken, noPrompt)
 	if err != nil {
-		//sylog.Warningf("Unable to verify the container (%v): %v", cpath, err)
 		return false, fmt.Errorf("Unable to verify the container (%v): %v", cpath, err)
 	}
 	return true, nil
-
-	/*fimg, err := sif.LoadContainer(cpath, true)
-	if err != nil {
-		sylog.Fatalf("failed to load SIF container file: %v", err)
-		os.Exit(100)
-	}
-	defer fimg.UnloadContainer()
-
-	_, _, err = getSigsForSelection(&fimg, 0, false)
-	if err != nil {
-		// Not signed
-		return false, nil
-	}
-
-	// get all signature blocks (signatures) for ID/GroupID selected (descr) from SIF file
-	signatures, descr, err := getSigsForSelection(&fimg, id, isGroup)
-	if err != nil {
-		return false, nil
-		//		return fmt.Errorf("error while searching for signature blocks: %s", err)
-	}
-
-	// the selected data object is hashed for comparison against signature block's
-	sifhash := computeHashStr(&fimg, descr)
-
-	// load the public keys available locally from the cache
-	elist, err := sypgp.LoadPubKeyring()
-	if err != nil {
-		return false, fmt.Errorf("could not load public keyring: %s", err)
-	}
-
-	// compare freshly computed hash with hashes stored in signatures block(s)
-	var authok string
-	for _, v := range signatures {
-		// Extract hash string from signature block
-		data := v.GetData(&fimg)
-		block, _ := clearsign.Decode(data)
-		if block == nil {
-			sylog.Warningf("failed to parse signature block")
-			return false, nil
-		}
-
-		if !bytes.Equal(bytes.TrimRight(block.Plaintext, "\n"), []byte(sifhash)) {
-			sylog.Infof("NOTE: group signatures will fail if new data is added to a group")
-			sylog.Infof("after the group signature is created.")
-			return false, fmt.Errorf("hashes differ, data may be corrupted")
-		}
-
-		// (1) Data integrity is verified, (2) now validate identify of signers
-
-		// get the entity fingerprint for the signature block
-		fingerprint, err := v.GetEntityString()
-		if err != nil {
-			return false, fmt.Errorf("could not get the signing entity fingerprint: %s", err)
-		}
-
-		// try to verify with local OpenPGP store first
-		signer, err := openpgp.CheckDetachedSignature(elist, bytes.NewBuffer(block.Bytes), block.ArmoredSignature.Body)
-		if err != nil {
-			// verification with local keyring failed, try to fetch from key server
-			sylog.Infof("key missing, searching key server for KeyID: %s...", fingerprint[24:])
-			netlist, err := sypgp.FetchPubkey(fingerprint, url, authToken, noPrompt)
-			if err != nil {
-				return false, fmt.Errorf("could not fetch public key from server: %s", err)
-			}
-			sylog.Infof("key retrieved successfully!")
-
-			block, _ := clearsign.Decode(data)
-			if block == nil {
-				return false, fmt.Errorf("failed to parse signature block")
-			}
-
-			// try verification again with downloaded key
-			signer, err = openpgp.CheckDetachedSignature(netlist, bytes.NewBuffer(block.Bytes), block.ArmoredSignature.Body)
-			if err != nil {
-				return false, fmt.Errorf("signature verification failed: %s", err)
-			}
-
-			if noPrompt {
-				// always store key when prompts disabled
-				if err = sypgp.StorePubKey(netlist[0]); err != nil {
-					return false, fmt.Errorf("could not store public key: %s", err)
-				}
-			} else {
-				// Ask to store new public key
-				resp, err := sypgp.AskQuestion("Store new public key %X? [Y/n] ", signer.PrimaryKey.Fingerprint)
-				if err != nil {
-					return false, err
-				}
-				if resp == "" || resp == "y" || resp == "Y" {
-					if err = sypgp.StorePubKey(netlist[0]); err != nil {
-						return false, fmt.Errorf("could not store public key: %s", err)
-					}
-				}
-			}
-		}
-
-		// Get first Identity data for convenience
-		var name string
-		for _, i := range signer.Identities {
-			name = i.Name
-			break
-		}
-		authok += fmt.Sprintf("\t%s, KeyID %X\n", name, signer.PrimaryKey.KeyId)
-	}
-	sylog.Infof("Container is signed")
-	fmt.Printf("Data integrity checked, authentic and signed by:\n")
-	fmt.Print(authok)*/
-
-	//return true, nil
 }
 
 // Verify takes a container path and look for a verification block for a
