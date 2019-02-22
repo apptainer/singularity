53a89ab863b1cadfb604f79b16a50ec5559a6f2e
Cedric Clerget <cedric.clerget@gmail.com>
2019-02-22 11:43:25 +0100

    Add compiler option checks. Support CFLAGS/LDFLAGS/CGO_ variables

--- mlocal/frags/common_opts.mk
+++ mlocal/frags/common_opts.mk
@@ -4,13 +4,4 @@ CLEANFILES :=
 # general build-wide compile options
 AFLAGS := -g
 
-CFLAGS := -Wall -Werror -Wfatal-errors  -Wno-unknown-warning-option
-CFLAGS += -Wstrict-prototypes -Wpointer-arith -Wbad-function-cast
-CFLAGS += -Woverlength-strings -Wframe-larger-than=2047
-CFLAGS += -Wno-sign-compare -Wclobbered -Wempty-body -Wmissing-parameter-type
-CFLAGS += -Wtype-limits -Wunused-parameter -Wunused-but-set-parameter
-CFLAGS += -Wno-discarded-qualifiers -Wno-incompatible-pointer-types
-CFLAGS += -pipe -fmessage-length=0 -fPIC
-CFLAGS += -D_FORTIFY_SOURCE=2 -Wformat -Wformat-security -fstack-protector --param ssp-buffer-size=4
-
 LDFLAGS += -Wl,-z,relro,-z,now
