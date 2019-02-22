5c32b93958d9f5898ac3e7a1f00f88650880a490
Michael Bauer <bauerm@umich.edu>
2019-02-22 11:24:56 -0700

    Implement ground work data type for plugin management commands
    Signed-off-by: Michael Bauer <bauerm@umich.edu>

--- internal/app/singularity/plugin_install_linux.go
+++ internal/app/singularity/plugin_install_linux.go
@@ -5,50 +5,23 @@
 
 package singularity
 
-import (
-	"fmt"
-	"path/filepath"
-
-	"github.com/sylabs/sif/pkg/sif"
-)
-
 // InstallPlugin takes a plugin located at path and installs it into
 // the singularity folder in libexecdir.
 //
 // Installing a plugin will also automatically enable it.
 func InstallPlugin(pluginPath, libexecdir string) error {
-	fimg, err := sif.LoadContainer(pluginPath, true)
-	if err != nil {
-		return fmt.Errorf("while opening sif file: %s", err)
-	}
+	// fimg, err := sif.LoadContainer(pluginPath, true)
+	// if err != nil {
+	// 	return fmt.Errorf("while opening sif file: %s", err)
+	// }
 
-	if !isPluginFile(&fimg) {
-		return fmt.Errorf("sif file is not a plugin")
-	}
+	// if !isPluginFile(&fimg) {
+	// 	return fmt.Errorf("sif file is not a plugin")
+	// }
 
-	if err := copyFile(pluginPath, filepath.Join(libexecdir, ".")); err != nil {
-		return fmt.Errorf("while copying plugin file to install location: %s", err)
-	}
+	// if err := copyFile(pluginPath, filepath.Join(libexecdir, ".")); err != nil {
+	// 	return fmt.Errorf("while copying plugin file to install location: %s", err)
+	// }
 
-	return nil
-}
-
-// isPluginFile checks if the sif.FileImage contains the sections which
-// make up a valid plugin. A plugin sif file should have the following
-// format:
-//
-// DESCR[0]: Sifplugin
-//   - Datatype: sif.DataPartition
-//   - Fstype:   sif.FsRaw
-//   - Parttype: sif.PartData
-// DESCR[1]: Sifmanifest
-//   - Datatype: sif.DataGenericJSON
-func isPluginFile(fimg *sif.FileImage) bool {
-	return false
-}
-
-// copyFile copies a file from src -> dst
-func copyFile(src, dst string) error {
-	// copycmd := exec.Command("cp", src, dst)
 	return nil
 }
