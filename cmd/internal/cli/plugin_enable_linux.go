2d668d0d1571f3d15e35eacaf4add8235d435465
Marcelo E. Magallon <marcelo@sylabs.io>
2019-03-12 14:11:40 -0600

    Implement all the plugin enable / disable functionality
    - Fixes #2322
    
    Signed-off-by: Marcelo E. Magallon <marcelo@sylabs.io>

---
+++ cmd/internal/cli/plugin_enable_linux.go
@@ -0,0 +1,34 @@
+package cli
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/sylabs/singularity/docs"
+	"github.com/sylabs/singularity/internal/app/singularity"
+	"github.com/sylabs/singularity/internal/pkg/buildcfg"
+	"github.com/sylabs/singularity/internal/pkg/sylog"
+)
+
+// PluginEnableCmd enables the named plugin
+//
+// singularity plugin enable <name>
+var PluginEnableCmd = &cobra.Command{
+	Run: func(cmd *cobra.Command, args []string) {
+		err := singularity.EnablePlugin(args[0], buildcfg.LIBEXECDIR)
+		if err != nil {
+			if os.IsNotExist(err) {
+				sylog.Fatalf("Failed to enable plugin %q: plugin not found.", args[0])
+			} else {
+				sylog.Fatalf("Failed to enable plugin %q: %s.", args[0], err)
+			}
+		}
+	},
+	DisableFlagsInUseLine: true,
+	Args:                  cobra.ExactArgs(1),
+
+	Use:     docs.PluginEnableUse,
+	Short:   docs.PluginEnableShort,
+	Long:    docs.PluginEnableLong,
+	Example: docs.PluginEnableExample,
+}
