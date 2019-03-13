ca4a993b84d14ec9e68b1bdf0cfd115b7684f11b
WestleyK <westley@sylabs.io>
2019-03-13 10:57:24 -0700

    Updated tests

--- cmd/singularity/pull_test.go
+++ cmd/singularity/pull_test.go
@@ -50,45 +50,29 @@ func TestPull(t *testing.T) {
 		{"Pull_Library", "library://alpine:3.8", false, false, "", imagePath, true}, // https://cloud.sylabs.io/library
 		{"Force", "library://alpine:3.8", true, false, "", imagePath, true},
 		{"Unsigned_image", "library://sylabs/tests/unsigned:1.0.0", true, true, "", imagePath, true},
-		{"Pull_Docker", "docker://alpine:3.8", true, false, "", imagePath, true},   // https://hub.docker.com/
-		{"Pull_Shub", "shub://GodloveD/busybox", true, false, "", imagePath, true}, // https://singularity-hub.org/
+		{"Unsigned_image_fail", "library://sylabs/tests/unsigned:1.0.0", true, false, "", imagePath, false}, // pull a unsigned image; should fail
+		{"Pull_Docker", "docker://alpine:3.8", true, false, "", imagePath, true},                            // https://hub.docker.com/
+		{"Pull_Shub", "shub://GodloveD/busybox", true, false, "", imagePath, true},                          // https://singularity-hub.org/
 		{"PullWithHash", "library://sylabs/tests/signed:sha256.5c439fd262095766693dae95fb81334c3a02a7f0e4dc6291e0648ed4ddc61c6c", true, false, "", imagePath, true},
 		{"PullWithoutTransportProtocol", "alpine:3.8", true, false, "", imagePath, true},
+		{"PullNonExistent", "library://this_should_not/exist/not_exist", true, false, "", imagePath, false}, // pull a non-existent container
 	}
 	defer os.Remove(imagePath)
 	for _, tt := range tests {
 		t.Run(tt.name, test.WithoutPrivilege(func(t *testing.T) {
-			if b, err := imagePull(tt.library, tt.imagePath, tt.sourceSpec, tt.force, tt.unauthenticated); err != nil {
-				t.Log(string(b))
-				t.Fatalf("unexpected failure: %v", err)
+			b, err := imagePull(tt.library, tt.imagePath, tt.sourceSpec, tt.force, tt.unauthenticated)
+			if tt.success {
+				if err != nil {
+					t.Log(string(b))
+					t.Fatalf("unexpected failure: %v", err)
+				}
+			} else {
+				if err == nil {
+					t.Log(string(b))
+					t.Fatalf("unexpected success: command should have failed")
+				}
 			}
 			imageVerify(t, tt.imagePath, false)
 		}))
 	}
-}
-
-func TestPullNonExistent(t *testing.T) {
-	test.DropPrivilege(t)
-
-	if b, err := imagePull("", "", "library://this_should_not/exist", false, false); err == nil {
-		t.Log(string(b))
-		t.Fatalf("unexpected success")
-	}
-}
-
-func TestUnauthenticatedPull(t *testing.T) {
-	test.DropPrivilege(t)
-
-	// pull a unsigned container. should fail
-	if b, err := imagePull("", imagePath, "library://sylabs/tests/unsigned:1.0.0", true, false); err == nil {
-		t.Log(string(b))
-		t.Fatalf("unexpected success when pulling a unsigned container")
-	}
-
-	// pull a unsigned container (with a hash). should fail
-	if b, err := imagePull("", imagePath, "library://sylabs/tests/unsigned:sha256.6dabf826cd9adf3f6390748176bf8a6ff7cb574d4ee1f0e449ba5d14ae35c2bb", true, false); err == nil {
-		t.Log(string(b))
-		t.Fatalf("unexpected success when pulling a unsigned container with hash")
-	}
-
 }
