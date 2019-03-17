dc30fbd611ee8e0536c68358f1f35fe9ad63392c
Michael Bauer <michael@bauer.dev>
2019-03-17 14:47:35 -0700

    Introduce support for remote add/remove --global flag
    We'd like to support the usage of a global remote endpoint config
    file which gives users a set of pre-configured SCS endpoints to select
    from. The first step taken here is allowing the root user to add and
    remove endpoints from a global configuration file. This file is located
    at $(buildcfg.SYSCONFDIR)/singularity/remote.yaml.
    
    Updates: #2894
    
    Signed-off-by: Michael Bauer <michael@bauer.dev>

--- cmd/internal/cli/remote.go
+++ cmd/internal/cli/remote.go
@@ -6,26 +6,53 @@
 package cli
 
 import (
+	"os"
 	"os/user"
 	"path/filepath"
 
 	"github.com/spf13/cobra"
 	"github.com/sylabs/singularity/docs"
 	"github.com/sylabs/singularity/internal/app/singularity"
+	"github.com/sylabs/singularity/internal/pkg/buildcfg"
 	"github.com/sylabs/singularity/internal/pkg/sylog"
+)
+
+const (
+	fileName = "remote.yaml"
+	userDir  = ".singularity"
+	sysDir   = "singularity"
 )
 
 var (
 	remoteConfig string
+	global       bool
 )
+
+var (
+	remoteConfigUser string
+	remoteConfigSys  string
+)
+
+func addGlobalFlag(c *cobra.Command) {
+	c.Flags().BoolVarP(&global, "global", "g", false, "edit the list of globally configured remote endpoints")
+}
 
 func init() {
 	usr, err := user.Current()
 	if err != nil {
 		sylog.Fatalf("Couldn't determine user home directory: %v", err)
 	}
-	remoteConfig = filepath.Join(usr.HomeDir, ".singularity", "remote.yaml")
-	RemoteCmd.Flags().StringVarP(&remoteConfig, "config", "c", remoteConfig, "path to the file holding remote endpoint configurations")
+
+	// assemble values of remoteConfig for user/sys locations
+	remoteConfigUser = filepath.Join(usr.HomeDir, userDir, fileName)
+	remoteConfigSys = filepath.Join(buildcfg.SYSCONFDIR, sysDir, fileName)
+
+	// default location of the remote.yaml file is the user directory
+	RemoteCmd.Flags().StringVarP(&remoteConfig, "config", "c", remoteConfigUser, "path to the file holding remote endpoint configurations")
+
+	// add --global flag to remote add/remove commands
+	addGlobalFlag(RemoteAddCmd)
+	addGlobalFlag(RemoteRemoveCmd)
 
 	SingularityCmd.AddCommand(RemoteCmd)
 	RemoteCmd.AddCommand(RemoteAddCmd)
@@ -36,7 +63,7 @@ func init() {
 	RemoteCmd.AddCommand(RemoteStatusCmd)
 }
 
-// RemoteCmd singularity remote ...
+// RemoteCmd singularity remote [...]
 var RemoteCmd = &cobra.Command{
 	Run: nil,
 
@@ -46,9 +73,25 @@ var RemoteCmd = &cobra.Command{
 	Example: docs.RemoteExample,
 }
 
+// setGlobalRemoteConfig will assign the appropriate value to remoteConfig if the global flag is set
+func setGlobalRemoteConfig(_ *cobra.Command, _ []string) {
+	if !global {
+		return
+	}
+
+	uid := uint32(os.Getuid())
+	if uid != 0 {
+		sylog.Fatalf("Unable to modify global endpoint configuration file: not root user")
+	}
+
+	// set remoteConfig value to the location of the global remote.yaml file
+	remoteConfig = remoteConfigSys
+}
+
 // RemoteAddCmd singularity remote add [remoteName] [remoteURI]
 var RemoteAddCmd = &cobra.Command{
-	Args: cobra.ExactArgs(2),
+	Args:   cobra.ExactArgs(2),
+	PreRun: setGlobalRemoteConfig,
 	Run: func(cmd *cobra.Command, args []string) {
 		if err := singularity.RemoteAdd(remoteConfig, args[0], args[1]); err != nil {
 			sylog.Fatalf("%s", err)
@@ -63,7 +106,8 @@ var RemoteAddCmd = &cobra.Command{
 
 // RemoteRemoveCmd singularity remote remove [remoteName]
 var RemoteRemoveCmd = &cobra.Command{
-	Args: cobra.ExactArgs(1),
+	Args:   cobra.ExactArgs(1),
+	PreRun: setGlobalRemoteConfig,
 	Run: func(cmd *cobra.Command, args []string) {
 		if err := singularity.RemoteRemove(remoteConfig, args[0]); err != nil {
 			sylog.Fatalf("%s", err)
