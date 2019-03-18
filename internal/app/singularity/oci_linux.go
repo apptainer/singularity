7f84d16c4aa0357a55eb5e93359de75f21957df8
Cedric Clerget <cedric.clerget@gmail.com>
2019-03-18 16:07:41 +0100

    Make CI happy with struct size optimization

--- internal/app/singularity/oci_linux.go
+++ internal/app/singularity/oci_linux.go
@@ -22,12 +22,12 @@ type OciArgs struct {
 	LogPath        string
 	LogFormat      string
 	SyncSocketPath string
-	EmptyProcess   bool
 	PidFile        string
 	FromFile       string
 	KillSignal     string
-	ForceKill      bool
 	KillTimeout    uint32
+	EmptyProcess   bool
+	ForceKill      bool
 }
 
 func getCommonConfig(containerID string) (*config.Common, error) {
