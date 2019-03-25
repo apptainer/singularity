a2bc437b0beefc85b7470ed5a3179b636869b982
Joana Chavez <chavezlucero88@gmail.com>
2019-03-25 20:49:02 +0100

    export command public key working

--- cmd/internal/cli/key_export.go
+++ cmd/internal/cli/key_export.go
@@ -6,7 +6,6 @@
 package cli
 
 import (
-	"bytes"
 	"fmt"
 	"os"
 
@@ -15,8 +14,6 @@ import (
 	"github.com/sylabs/singularity/internal/pkg/sylog"
 	"github.com/sylabs/singularity/pkg/sypgp"
 	"golang.org/x/crypto/openpgp"
-	"golang.org/x/crypto/openpgp/armor"
-	"golang.org/x/crypto/openpgp/packet"
 )
 
 var secretExport bool
@@ -45,6 +42,7 @@ func doKeyExportCmd(secretExport bool, fingerprint string, path string) error {
 
 	//describes the path from either the local public keyring or secret local keyring
 	var fetchPath string
+	var keyString string
 
 	if secretExport {
 		fetchPath = sypgp.SecretPath()
@@ -65,62 +63,77 @@ func doKeyExportCmd(secretExport bool, fingerprint string, path string) error {
 	}
 
 	var entityToSave *openpgp.Entity
-	foundKey = false
-	// sort through them, and remove any that match toDelete
-	for _, localEntity := range localEntityList {
-		if fmt.Sprintf("%X", localEntity.PrimaryKey.Fingerprint) == fingerprint {
-			foundKey = true
-			entityToSave = localEntity
-			break
-		}
+
+	file, err := os.Create(path)
+	if err != nil {
+		os.Exit(1)
 	}
 
 	if secretExport {
 
-		privKeyBuf := bytes.NewBuffer(nil)
+		foundKey = false
+		// sort through them, and remove any that match toDelete
+		for _, localEntity := range localEntityList {
 
-		var config *packet.Config
+			fmt.Printf("%0X\n", localEntity.PrivateKey.PublicKey.Fingerprint)
+			if fmt.Sprintf("%X", localEntity.PrivateKey.PublicKey.Fingerprint) == fingerprint {
+				foundKey = true
+				entityToSave = localEntity
+				break
+			}
 
-		privKeyWriter, err := armor.Encode(privKeyBuf, openpgp.PrivateKeyType, nil)
-		if err != nil {
-			return fmt.Errorf("error encoding private key")
 		}
 
-		err = entityToSave.SerializePrivate(privKeyWriter, config)
-		if err != nil {
-			return fmt.Errorf("error encoding private key")
-		}
-		privKeyWriter.Close()
-		file, err := os.Create(path)
-		if err != nil {
-			os.Exit(1)
-		}
-		file.WriteString(privKeyBuf.String())
-		defer file.Close()
+		fmt.Println(entityToSave)
 
+		pass, err := sypgp.AskQuestionNoEcho("Enter key passphrase: ")
+		if err != nil {
+			return err
+		}
+
+		fmt.Println(entityToSave.PrivateKey)
+		if entityToSave.PrivateKey != nil && entityToSave.PrivateKey.Encrypted {
+			err := entityToSave.PrivateKey.Decrypt([]byte(pass))
+			if err != nil {
+				fmt.Println("Failed to decrypt key")
+			}
+		}
+		/*
+
+			privKeyBuf := bytes.NewBuffer(nil)
+
+			var config *packet.Config
+
+			privKeyWriter, err := armor.Encode(privKeyBuf, openpgp.PrivateKeyType, nil)
+			if err != nil {
+				return fmt.Errorf("error encoding private key")
+			}
+
+			err = entityToSave.SerializePrivate(privKeyWriter, config)
+			if err != nil {
+				return fmt.Errorf("error encoding private key")
+			}
+			privKeyWriter.Close()
+
+			file.WriteString(privKeyBuf.String())
+			defer file.Close()
+		*/
 	} else {
 
-		pubKeyBuf := bytes.NewBuffer(nil)
-
-		pubKeyWriter, err := armor.Encode(pubKeyBuf, openpgp.PublicKeyType, nil)
-		if err != nil {
-			return fmt.Errorf("error encoding public key")
+		foundKey = false
+		// sort through them, and remove any that match toDelete
+		for _, localEntity := range localEntityList {
+			if fmt.Sprintf("%X", localEntity.PrimaryKey.Fingerprint) == fingerprint {
+				foundKey = true
+				entityToSave = localEntity
+				break
+			}
 		}
-
-		err = entityToSave.Serialize(pubKeyWriter)
-		if err != nil {
-			return fmt.Errorf("error serializing public key")
-		}
-		pubKeyWriter.Close()
-		file, err := os.Create(path)
-		if err != nil {
-			os.Exit(1)
-		}
-
-		file.WriteString(pubKeyBuf.String())
+		fmt.Println(entityToSave.PrimaryKey)
+		keyString, err = sypgp.SerializePublicEntity(entityToSave, openpgp.PublicKeyType)
+		file.WriteString(keyString)
 		defer file.Close()
 		fmt.Printf("Public key with fingerprint %s correctly exported to file: %s\n", fingerprint, path)
-
 	}
 
 	return nil
