6293a127584f22af314883552e3125334a3dfd76
Cedric Clerget <cedric.clerget@gmail.com>
2019-03-26 11:01:23 +0100

    Fix error message for oci state

--- internal/app/singularity/oci_linux.go
+++ internal/app/singularity/oci_linux.go
@@ -7,6 +7,7 @@ package singularity
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
 
 	"github.com/sylabs/singularity/internal/pkg/instance"
@@ -37,11 +38,11 @@ func getCommonConfig(containerID string) (*config.Common, error) {
 
 	file, err := instance.Get(containerID, instance.OciSubDir)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("no container found with name %s", containerID)
 	}
 
 	if err := json.Unmarshal(file.Config, &commonConfig); err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to read %s container configuration: %s", containerID, err)
 	}
 
 	return &commonConfig, nil
