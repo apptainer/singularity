9cfd13a55ebc33b34deb28d1dd51eafd1f5b9533
Justin Cook <jscook2345@gmail.com>
2019-03-26 12:16:19 -0500

    Update the message shown when oci delete is run on a container without a state of either created or stopped

--- internal/app/singularity/oci_delete_linux.go
+++ internal/app/singularity/oci_delete_linux.go
@@ -23,7 +23,7 @@ func OciDelete(containerID string) error {
 
 	switch engineConfig.State.Status {
 	case ociruntime.Running:
-		return fmt.Errorf("container is not stopped: running")
+		return fmt.Errorf("cannot delete '%s', the state of the container must be either %s or %s", containerID, ociruntime.Created, ociruntime.Stopped)
 	case ociruntime.Stopped:
 	case ociruntime.Created:
 		if err := OciKill(containerID, "SIGTERM", 2); err != nil {
