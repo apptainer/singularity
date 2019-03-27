fd2befaa3d61ca1b662961a523133229444ee4f9
Cedric Clerget <cedric.clerget@gmail.com>
2019-03-27 10:01:04 +0100

    Remove all subcommand in short use helps

--- docs/content.go
+++ docs/content.go
@@ -159,7 +159,7 @@ Enterprise Performance Computing (EPC)`
 	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	// Cache
 	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	CacheUse   string = `cache <subcommand>`
+	CacheUse   string = `cache`
 	CacheShort string = `Manage the local cache`
 	CacheLong  string = `
   Manage your local singularity cache. There are 3 types of cache; library, oci, and blob.
@@ -204,10 +204,10 @@ Enterprise Performance Computing (EPC)`
 	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	// key
 	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	KeyUse string = `key [key options...] <subcommand>`
+	KeyUse string = `key [key options...]`
 
 	// keys : for the hidden `keys` command
-	KeysUse  string = `keys [keys options...] <subcommand>`
+	KeysUse  string = `keys [keys options...]`
 	KeyShort string = `Manage OpenPGP keys`
 	KeyLong  string = `
   The 'key' command allows you to manage local OpenPGP key stores by creating
@@ -219,6 +219,21 @@ Enterprise Performance Computing (EPC)`
 
   $ singularity help key newpair
   $ singularity key list --help`
+
+	// keys : for the hidden `keys` command
+	KeysUse string = `keys [keys options...]`
+
+	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	// key import
+	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	KeyImportUse   string = `import [import options...] <full-path-to-local-key>`
+	KeyImportShort string = `Import a local key into the local Singularity key store`
+	KeyImportLong  string = `
+  The 'key import' command allows you to add to your local key store, keys from a specific local folder`
+	KeyImportExample string = `
+  $ singularity key import $HOME/key.asc
+  $ singularity keys import $HOME/key.asc
+  `
 
 	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	// key newpair
@@ -280,7 +295,7 @@ Enterprise Performance Computing (EPC)`
 	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	// capability
 	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	CapabilityUse   string = `capability <subcommand>`
+	CapabilityUse   string = `capability`
 	CapabilityShort string = `Manage Linux capabilities for an image`
 	CapabilityLong  string = `
   Capabilities allow you to have fine grained control over the permissions that
@@ -458,7 +473,7 @@ Enterprise Performance Computing (EPC)`
 	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	// instance
 	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	InstanceUse   string = `instance <subcommand>`
+	InstanceUse   string = `instance`
 	InstanceShort string = `Manage containers running as services`
 	InstanceLong  string = `
   Instances allow you to run containers as background processes. This can be
@@ -795,7 +810,7 @@ found at:
 	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	// OCI
 	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	OciUse   string = `oci <subcommand>`
+	OciUse   string = `oci`
 	OciShort string = `Manage OCI containers`
 	OciLong  string = `
   Allow you to manage containers from OCI bundle directories.
