b7000b63ca98778b600fc366596f5b6bbe3426b8
WestleyK <westley@sylabs.io>
2019-04-01 13:00:45 -0700

    Updated docs

--- docs/content.go
+++ docs/content.go
@@ -204,17 +204,15 @@ Enterprise Performance Computing (EPC)`
 	KeyUse   string = `key [key options...]`
 	KeyShort string = `Manage OpenPGP keys`
 	KeyLong  string = `
-  The 'key' command allows you to manage local OpenPGP key stores by creating
-  a new store and new key pairs. You can also list available keys from the
-  default store. Finally, the key command offers subcommands to communicate
-  with an HKP key server to fetch and upload public keys.`
+  The 'key' command allows you to manage your local keys,
+  and push/pull public keys from the key server.`
 	KeyExample string = `
   All group commands have their own help output:
 
   $ singularity help key newpair
   $ singularity key list --help`
 
-	// keys : for the hidden `keys` command
+	// keys is for the hidden 'keys' command
 	KeysUse string = `keys [keys options...]`
 
 	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -233,7 +231,7 @@ Enterprise Performance Computing (EPC)`
 	// key newpair
 	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	KeyNewPairUse   string = `newpair`
-	KeyNewPairShort string = `Create a new OpenPGP key pair`
+	KeyNewPairShort string = `Create a new key pair`
 	KeyNewPairLong  string = `
   The 'key newpair' command allows you to create a new key or public/private
   keys to be stored in the default user local key store location (e.g., 
@@ -245,10 +243,10 @@ Enterprise Performance Computing (EPC)`
 	// key list
 	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	KeyListUse   string = `list`
-	KeyListShort string = `List keys from the default key store`
+	KeyListShort string = `List keys in your local keyring`
 	KeyListLong  string = `
-  The 'key list' command allows you to list public/private key pairs from the 
-  default user local key store location (e.g., $HOME/.singularity/sypgp).`
+  The 'key list' command allows you to list public/private keys from the 
+  default user local keyring location (i.e., $HOME/.singularity/sypgp).`
 	KeyListExample string = `
   $ singularity key list`
 
@@ -256,7 +254,7 @@ Enterprise Performance Computing (EPC)`
 	// key search
 	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	KeySearchUse   string = `search [search options...] <search_string>`
-	KeySearchShort string = `Search for keys matching string argument`
+	KeySearchShort string = `Search for keys on a key server`
 	KeySearchLong  string = `
   The 'key search' command allows you to connect to a key server and look for
   public keys matching the argument passed to the command line. You can
@@ -275,7 +273,7 @@ Enterprise Performance Computing (EPC)`
 	// key pull
 	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	KeyPullUse   string = `pull [pull options...] <fingerprint>`
-	KeyPullShort string = `Fetch an OpenPGP public key from a key server`
+	KeyPullShort string = `Download a public key from a key server`
 	KeyPullLong  string = `
   The 'key pull' command allows you to connect to a key server look for and 
   download a public key. Key rings are stored into (e.g., 
@@ -287,7 +285,7 @@ Enterprise Performance Computing (EPC)`
 	// key push
 	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	KeyPushUse   string = `push [push options...] <fingerprint>`
-	KeyPushShort string = `Upload an OpenPGP public key to a key server`
+	KeyPushShort string = `Upload a public key to a key server`
 	KeyPushLong  string = `
   The 'key push' command allows you to connect to a key server and upload 
   public keys from the local key store.`
@@ -298,7 +296,7 @@ Enterprise Performance Computing (EPC)`
 	// key remove
 	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	KeyRemoveUse     string = `remove <fingerprint>`
-	KeyRemoveShort   string = `Remove a local public key`
+	KeyRemoveShort   string = `Remove a local public key from your keyring`
 	KeyRemoveLong    string = `The 'key remove' command will remove a local public key.`
 	KeyRemoveExample string = `
   $ singularity key remove D87FE3AF5C1F063FCBCC9B02F812842B5EEE5934`
