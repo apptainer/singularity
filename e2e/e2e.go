0b4a72e04d8dca95c313edf807ce7ce952dd7745
Eduardo Arango <eduardo@sylabs.io>
2019-04-15 18:42:36 -0500

    base e2e framework
    Signed-off-by: Eduardo Arango <eduardo@sylabs.io>

---
+++ e2e/e2e.go
@@ -0,0 +1,28 @@
+// Copyright (c) 2019, Sylabs Inc. All rights reserved.
+// This software is licensed under a 3-clause BSD license. Please consult the
+// LICENSE.md file distributed with the sources of this project regarding your
+// rights to use or distribute this software.
+
+package e2e
+
+import (
+	"os"
+	"testing"
+
+	"github.com/sylabs/singularity/internal/pkg/buildcfg"
+	useragent "github.com/sylabs/singularity/pkg/util/user-agent"
+)
+
+// RunE2ETests is the main func to trigger the test suite
+func RunE2ETests(t *testing.T) {
+	if os.Getenv("SINGULARITY_E2E") == "" {
+		t.Skip("Skipping e2e tests, SINGULARITY_E2E not set")
+	} else {
+		t.Log("Running E2E tests for Singularity")
+		Run(t)
+	}
+}
+
+func init() {
+	useragent.InitValue(buildcfg.PACKAGE_NAME, buildcfg.PACKAGE_VERSION)
+}
