3fd85eb95489386ea6eb46ac4fc205bdbb5a9c7e
Geoffroy Vallee <geoffroy.vallee@gmail.com>
2019-05-01 19:15:33 -0400

    Switch two calls to sylog.Fatalf to returning errors in order to avoid code to exit during testing

--- internal/app/singularity/cache_list_linux.go
+++ internal/app/singularity/cache_list_linux.go
@@ -53,9 +53,9 @@ func listTypeCache(printList bool, cacheType string) (int, int64, error) {
 	case "oci":
 		cachePath = cache.OciTemp()
 	case "":
-		sylog.Fatalf("No cache type specifyed")
+		return 0, 0, fmt.Errorf("no cache type specifyed")
 	default:
-		sylog.Fatalf("Not a valid type: %v", cacheType)
+		return 0, 0, fmt.Errorf("not a valid type: %s", cacheType)
 	}
 
 	cacheTmp, err := ioutil.ReadDir(cachePath)
