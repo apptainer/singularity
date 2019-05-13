ae666ea2edf1db88810c9e9177ce0f68d516000a
Egbert Eich <eich@suse.com>
2019-05-13 20:51:42 +0200

    Support multi-line bootdef settings
    Allow to split longer bootdef setting definitions accross lines:
    If a line ends with '\' as the last character concatenate it with
    the next line. If the characters before the trailing '\' is the
    string "\n", replace it by a newline.
    
    Signed-off-by: Egbert Eich <eich@suse.com>

--- pkg/build/types/parser/deffile.go
+++ pkg/build/types/parser/deffile.go
@@ -334,22 +334,41 @@ func doHeader(h string, d *types.Definition) (err error) {
 	h = strings.TrimSpace(h)
 	toks := strings.Split(h, "\n")
 	header := make(map[string]string)
+	keyCont, valCont := "", ""
 
 	for _, line := range toks {
+		var key, val string
 		// skip empty or comment lines
 		if line = strings.TrimSpace(line); line == "" || strings.Index(line, "#") == 0 {
+			if len(keyCont) > 0 {
+				d.Header[keyCont] = valCont
+				keyCont, valCont = "", ""
+			}
 			continue
 		}
 
 		// trim any comments on header lines
 		trimLine := strings.Split(line, "#")[0]
+		if len(valCont) == 0 {
+			linetoks := strings.SplitN(trimLine, ":", 2)
+			if len(linetoks) == 1 {
+				return fmt.Errorf("header key %s had no val", linetoks[0])
+			}
 
-		linetoks := strings.SplitN(trimLine, ":", 2)
-		if len(linetoks) == 1 {
-			return fmt.Errorf("header key %s had no val", linetoks[0])
+			key, val = strings.ToLower(strings.TrimSpace(linetoks[0])), strings.TrimSpace(linetoks[1])
+		} else {
+			key, val = keyCont, valCont+strings.TrimSpace(trimLine)
+			keyCont, valCont = "", ""
 		}
-
-		key, val := strings.ToLower(strings.TrimSpace(linetoks[0])), strings.TrimSpace(linetoks[1])
+		// continuation
+		if strings.HasSuffix(val, "\\") {
+			keyCont = key
+			valCont = strings.TrimSuffix(val, "\\")
+			if strings.HasSuffix(valCont, "\\n") {
+				valCont = strings.TrimSuffix(valCont, "\\n") + "\n"
+			}
+			continue
+		}
 		if _, ok := validHeaders[key]; !ok {
 			return fmt.Errorf("invalid header keyword found: %s", key)
 		}
