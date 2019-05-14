3642fae637447c461c2eb1471458e4446bef385f
WestleyK <westley@sylabs.io>
2019-05-14 10:18:58 -0700

    Updated warning message per @jscook2345 request

--- cmd/internal/cli/push.go
+++ cmd/internal/cli/push.go
@@ -112,9 +112,9 @@ var PushCmd = &cobra.Command{
 
 				// if its not signed, print a warning
 				if !imageSigned {
-					sylog.Infof("TIP: Learn how to sign your own containers by running 'singularity help sign'")
-					fmt.Fprintf(os.Stderr, "\nYou can avoid this by using the '-U' flag\n")
-					sylog.Fatalf("Unable to upload container: image not signed")
+					fmt.Printf("TIP: You can push unsigned images with 'singularity push -U %s'.\n", file)
+					fmt.Printf("TIP: Learn how to sign your own containers by using 'singularity help sign'\n\n")
+					sylog.Fatalf("Unable to upload container: unable to verify signature")
 					os.Exit(3)
 				}
 			} else {
