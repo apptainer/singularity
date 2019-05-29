f1c9d7145bcc1781c02084874d099a67723797d9
Mike Frisch <mikef17@gmail.com>
2019-05-29 16:39:49 -0400

    Mods to support scs-library-client v0.2.0

---
+++ vendor/github.com/sylabs/scs-library-client/client/request.go
@@ -0,0 +1,16 @@
+// Copyright (c) 2019, Sylabs Inc. All rights reserved.
+// This software is licensed under a 3-clause BSD license. Please consult the
+// LICENSE.md file distributed with the sources of this project regarding your
+// rights to use or distribute this software.
+
+package client
+
+// UploadImageRequest is sent to initiate V2 image upload
+type UploadImageRequest struct {
+	Size int64 `json:"filesize"`
+}
+
+// UploadImageCompleteRequest is sent to complete V2 image upload; it is
+// currently unused.
+type UploadImageCompleteRequest struct {
+}
