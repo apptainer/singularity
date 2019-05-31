01dc1b9ff7c359e9f6b8c51fafe553e346d93d96
Geoffroy Vallee <geoffroy.vallee@gmail.com>
2019-05-31 10:12:12 -0400

    Updates based on adam-tri's comments

--- internal/pkg/test/cache.go
+++ internal/pkg/test/cache.go
@@ -8,36 +8,44 @@ package test
 import (
 	"io/ioutil"
 	"os"
+	"syscall"
 	"testing"
 )
 
-// SetCacheDir creates a new image cache in the context of testing
-// and set the appropriate environment variable to ensure that the
-// temporary cache is used by the caller. Note that the context of
-// the directory that is passed in will be deleted when calling
-// CleanCacheDir().
+// SetCacheDir creates a new temporary image cache directory in the
+// directory basedir and returns the path of the new directory. If
+// basedir is an empty string, SetCacheDir uses the default directory
+// for temporary files (see os.TempDir). It is the caller's responsibility
+// to remove the directory when no longer needed, which can be accomplished
+// by calling CleanCacheDir
 func SetCacheDir(t *testing.T, basedir string) string {
-	if basedir == "" {
-		// We create a unique temporary directory for the image cache since Go run
-		// tests concurrently and since SINGULARITY_TESTBASEDIR is a static directory.
-		dir, err := ioutil.TempDir("", "image_cache-")
-		if err != nil {
-			t.Fatalf("failed to create temporary directory: %s", err)
-		}
-
-		return dir
+	// We create a unique temporary directory for the image cache since Go run
+	// tests concurrently.
+	dir, err := ioutil.TempDir(basedir, "image_cache-")
+	if err != nil {
+		t.Fatalf("failed to create temporary directory: %s", err)
 	}
 
-	return basedir
+	// Update the access mode to the directory since ioutil.TempDir()
+	// creates a new directory with a different access mode than the
+	// one we want here.
+	oldmask := syscall.Umask(0)
+	defer syscall.Umask(oldmask)
+	err = os.Chmod(dir, 0755)
+	if err != nil {
+		t.Fatalf("failed to change permission of %s: %s", dir, err)
+	}
+
+	return dir
 }
 
-// CleanCacheDir deleted the temporary cache that was created for
-// testing purposes.
-func CleanCacheDir(t *testing.T, basedir string) {
-	// We clean the image cache only if we are not using the
-	// default cache; we do not want to clean the developer's
-	// default cache.
-	if basedir != "" {
-		os.RemoveAll(basedir)
+// CleanCacheDir deleted the temporary image cache that was created for
+// testing purposes. CleanCacheDir will fail the test if an error occurs
+// during its execution.
+func CleanCacheDir(t *testing.T, path string) {
+	// Note: if path is empty os.RemoveAll() will fail
+	err := os.RemoveAll(path)
+	if err != nil {
+		t.Fatalf("failed to remove directory %s: %s", path, err)
 	}
 }
