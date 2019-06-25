fbf81ca0ff95babb90c506ecfd22a9330509c38a
WestleyK <westley@sylabs.io>
2019-06-25 16:42:02 -0700

    Use '"' instead of '`', now it will take the expected output without changing anything! And removed extra code

--- e2e/inspect/inspect.go
+++ e2e/inspect/inspect.go
@@ -23,70 +23,6 @@ var testenv testingEnv
 
 const containerTesterSIF = "testdata/inspecter_container.sif"
 
-const expectedLabelsJSON = `
-{
-	"attributes": {
-		"apps": "",
-		"labels": {
-			"E2E": "AWSOME",
-			"HI": "\"HELLO WORLD\"",
-			"MAINTAINER": "\"WestleyK \u003cwestley@sylabs.io\u003e\"",
-			"e2e": "awsome",
-			"hi": "\"hello world\"",
-			"org.label-schema.build-date": "Friday_14_June_2019_16:49:57_PDT",
-			"org.label-schema.schema-version": "1.0",
-			"org.label-schema.usage": "/.singularity.d/runscript.help",
-			"org.label-schema.usage.singularity.deffile.bootstrap": "library",
-			"org.label-schema.usage.singularity.deffile.from": "alpine:latest",
-			"org.label-schema.usage.singularity.runscript.help": "/.singularity.d/runscript.help",
-			"org.label-schema.usage.singularity.version": "3.2.1-660.g4c8a84050"
-		}
-	},
-	"type": "container"
-}
-`
-
-const expectedRunscriptJSON = `
-{
-	"attributes": {
-		"apps": "",
-		"runscript": "#!/bin/sh\n\ncat /.singularity.d/runscript.help\n\n\n"
-	},
-	"type": "container"
-}
-`
-
-const expectedListAppsJSON = `
-{
-	"attributes": {
-		"apps": "hello\nworld\n"
-	},
-	"type": "container"
-}
-`
-
-const expectedTestJSON = `
-{
-	"attributes": {
-		"apps": "",
-		"test": "#!/bin/sh\n\nls /\ntest -d /\ntest -d /etc\n\n\n"
-	},
-	"type": "container"
-}
-`
-
-const expectedEnvironmentJSON = `
-{
-	"attributes": {
-		"apps": "",
-		"environment": {
-			"90-environment.sh": "#!/bin/sh\n#Custom environment shell code should follow\n\n\nexport test=\"testing\"\nexport e2e=\"e2e testing\"\n\n\n"
-		}
-	},
-	"type": "container"
-}
-`
-
 func runInspectCommand(inspectType string) ([]byte, error) {
 	argv := []string{"inspect", "--json", inspectType, containerTesterSIF}
 	cmd := exec.Command(testenv.CmdPath, argv...)
@@ -105,121 +41,96 @@ func singularityInspect(t *testing.T) {
 			name:      "label maintainer",
 			insType:   "--labels",
 			json:      []string{"attributes", "labels", "MAINTAINER"},
-			expectOut: `"WestleyK <westley@sylabs.io>"`,
+			expectOut: "\"WestleyK <westley@sylabs.io>\"",
 		},
 		{
 			name:      "label",
 			insType:   "--labels",
 			json:      []string{"attributes", "labels", "E2E"},
-			expectOut: `AWSOME`,
+			expectOut: "AWSOME",
 		},
 		{
 			name:      "label",
 			insType:   "--labels",
 			json:      []string{"attributes", "labels", "HI"},
-			expectOut: `"HELLO WORLD"`,
+			expectOut: "\"HELLO WORLD\"",
 		},
 		{
 			name:      "label",
 			insType:   "--labels",
 			json:      []string{"attributes", "labels", "e2e"},
-			expectOut: `awsome`,
+			expectOut: "awsome",
 		},
 		{
 			name:      "label",
 			insType:   "--labels",
 			json:      []string{"attributes", "labels", "hi"},
-			expectOut: `"hello world"`,
+			expectOut: "\"hello world\"",
 		},
 		{
 			name:      "label",
 			insType:   "--labels",
 			json:      []string{"attributes", "labels", "org.label-schema.usage"},
-			expectOut: `/.singularity.d/runscript.help`,
+			expectOut: "/.singularity.d/runscript.help",
 		},
 		{
 			name:      "label",
 			insType:   "--labels",
 			json:      []string{"attributes", "labels", "org.label-schema.usage.singularity.deffile.bootstrap"},
-			expectOut: `library`,
+			expectOut: "library",
 		},
 		{
 			name:      "label",
 			insType:   "--labels",
 			json:      []string{"attributes", "labels", "org.label-schema.usage.singularity.deffile.from"},
-			expectOut: `alpine:latest`,
+			expectOut: "alpine:latest",
 		},
 		{
 			name:      "label",
 			insType:   "--labels",
 			json:      []string{"attributes", "labels", "org.label-schema.usage.singularity.runscript.help"},
-			expectOut: `/.singularity.d/runscript.help`,
+			expectOut: "/.singularity.d/runscript.help",
 		},
 		{
-			name:    "runscript",
-			insType: "--runscript",
-			json:    []string{"attributes", "runscript"},
-			expectOut: `#!/bin/sh
-
-cat /.singularity.d/runscript.help
-
-
-`,
+			name:      "runscript",
+			insType:   "--runscript",
+			json:      []string{"attributes", "runscript"},
+			expectOut: "#!/bin/sh\n\ncat /.singularity.d/runscript.help\n\n\n",
 		},
 		{
-			name:    "list apps",
-			insType: "--list-apps",
-			json:    []string{"attributes", "apps"},
-			expectOut: `hello
-world
-`,
+			name:      "list apps",
+			insType:   "--list-apps",
+			json:      []string{"attributes", "apps"},
+			expectOut: "hello\nworld\n",
 		},
 		{
-			name:    "test",
-			insType: "--test",
-			json:    []string{"attributes", "test"},
-			expectOut: `#!/bin/sh
-
-ls /
-test -d /
-test -d /etc
-
-
-`,
+			name:      "test",
+			insType:   "--test",
+			json:      []string{"attributes", "test"},
+			expectOut: "#!/bin/sh\n\nls /\ntest -d /\ntest -d /etc\n\n\n",
 		},
 		{
-			name:    "environment",
-			insType: "--environment",
-			json:    []string{"attributes", "environment", "90-environment.sh"},
-			expectOut: `#!/bin/sh
-#Custom environment shell code should follow
-
-
-export test="testing"
-export e2e="e2e testing"
-
-
-`,
+			name:      "environment",
+			insType:   "--environment",
+			json:      []string{"attributes", "environment", "90-environment.sh"},
+			expectOut: "#!/bin/sh\n#Custom environment shell code should follow\n\n\nexport test=\"testing\"\nexport e2e=\"e2e testing\"\n\n\n",
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, test.WithoutPrivilege(func(t *testing.T) {
+			// Inspect the container, and get the output
 			out, err := runInspectCommand(tt.insType)
 			if err != nil {
 				t.Fatalf("unexpected failure: %s: %s", string(out), err)
 			}
 
-			// Check the E2E label in test.sif, does it match our expected output
+			// Parse the output
 			v, err := jsonparser.GetString(out, tt.json...)
 			if err != nil {
 				t.Fatalf("unable to get expected output from json: %v", err)
 			}
-			// Get the expected output, and compare them
-			//e, err := jsonparser.GetString([]byte(tt.expectOut), tt.json...)
-			//if err != nil {
-			//	t.Fatalf("unable to get expected output from json: %v", err)
-			//}
+			// Compair the output, with the expected output
 			if v != tt.expectOut {
 				t.Fatalf("unexpected failure: got: %s, expecting: %s", v, tt.expectOut)
 			}
