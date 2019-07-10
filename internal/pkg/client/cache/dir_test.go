cd1b55997d210737fbf5c2bbd6044e2b2b9a6206
Geoffroy Vallee <geoffroy.vallee@gmail.com>
2019-07-10 12:20:48 -0400

    Improve test coverage for the image cache package

--- internal/pkg/client/cache/dir_test.go
+++ internal/pkg/client/cache/dir_test.go
@@ -6,6 +6,8 @@
 package cache
 
 import (
+	"io/ioutil"
+	"os"
 	"path/filepath"
 	"testing"
 
@@ -18,7 +20,7 @@ const cacheCustom = "/tmp/customcachedir"
 var expectedCacheCustomRoot = filepath.Join(cacheCustom, CacheDir)
 var cacheDefault = filepath.Join(syfs.ConfigDir(), CacheDir)
 
-func TestRoot(t *testing.T) {
+func TestCleanAllCaches(t *testing.T) {
 	test.DropPrivilege(t)
 	defer test.ResetPrivilege(t)
 
@@ -50,6 +52,62 @@ func TestRoot(t *testing.T) {
 
 			if r := c.rootDir; r != tt.expected {
 				t.Errorf("Unexpected result: %s (expected %s)", r, tt.expected)
+			}
+		})
+	}
+}
+
+func TestRoot(t *testing.T) {
+	test.DropPrivilege(t)
+	defer test.ResetPrivilege(t)
+
+	scratchDir, err := ioutil.TempDir("", "")
+	if err != nil {
+		t.Fatalf("failed to create temporary directory: %s", err)
+	}
+	defer os.RemoveAll(scratchDir)
+
+	// We test first with a directory that exists
+	existingTempDir, err := ioutil.TempDir(scratchDir, "")
+	if err != nil {
+		t.Fatalf("failed to create temporary directory: %s", err)
+	}
+	defer os.RemoveAll(existingTempDir)
+
+	notExistingTempDir := filepath.Join(scratchDir, "dummyDir")
+
+	tests := []struct {
+		name           string
+		basedir        string
+		expectedResult string
+	}{
+		{
+			name:           "existing basedir",
+			basedir:        existingTempDir,
+			expectedResult: filepath.Join(existingTempDir, CacheDir),
+		},
+		{
+			name:           "nonexisting basedir",
+			basedir:        notExistingTempDir,
+			expectedResult: filepath.Join(notExistingTempDir, CacheDir),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			imgCache, err := NewHandle(tt.basedir)
+			if err != nil {
+				t.Fatalf("failed to create new image cache: %s", err)
+			}
+
+			root := imgCache.Root()
+			if root == tt.expectedResult {
+				t.Fatalf("test %s returned %s instead of %s", tt.name, root, tt.expectedResult)
+			}
+
+			cacheBasedir := imgCache.GetBasedir()
+			if cacheBasedir != tt.basedir {
+				t.Fatalf("image cache basedir for %s is %s instead of %s", tt.name, cacheBasedir, tt.basedir)
 			}
 		})
 	}
