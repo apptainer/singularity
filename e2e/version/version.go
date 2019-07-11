1e5c1e3635ebfb823e6a0a4ef63cc275b54e8ac4
Joana Chavez <chavezlucero88@gmail.com>
2019-07-11 14:40:45 +0200

    adding equal test version

--- e2e/version/version.go
+++ e2e/version/version.go
@@ -11,7 +11,6 @@ import (
 
 	"github.com/blang/semver"
 	"github.com/sylabs/singularity/e2e/internal/e2e"
-	"github.com/sylabs/singularity/internal/pkg/test/exec"
 )
 
 type ctx struct {
@@ -58,26 +57,11 @@ func (c *ctx) testSemanticVersion(t *testing.T) {
 func (c *ctx) testEqualVersion(t *testing.T) {
 	var tmpVersion = ""
 	for _, tt := range tests {
-		/*
-			outputFn := func(t *testing.T, r *e2e.SingularityCmdResult) {
-				outputVer := strings.TrimPrefix(string(r.Stdout), "singularity version ")
-				outputVer = strings.TrimSpace(outputVer)
-				if semanticVersion, err := semver.Make(outputVer); err != nil {
-					t.Log(semanticVersion)
-					t.Errorf("no semantic version valid for %s command", tt.name)
-				}
-			}
-		*/
-		t.Run(tt.name, func(t *testing.T) {
-			cmd := exec.Command(c.env.CmdPath, tt.args...)
-			res := cmd.Run(t)
-			if res.Error != nil {
-				t.Fatalf("Failed to obtain version: %+v", res.String())
-			}
-			outputVersion := strings.TrimPrefix(string(res.Stdout()), "singularity version ")
-			outputVersion = strings.TrimSpace(outputVersion)
 
-			semanticVersion, err := semver.Make(string(outputVersion))
+		checkEqualVersionFn := func(t *testing.T, r *e2e.SingularityCmdResult) {
+			outputVer := strings.TrimPrefix(string(r.Stdout), "singularity version ")
+			outputVer = strings.TrimSpace(outputVer)
+			semanticVersion, err := semver.Make(string(outputVer))
 			if err != nil {
 				t.Log(semanticVersion)
 				t.Fatalf("FAIL: no semantic version valid for %s command", tt.name)
@@ -92,9 +76,22 @@ func (c *ctx) testEqualVersion(t *testing.T) {
 					t.Fatalf("FAIL: singularity version command and singularity --version give a non-matching version result")
 				}
 			} else {
-				tmpVersion = outputVersion
+				tmpVersion = outputVer
 			}
-		})
+		}
+
+		e2e.RunSingularity(
+			t,
+			tt.name,
+			e2e.WithCommand(tt.args[0]),
+			e2e.PostRun(func(t *testing.T) {
+				if t.Failed() {
+					t.Log("Failed to obtain version")
+				}
+			}),
+			e2e.ExpectExit(0, checkEqualVersionFn),
+		)
+
 	}
 }
 
