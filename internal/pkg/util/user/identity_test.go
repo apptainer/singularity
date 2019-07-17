7e6c20d594cf51893ce73739f1fa1e1e6e092886
Cedric Clerget <cedric.clerget@gmail.com>
2019-07-17 12:32:48 +0200

    Use newuidmap/newgidmap as fakeroot fallback for unprivileged installation.
    Fakeroot configuration is now handled by /etc/subxid files.
    
    Introduce new SINGULARITY_SUID_INSTALL constant to
    ease setup detection and use starter binary accordingly.
    
    Fix fakeroot issues:
    - fix home directory (previously used user directory)
    - with HOME environment variable while joining an instance
    
    Co-Authored-By: WestleyK <westley@sylabs.io>

--- internal/pkg/util/user/identity_test.go
+++ internal/pkg/util/user/identity_test.go
@@ -1,14 +1,12 @@
-/*
-  Copyright (c) 2018, Sylabs, Inc. All rights reserved.
-
-  This software is licensed under a 3-clause BSD license.  Please
-  consult LICENSE.md file distributed with the sources of this project regarding
-  your rights to use or distribute this software.
-*/
+// Copyright (c) 2018-2019, Sylabs Inc. All rights reserved.
+// This software is licensed under a 3-clause BSD license. Please consult the
+// LICENSE.md file distributed with the sources of this project regarding your
+// rights to use or distribute this software.
 
 package user
 
 import (
+	"os"
 	"testing"
 
 	"github.com/sylabs/singularity/internal/pkg/test"
@@ -18,11 +16,11 @@ func TestGetPwUID(t *testing.T) {
 	test.DropPrivilege(t)
 	defer test.ResetPrivilege(t)
 
-	user, err := GetPwUID(0)
+	u, err := GetPwUID(0)
 	if err != nil {
 		t.Fatalf("Failed to retrieve information for UID 0")
 	}
-	if user.Name != "root" {
+	if u.Name != "root" {
 		t.Fatalf("UID 0 doesn't correspond to root user")
 	}
 }
@@ -31,11 +29,11 @@ func TestGetPwNam(t *testing.T) {
 	test.DropPrivilege(t)
 	defer test.ResetPrivilege(t)
 
-	user, err := GetPwNam("root")
+	u, err := GetPwNam("root")
 	if err != nil {
 		t.Fatalf("Failed to retrieve information for root user")
 	}
-	if user.UID != 0 {
+	if u.UID != 0 {
 		t.Fatalf("root user doesn't have UID 0")
 	}
 }
@@ -64,4 +62,46 @@ func TestGetGrNam(t *testing.T) {
 	if group.GID != 0 {
 		t.Fatalf("root group doesn't have GID 0")
 	}
+}
+
+func testCurrent(t *testing.T, fn func() (*User, error)) {
+	uid := os.Getuid()
+
+	u, err := fn()
+	if err != nil {
+		t.Fatalf("Failed to retrieve information for current user")
+	}
+	if u.UID != uint32(uid) {
+		t.Fatalf("returned UID (%d) doesn't match current UID (%d)", uid, u.UID)
+	}
+}
+
+func TestCurrent(t *testing.T) {
+	// as a regular user
+	test.DropPrivilege(t)
+	defer test.ResetPrivilege(t)
+
+	testCurrent(t, Current)
+
+	// as root
+	test.ResetPrivilege(t)
+
+	testCurrent(t, Current)
+}
+
+func TestCurrentOriginal(t *testing.T) {
+	// as a regular user
+	test.DropPrivilege(t)
+	defer test.ResetPrivilege(t)
+
+	// to fully test CurrentOriginal, we would need
+	// to execute it from a user namespace, actually
+	// we just ensure that current user informations
+	// are returned
+	testCurrent(t, CurrentOriginal)
+
+	// as root
+	test.ResetPrivilege(t)
+
+	testCurrent(t, CurrentOriginal)
 }
