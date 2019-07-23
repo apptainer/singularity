affea0af76f9b24b5e09b306ebe8d8aeb4bfd412
Geoffroy Vallee <geoffroy.vallee@gmail.com>
2019-07-23 11:49:30 -0400

    Reorganize code to avoid duplication when creating a squashfs file system

---
+++ pkg/util/fs/squashfs/squashfs.go
@@ -0,0 +1,33 @@
+package squashfs
+
+import (
+	"fmt"
+	"os/exec"
+	"path/filepath"
+	"strings"
+
+	"github.com/sylabs/singularity/internal/pkg/buildcfg"
+	"github.com/sylabs/singularity/internal/pkg/runtime/engines/config"
+	singularityConfig "github.com/sylabs/singularity/pkg/runtime/engines/singularity/config"
+)
+
+// GetPath figures out where the mksquashfs binary is
+// and return an error is not available or not usable.
+func GetPath() (string, error) {
+	// Parse singularity configuration file
+	c := &singularityConfig.FileConfig{}
+	if err := config.Parser(buildcfg.SYSCONFDIR+"/singularity/singularity.conf", c); err != nil {
+		return "", fmt.Errorf("Unable to parse singularity.conf file: %s", err)
+	}
+
+	// p is either "" or the string value in the conf file
+	p := c.MksquashfsPath
+
+	// If the path contains the binary name use it as is, otherwise add mksquashfs via filepath.Join
+	if !strings.HasSuffix(c.MksquashfsPath, "mksquashfs") {
+		p = filepath.Join(c.MksquashfsPath, "mksquashfs")
+	}
+
+	// exec.LookPath functions on absolute paths (ignoring $PATH) as well
+	return exec.LookPath(p)
+}
