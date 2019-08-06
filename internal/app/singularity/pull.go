0f69ef1c7b636e2e07dee8f292d940210596b72e
cali4888 <pisarukvadim@gmail.com>
2019-08-06 13:36:33 +0300

    Add a new err ErrLibraryPullNoSign

--- internal/app/singularity/pull.go
+++ internal/app/singularity/pull.go
@@ -31,6 +31,9 @@ var (
 	// ErrLibraryPullUnsigned indicates that the interactive portion of the
 	// pull was aborted
 	ErrLibraryPullUnsigned = errors.New("failed to verify container")
+
+	// ErrLibraryPullNoSign indicates that requested image has no sign.
+	ErrLibraryPullNoSign = errors.New("image has no sign")
 )
 
 // LibraryPull will download the image specified by file from the library specified by libraryURI.
@@ -65,8 +68,9 @@ func LibraryPull(imgCache *cache.Handle, name, ref, transport, fullURI, libraryU
 		return fmt.Errorf("image does not exist in the library: %s", imageRef)
 	}
 
+	// check if image has a sign
 	if !unauthenticated && (libraryImage.Signed != nil && !*libraryImage.Signed) {
-		return ErrLibraryPullUnsigned
+		return ErrLibraryPullNoSign
 	}
 
 	if noCache {
