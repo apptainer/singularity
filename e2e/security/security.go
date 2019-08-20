89165d7f87da7b682a2f19810d86850335e36056
WestleyK <westley@sylabs.io>
2019-08-20 14:49:23 -0700

    Added working tests for SecurityConfOwnership + finishing up tests (removing debugging)

--- e2e/security/security.go
+++ e2e/security/security.go
@@ -87,10 +87,6 @@ func (c *ctx) testSecurityUnpriv(t *testing.T) {
 		},
 	}
 
-	verifyOutput := func(t *testing.T, r *e2e.SingularityCmdResult) {
-		t.Logf("INFOOOOOOOOO: %s\n", r.Stdout)
-	}
-
 	for _, tt := range tests {
 		optArgs := []string{}
 		optArgs = append(optArgs, tt.opts...)
@@ -102,8 +98,7 @@ func (c *ctx) testSecurityUnpriv(t *testing.T) {
 			e2e.AsSubtest(tt.name),
 			e2e.WithCommand("exec"),
 			e2e.WithArgs(optArgs...),
-			e2e.ExpectExit(tt.expectExit, verifyOutput),
-			//e2e.ExpectExit(tt.expectExit, e2e.ExpectOutput(e2e.ContainMatch, tt.stdout)),
+			e2e.ExpectExit(tt.expectExit),
 		)
 
 	}
@@ -112,34 +107,22 @@ func (c *ctx) testSecurityUnpriv(t *testing.T) {
 // testSecurityPriv tests security flag fuctionality for singularity exec with elevated privileges
 func (c *ctx) testSecurityPriv(t *testing.T) {
 	tests := []struct {
-		name         string
-		argv         []string
-		opts         []string
-		notExpecting string
-		expectExit   int
+		name       string
+		argv       []string
+		opts       []string
+		expectExit int
 	}{
-		{
-			name:       "TESTSSSSSSSSSSS",
-			argv:       []string{"id"},
-			opts:       []string{},
-			expectExit: 0,
-		},
-
 		// taget UID/GID
 		{
-			name: "Set_uid",
-			//argv: []string{"id", "-u"},
-			argv: []string{"id", "-u", "|", "grep", "99"},
-			opts: []string{"--security", "uid:99"},
-			//notExpecting: "99",
+			name:       "Set_uid",
+			argv:       []string{"id", "-u", "|", "grep", "99"},
+			opts:       []string{"--security", "uid:99"},
 			expectExit: 1,
 		},
 		{
-			name: "Set_gid",
-			argv: []string{"id", "-g", "|", "grep", "99"},
-			//argv:         []string{"id", "-g"},
-			opts: []string{"--security", "gid:99"},
-			//notExpecting: "98",
+			name:       "Set_gid",
+			argv:       []string{"id", "-g", "|", "grep", "99"},
+			opts:       []string{"--security", "gid:99"},
 			expectExit: 1,
 		},
 		// seccomp from json file
@@ -171,13 +154,6 @@ func (c *ctx) testSecurityPriv(t *testing.T) {
 	}
 
 	for _, tt := range tests {
-		verifyOutput := func(t *testing.T, r *e2e.SingularityCmdResult) {
-			t.Logf("INFOOOOOOOOO2222222: %s\n", string(r.Stdout))
-			if tt.notExpecting != "" && tt.notExpecting != string(r.Stdout) {
-				t.Fatalf("unexpected output; expecting: %s to not be: %s", tt.notExpecting, string(r.Stdout))
-			}
-		}
-
 		optArgs := []string{}
 		optArgs = append(optArgs, tt.opts...)
 		optArgs = append(optArgs, c.env.ImagePath)
@@ -189,7 +165,7 @@ func (c *ctx) testSecurityPriv(t *testing.T) {
 			e2e.WithPrivileges(true),
 			e2e.WithCommand("exec"),
 			e2e.WithArgs(optArgs...),
-			e2e.ExpectExit(tt.expectExit, verifyOutput),
+			e2e.ExpectExit(tt.expectExit),
 		)
 
 	}
@@ -199,17 +175,9 @@ func (c *ctx) testSecurityPriv(t *testing.T) {
 func (c *ctx) testSecurityConfOwnership(t *testing.T) {
 	configFile := buildcfg.SINGULARITY_CONF_FILE
 
-	//	// Change file ownership (do not try this at home)
-	//	err := os.Chown(configFile, 1001, 0)
-	//	if err != nil {
-	//		t.Fatalf("failed to change owner for: %s: %s", configFile, err)
-	//	}
-
-	// try to run
 	c.env.RunSingularity(
 		t,
 		e2e.AsSubtest("non root config"),
-		e2e.WithPrivileges(true),
 		e2e.PreRun(func(t *testing.T) {
 			e2e.Privileged(func(t *testing.T) {
 				// Change file ownership (do not try this at home)
@@ -217,7 +185,7 @@ func (c *ctx) testSecurityConfOwnership(t *testing.T) {
 				if err != nil {
 					t.Fatalf("failed to change owner for: %s: %s", configFile, err)
 				}
-			})
+			})(t)
 		}),
 		e2e.PostRun(func(t *testing.T) {
 			e2e.Privileged(func(t *testing.T) {
@@ -226,18 +194,12 @@ func (c *ctx) testSecurityConfOwnership(t *testing.T) {
 				if err != nil {
 					t.Fatalf("failed to change config file owner to root: %s: %s", configFile, err)
 				}
-			})
+			})(t)
 		}),
 		e2e.WithCommand("exec"),
 		e2e.WithArgs([]string{c.env.ImagePath, "/bin/true"}...),
-		e2e.ExpectExit(0),
+		e2e.ExpectExit(255),
 	)
-
-	//	// return file ownership to normal
-	//	err := os.Chown(configFile, 0, 0)
-	//	if err != nil {
-	//		t.Fatalf("failed to change config file owner to root: %s: %s", configFile, err)
-	//	}
 }
 
 // RunE2ETests is the main func to trigger the test suite
@@ -251,6 +213,6 @@ func RunE2ETests(env e2e.TestEnv) func(*testing.T) {
 
 		t.Run("singularitySecurityUnpriv", c.testSecurityUnpriv)
 		t.Run("singularitySecurityPriv", c.testSecurityPriv)
-		//		t.Run("testSecurityConfOwnership", c.testSecurityConfOwnership)
+		t.Run("testSecurityConfOwnership", c.testSecurityConfOwnership)
 	}
 }
