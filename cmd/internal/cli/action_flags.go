f522b5cb268e41cc8586e7bb7570927f9006ad0b
Dave Dykstra <2129743+DrDaveD@users.noreply.github.com>
2019-08-23 12:46:52 -0500

    make --fusemount hidden from help

--- cmd/internal/cli/action_flags.go
+++ cmd/internal/cli/action_flags.go
@@ -315,7 +315,7 @@ var actionPEMPathFlag = cmdline.Flag{
 	Usage:        "Enter an path to a PEM formated RSA key for an encrypted container",
 }
 
-// --fusemount
+// --fusemount, hidden for now while experimental
 var actionFuseMountFlag = cmdline.Flag{
 	ID:           "actionFuseMountFlag",
 	Value:        &FuseMount,
@@ -324,6 +324,7 @@ var actionFuseMountFlag = cmdline.Flag{
 	Usage:        "a FUSE filesystem mount specification. Begins with the source of the FUSE driver followed by a colon; currently must be 'container:'.  After the colon is the command to run to implement a libfuse3- based filesystem. The last space- separated part of the string is a mountpoint that will be pre-mounted and replaced with a /dev/fd path to the FUSE file descriptor.  Implies --pid.",
 	EnvKeys:      []string{"FUSESPEC"},
 	ExcludedOS:   []string{cmdline.Darwin},
+	Hidden:       true,
 }
 
 // hidden flags to handle docker credentials
