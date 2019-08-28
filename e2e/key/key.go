eab55ddab300130a9ef4a026d161d6620a21d503
Geoffroy Vallee <geoffroy.vallee@gmail.com>
2019-08-28 15:48:06 -0400

    fix problem after accepting suggestions

--- e2e/key/key.go
+++ e2e/key/key.go
@@ -324,7 +324,7 @@ func (c *ctx) singularityKeyPush(t *testing.T) {
 		expectedRegex string
 	}{
 		{
-￼			name:          "push help",
+			name:          "push help",
 			cmdArgs:       []string{"--help"},
 			expectedExit:  0,
 			expectedRegex: `^Upload a public key to a key server`,
