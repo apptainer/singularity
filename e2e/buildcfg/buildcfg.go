885166fee68aca5528032f411b20247775f508cd
Geoffroy Vallee <geoffroy.vallee@gmail.com>
2019-08-30 10:40:17 -0400

    Add an e2e test for the buildcfg command. Only the positive case at the moment in order to improve coverage.

---
+++ e2e/buildcfg/buildcfg.go
@@ -0,0 +1,55 @@
+// Copyright (c) 2019, Sylabs Inc. All rights reserved.
+// This software is licensed under a 3-clause BSD license. Please consult the
+// LICENSE.md file distributed with the sources of this project regarding your
+// rights to use or distribute this software.
+
+package buildcfg
+
+import (
+	"testing"
+
+	"github.com/sylabs/singularity/e2e/internal/e2e"
+)
+
+type buildcfgTests struct {
+	env e2e.TestEnv
+}
+
+func (c *buildcfgTests) buildcfgTests(t *testing.T) {
+	tests := []struct {
+		name           string
+		cmdArgs        []string
+		expectedOutput string
+	}{
+		{
+			name:           "help",
+			cmdArgs:        []string{"--help"},
+			expectedOutput: "Output the currently set compile-time parameters",
+		},
+	}
+
+	for _, tt := range tests {
+		c.env.RunSingularity(
+			t,
+			e2e.AsSubtest(tt.name),
+			e2e.WithProfile(e2e.UserProfile),
+			e2e.WithCommand("buildcfg"),
+			e2e.WithArgs(tt.cmdArgs...),
+			e2e.ExpectExit(
+				0,
+				e2e.ExpectOutput(e2e.RegexMatch, `^`+tt.expectedOutput),
+			),
+		)
+	}
+}
+
+// E2ETests is the main func to trigger the test suite
+func E2ETests(env e2e.TestEnv) func(*testing.T) {
+	c := &buildcfgTests{
+		env: env,
+	}
+
+	return func(t *testing.T) {
+		t.Run("buildcfgHelp", c.buildcfgTests)
+	}
+}
