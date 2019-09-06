997fbd62bb95c7a109f0c75eb65956ce032167e5
sashayakovtseva <sasha@sylabs.io>
2019-09-06 17:06:30 +0300

    Clean all envs that contain PATH
    Also fix wrong tests in env package.

--- internal/pkg/util/env/env_test.go
+++ internal/pkg/util/env/env_test.go
@@ -15,27 +15,52 @@ func TestSetFromList(t *testing.T) {
 	test.DropPrivilege(t)
 	defer test.ResetPrivilege(t)
 
-	type args struct {
-		environ []string
-	}
-	tests := []struct {
+	tt := []struct {
 		name    string
-		args    args
+		environ []string
 		wantErr bool
 	}{
-		{name: "Good_envs", args: args{[]string{"LD_LIBRARY_PATH=/.singularity.d/libs", "HOME=/home/tester",
-			"PS1=test", "TERM=xterm-256color", "PATH=/usr/games:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
-			"LANG=C", "SINGULARITY_CONTAINER=/tmp/lolcow.sif", "PWD=/tmp", "LC_ALL=C",
-			"SINGULARITY_NAME=lolcow.sif"}}, wantErr: false},
-		{name: "Bad_envs", args: args{[]string{"LD_LIBRARY_PATH=/.singularity.d/libs", "HOME=/home/tester",
-			"PS1=test", "TERM=xterm-256color", "PATH=/usr/games:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
-			"LANG=C", "SINGULARITY_CONTAINER=/tmp/lolcow.sif", "TEST", "LC_ALL=C",
-			"SINGULARITY_NAME=lolcow.sif"}}, wantErr: true},
+		{
+			name: "all ok",
+			environ: []string{
+				"LD_LIBRARY_PATH=/.singularity.d/libs",
+				"HOME=/home/tester",
+				"PS1=test",
+				"TERM=xterm-256color",
+				"PATH=/usr/games:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+				"LANG=C",
+				"SINGULARITY_CONTAINER=/tmp/lolcow.sif",
+				"PWD=/tmp",
+				"LC_ALL=C",
+				"SINGULARITY_NAME=lolcow.sif",
+			},
+			wantErr: false,
+		},
+		{
+			name: "bad envs",
+			environ: []string{
+				"LD_LIBRARY_PATH=/.singularity.d/libs",
+				"HOME=/home/tester",
+				"PS1=test",
+				"TERM=xterm-256color",
+				"PATH=/usr/games:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+				"LANG=C",
+				"SINGULARITY_CONTAINER=/tmp/lolcow.sif",
+				"TEST",
+				"LC_ALL=C",
+				"SINGULARITY_NAME=lolcow.sif",
+			},
+			wantErr: true,
+		},
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if err := SetFromList(tt.args.environ); (err != nil) != tt.wantErr {
-				t.Errorf("SetFromList() error = %v, wantErr %v", err, tt.wantErr)
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			err := SetFromList(tc.environ)
+			if tc.wantErr && err == nil {
+				t.Fatalf("Expected error, but got nil")
+			}
+			if !tc.wantErr && err != nil {
+				t.Errorf("Unexpected error: %v", err)
 			}
 		})
 	}
