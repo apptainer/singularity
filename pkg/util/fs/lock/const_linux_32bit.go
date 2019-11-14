d21494104bc1e7c1824cadd8f56147e02abdfeb4
Cedric Clerget <cedric.clerget@gmail.com>
2019-11-14 14:34:22 +0100

    Fix F_SETLK64 bad value on ppc64 arch with fcntl syscall

--- pkg/util/fs/lock/const_linux_32bit.go
+++ pkg/util/fs/lock/const_linux_32bit.go
@@ -3,6 +3,8 @@
 // LICENSE file distributed with the sources of this project regarding your
 // rights to use or distribute this software.
 
+// +build linux,386 linux,arm linux,mips linux,mipsle
+
 package lock
 
 import "golang.org/x/sys/unix"
