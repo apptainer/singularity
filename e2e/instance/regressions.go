01f63eda676d181c1674ee32377d3d4f143e0a36
Cedric Clerget <cedric.clerget@gmail.com>
2020-02-11 12:55:49 +0100

    Move comm name for setuid workflow only during instance join as it avoid to join instance started with --boot.

---
+++ e2e/instance/regressions.go
@@ -0,0 +1,42 @@
+// Copyright (c) 2020, Sylabs Inc. All rights reserved.
+// This software is licensed under a 3-clause BSD license. Please consult the
+// LICENSE.md file distributed with the sources of this project regarding your
+// rights to use or distribute this software.
+
+package instance
+
+import (
+	"fmt"
+	"testing"
+
+	uuid "github.com/satori/go.uuid"
+	"github.com/sylabs/singularity/e2e/internal/e2e"
+)
+
+func (c *ctx) issue5033(t *testing.T) {
+	e2e.EnsureImage(t, c.env)
+
+	c.profile = e2e.RootProfile
+
+	// pick up a random name
+	instanceName := uuid.NewV4().String()
+	joinName := fmt.Sprintf("instance://%s", instanceName)
+
+	c.env.RunSingularity(
+		t,
+		e2e.WithProfile(c.profile),
+		e2e.WithCommand("instance start"),
+		e2e.WithArgs("--boot", c.env.ImagePath, instanceName),
+		e2e.ExpectExit(0),
+	)
+
+	c.env.RunSingularity(
+		t,
+		e2e.WithProfile(c.profile),
+		e2e.WithCommand("exec"),
+		e2e.WithArgs(joinName, "/bin/true"),
+		e2e.ExpectExit(0),
+	)
+
+	c.stopInstance(t, instanceName)
+}
