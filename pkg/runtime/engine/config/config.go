364ccce6d4b83319ac5ac0ff5344a0bfe02ab1a3
Cedric Clerget <cedric.clerget@gmail.com>
2020-03-12 10:43:14 +0100

    Add runtime plugin configuration

--- pkg/runtime/engine/config/config.go
+++ pkg/runtime/engine/config/config.go
@@ -5,6 +5,12 @@
 
 package config
 
+import (
+	"encoding/json"
+
+	"github.com/sylabs/singularity/pkg/plugin"
+)
+
 // Common provides the basis for all engine configs. Anything that can not be
 // properly described through the OCI config can be stored as a generic JSON []byte.
 type Common struct {
@@ -12,6 +18,33 @@ type Common struct {
 	ContainerID string `json:"containerID"`
 	// EngineConfig is the raw JSON representation of the Engine's underlying config.
 	EngineConfig EngineConfig `json:"engineConfig"`
+
+	// PluginConfig is the JSON raw representation of the plugin configurations.
+	PluginConfig map[string]json.RawMessage `json:"plugin"`
+}
+
+// GetPluginConfig retrieves the configuration for the corresponding plugin.
+func (c *Common) GetPluginConfig(pl plugin.Plugin, cfg interface{}) error {
+	if c.PluginConfig == nil {
+		c.PluginConfig = make(map[string]json.RawMessage)
+	}
+	if raw, found := c.PluginConfig[pl.Manifest.Name]; found {
+		return json.Unmarshal(raw, cfg)
+	}
+	return nil
+}
+
+// SetPluginConfig sets the configuration for the corresponding plugin.
+func (c *Common) SetPluginConfig(pl plugin.Plugin, cfg interface{}) error {
+	raw, err := json.Marshal(cfg)
+	if err != nil {
+		return err
+	}
+	if c.PluginConfig == nil {
+		c.PluginConfig = make(map[string]json.RawMessage)
+	}
+	c.PluginConfig[pl.Manifest.Name] = raw
+	return nil
 }
 
 // EngineConfig is a generic interface to represent the implementations of an EngineConfig.
