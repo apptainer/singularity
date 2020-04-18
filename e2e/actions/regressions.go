904234de67544e6a04d29d40a94d45091069a5c7
Shane Loretz <sloretz@osrfoundation.org>
2020-04-18 15:55:46 -0700

    Add regression test for 5211
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>

--- e2e/actions/regressions.go
+++ e2e/actions/regressions.go
@@ -335,3 +335,26 @@ func (c actionTests) issue5228(t *testing.T) {
 		e2e.ExpectExit(0),
 	)
 }
+
+// Check that home directory is not mounted under `/root` when --fakeroot and
+// --contain are both given.
+func (c actionTests) issue5211(t *testing.T) {
+	e2e.EnsureImage(t, c.env)
+
+	u := e2e.UserProfile.HostUser(t)
+
+	// Make non-hidden file in host home to check if it was mounted in container
+	canaryDir, cleanup := e2e.MakeTempDir(t, u.Dir, "singularity-issue5211-dir-", "")
+	defer cleanup(t)
+
+	canaryBasename := filepath.Base(canaryDir)
+
+	c.env.RunSingularity(
+		t,
+		e2e.WithProfile(e2e.UserProfile),
+		e2e.WithDir(u.Dir),
+		e2e.WithCommand("exec"),
+		e2e.WithArgs("--fakeroot", "--contain", c.env.ImagePath, "test", "!", "-d", filepath.Join("/root", canaryBasename)),
+		e2e.ExpectExit(0),
+	)
+}
