6f077946540d3aaf90e3c0f2afc8e256f160ecdf
David Trudgian <dave@trudgian.net>
2020-11-02 13:39:02 -0600

    Light refactor of GPU libs handling

--- pkg/util/gpu/paths.go
+++ pkg/util/gpu/paths.go
@@ -7,7 +7,6 @@ package gpu
 
 import (
 	"bufio"
-	"bytes"
 	"debug/elf"
 	"fmt"
 	"os"
@@ -21,80 +20,7 @@ import (
 
 const systemLdconfig = "/sbin/ldconfig"
 
-// nvidiaContainerCli runs `nvidia-container-cli list` and returns list of
-// libraries, ipcs and binaries for proper NVIDIA work. This may return duplicates!
-func nvidiaContainerCli(args ...string) ([]string, error) {
-	nvidiaCLIPath, err := exec.LookPath("nvidia-container-cli")
-	if err != nil {
-		return nil, fmt.Errorf("could not find nvidia-container-cli: %v", err)
-	}
-
-	var out bytes.Buffer
-	cmd := exec.Command(nvidiaCLIPath, args...)
-	cmd.Stdout = &out
-	err = cmd.Run()
-	if err != nil {
-		return nil, fmt.Errorf("could not execute nvidia-container-cli list: %v", err)
-	}
-
-	var libs []string
-	scanner := bufio.NewScanner(&out)
-	for scanner.Scan() {
-		line := strings.TrimSpace(scanner.Text())
-		if line == "" {
-			continue
-		}
-
-		if strings.Contains(line, ".so") {
-			// this is a library -> add full path to library
-			libs = append(libs, line)
-
-			// also add full path to all linked libraries of similar name
-			// for example libGLX_nvidia.so.0 -> libGLX_nvidia.so.390.116
-			var symlinkCandidates []string
-			bareLibPath := strings.SplitAfter(line, ".so")[0]
-			similarlyNamedFiles, _ := filepath.Glob(fmt.Sprintf("%s*", bareLibPath))
-
-			if len(similarlyNamedFiles) == 0 {
-				// should have at least found current lib
-				sylog.Warningf("no files found matching %s*", bareLibPath)
-				continue
-			}
-
-			// check all files with a similar name (up to .so extension) and
-			// work out which are symlinks rather than regular files
-			for _, similarlyNamedFile := range similarlyNamedFiles {
-				if fi, err := os.Lstat(similarlyNamedFile); err == nil {
-					if fi.Mode()&os.ModeSymlink == os.ModeSymlink {
-						symlinkCandidates = append(symlinkCandidates, similarlyNamedFile)
-					}
-				} else {
-					// error in Lstat
-					sylog.Warningf("error extracting file info for %s: %v", similarlyNamedFile, err)
-				}
-			}
-
-			// resolve symlinks and check if they eventually point to driver
-			for _, symlinkCandidate := range symlinkCandidates {
-				if resolvedLib, err := filepath.EvalSymlinks(symlinkCandidate); err == nil {
-					if resolvedLib == line {
-						// symlinkCandidate resolves (eventually) to required lib
-						libs = append(libs, symlinkCandidate)
-					}
-				} else {
-					// error resolving symlink?
-					sylog.Warningf("unable to resolve symlink for %s: %v", symlinkCandidate, err)
-				}
-			}
-		} else {
-			// this is a binary -> need full path
-			libs = append(libs, line)
-		}
-	}
-	return libs, nil
-}
-
-// gpuliblist returns libraries listed in a gpu lib list config file, typically
+// gpuliblist returns libraries/binaries listed in a gpu lib list config file, typically
 // located in buildcfg.SINGULARITY_CONFDIR
 func gpuliblist(configFilePath string) ([]string, error) {
 	file, err := os.Open(configFilePath)
@@ -114,120 +40,80 @@ func gpuliblist(configFilePath string) ([]string, error) {
 	return libs, nil
 }
 
-// NvidiaPaths returns a list of Nvidia libraries/binaries that should be
-// mounted into the container in order to use Nvidia GPUs
-func NvidiaPaths(configFilePath, userEnvPath string) ([]string, []string, error) {
-	if userEnvPath != "" {
-		oldPath := os.Getenv("PATH")
-		os.Setenv("PATH", userEnvPath)
-		defer os.Setenv("PATH", oldPath)
+// soLinks returns a list of versioned symlinks resolving to a specified library file
+func soLinks(libPath string) (paths []string, err error) {
+	bareLibPath := strings.SplitAfter(libPath, ".so")[0]
+	libCandidates := []string{}
+	libGlobPaths, _ := filepath.Glob(fmt.Sprintf("%s*", bareLibPath))
+	if len(libGlobPaths) == 0 {
+		// should have at least found current lib
+		return paths, fmt.Errorf("library not found: %s", libPath)
 	}
-
-	// Parse nvidia-container-cli for the necessary binaries/libs, fallback to a
-	// list of required binaries/libs if the nvidia-container-cli is unavailable
-	nvidiaFiles, err := nvidiaContainerCli("list", "--binaries", "--libraries")
-	if err != nil {
-		sylog.Verbosef("nvidiaContainerCli returned: %v", err)
-		sylog.Verbosef("Falling back to nvliblist.conf")
-
-		nvidiaFiles, err = gpuliblist(configFilePath)
-		if err != nil {
-			return nil, nil, fmt.Errorf("could not read %s: %v", filepath.Base(configFilePath), err)
+	// check all files with a similar name (up to .so extension) and
+	// work out which are symlinks rather than regular files
+	for _, lPath := range libGlobPaths {
+		if fi, err := os.Lstat(lPath); err == nil {
+			if fi.Mode()&os.ModeSymlink == os.ModeSymlink {
+				libCandidates = append(libCandidates, lPath)
+			}
+		} else {
+			sylog.Warningf("error extracting file info for %s: %v", lPath, err)
 		}
 	}
-
-	return paths(nvidiaFiles)
+	// resolve symlinks and check if they eventually point to driver
+	for _, lPath := range libCandidates {
+		if resolvedLib, err := filepath.EvalSymlinks(lPath); err == nil {
+			if resolvedLib == libPath {
+				// symlinkCandidate resolves (eventually) to required lib
+				sylog.Debugf("Idenfified %s as a symlink for %s", lPath, libPath)
+				paths = append(paths, lPath)
+			}
+		} else {
+			// error resolving symlink?
+			sylog.Warningf("unable to resolve symlink for %s: %v", lPath, err)
+		}
+	}
+	return paths, nil
 }
 
-// RocmPaths returns a list of rocm libraries/binaries that should be
-// mounted into the container in order to use AMD GPUs
-func RocmPaths(configFilePath, userEnvPath string) ([]string, []string, error) {
-	if userEnvPath != "" {
-		oldPath := os.Getenv("PATH")
-		os.Setenv("PATH", userEnvPath)
-		defer os.Setenv("PATH", oldPath)
-	}
-
-	rocmFiles, err := gpuliblist(configFilePath)
-	if err != nil {
-		return nil, nil, fmt.Errorf("could not read %s: %v", filepath.Base(configFilePath), err)
-	}
-
-	return paths(rocmFiles)
-}
-
-// paths handles generic library parsing functionality once the platform
-// specific libs/binaries have been identified
+// paths takes a list of library/binary files (absolute paths, or bare filenames) and processes them into lists of
+// resolved library and binary paths to be bound into the container.
 func paths(gpuFileList []string) ([]string, []string, error) {
-	// walk through the ldconfig output and add entries which contain the filenames
-	// returned by nvidia-container-cli OR the nvliblist.conf file contents
-	ldConfig, err := exec.LookPath("ldconfig")
+	machine, err := elfMachine()
 	if err != nil {
-		return nil, nil, fmt.Errorf("could not lookup ldconfig: %v", err)
+		return nil, nil, fmt.Errorf("could not retrieve ELF machine ID: %v", err)
 	}
-	out, err := exec.Command(ldConfig, "-p").Output()
-	// #5002 If we failed and our ldconfig is not in standard POSIX location, try that
-	if err != nil && ldConfig != systemLdconfig {
-		sylog.Warningf("%s failed - trying %s", ldConfig, systemLdconfig)
-		out, err = exec.Command(systemLdconfig, "-p").Output()
-	}
+	ldCache, err := ldCache()
 	if err != nil {
-		return nil, nil, fmt.Errorf("could not execute ldconfig: %v", err)
+		return nil, nil, fmt.Errorf("could not retrieve ld cache: %v", err)
 	}
 
-	// sample ldconfig -p output:
-	// libnvidia-ml.so.1 (libc6,x86-64) => /usr/lib64/nvidia/libnvidia-ml.so.1
-	r, err := regexp.Compile(`(?m)^(.*)\s*\(.*\)\s*=>\s*(.*)$`)
-	if err != nil {
-		return nil, nil, fmt.Errorf("could not compile ldconfig regexp: %v", err)
-	}
-
-	// get elf machine to match correct libraries during ldconfig lookup
-	self, err := elf.Open("/proc/self/exe")
-	if err != nil {
-		return nil, nil, fmt.Errorf("could not open /proc/self/exe: %v", err)
-	}
-
-	machine := self.Machine
-	if err := self.Close(); err != nil {
-		sylog.Warningf("Could not close ELF: %v", err)
-	}
-
-	// store library name with associated path
-	ldCache := make(map[string]string)
-	for _, match := range r.FindAllSubmatch(out, -1) {
-		if match != nil {
-			// libName is the "libnvidia-ml.so.1" (from the above example)
-			// libPath is the "/usr/lib64/nvidia/libnvidia-ml.so.1" (from the above example)
-			libName := strings.TrimSpace(string(match[1]))
-			libPath := strings.TrimSpace(string(match[2]))
-			ldCache[libPath] = libName
-		}
-	}
-
-	// trach binaries/libraries to eliminate duplicates
+	// Track processed binaries/libraries to eliminate duplicates
 	bins := make(map[string]struct{})
 	libs := make(map[string]struct{})
 
 	var libraries []string
 	var binaries []string
 	for _, file := range gpuFileList {
-		// if the file contains a ".so", treat it as a library
+		// if the file contains an ".so", treat it as a library
 		if strings.Contains(file, ".so") {
-			// if library path is absolute, it will have been returned by the container cli, which is more
-			// accurate than ldconfig when more than one drivers are found with same basename (e.g.
-			// /usr/lib64/libnvidia-tls.so and /usr/lib64/tls/libnvidia-tls.so)
+			// If we have an absolute path, add it 'as-is', plus any symlinks that resolve to it
 			if filepath.IsAbs(file) {
 				elib, err := elf.Open(file)
 				if err != nil {
-					sylog.Debugf("ignore library %s: %s", file, err)
+					sylog.Debugf("ignoring library %s: %s", file, err)
 					continue
 				}
 
 				if elib.Machine == machine {
 					libraries = append(libraries, file)
+					links, err := soLinks(file)
+					if err != nil {
+						sylog.Warningf("ignoring symlinks to %s: %v", file, err)
+					} else {
+						libraries = append(libraries, links...)
+					}
 				}
-
 				if err := elib.Close(); err != nil {
 					sylog.Warningf("Could not close ELIB: %v", err)
 				}
@@ -239,7 +125,7 @@ func paths(gpuFileList []string) ([]string, []string, error) {
 					if _, ok := libs[libName]; !ok {
 						elib, err := elf.Open(libPath)
 						if err != nil {
-							sylog.Debugf("ignore library %s: %s", libName, err)
+							sylog.Debugf("ignoring library %s: %s", libName, err)
 							continue
 						}
 
@@ -247,7 +133,6 @@ func paths(gpuFileList []string) ([]string, []string, error) {
 							libs[libName] = struct{}{}
 							libraries = append(libraries, libPath)
 						}
-
 						if err := elib.Close(); err != nil {
 							sylog.Warningf("Could not close ELIB: %v", err)
 						}
@@ -255,8 +140,7 @@ func paths(gpuFileList []string) ([]string, []string, error) {
 				}
 			}
 		} else {
-			// treat the file as a binary file - add it to the bind list
-			// no need to check the ldconfig output
+			// treat the file as a binary file - find on PATH and add it to the bind list
 			binary, err := exec.LookPath(file)
 			if err != nil {
 				continue
@@ -271,31 +155,54 @@ func paths(gpuFileList []string) ([]string, []string, error) {
 	return libraries, binaries, nil
 }
 
-// NvidiaIpcsPath returns list of nvidia ipcs driver.
-func NvidiaIpcsPath(envPath string) []string {
-	const persistencedSocket = "/var/run/nvidia-persistenced/socket"
-
-	if envPath != "" {
-		oldPath := os.Getenv("PATH")
-		os.Setenv("PATH", envPath)
-		defer os.Setenv("PATH", oldPath)
+// ldcache retrieves a map of absolute path of a library to it's bare name using the system ld cache via `ldconfig -p`
+func ldCache() (map[string]string, error) {
+	// walk through the ldconfig output and add entries which contain the filenames
+	// returned by nvidia-container-cli OR the nvliblist.conf file contents
+	ldConfig, err := exec.LookPath("ldconfig")
+	if err != nil {
+		return nil, fmt.Errorf("could not lookup ldconfig: %v", err)
+	}
+	out, err := exec.Command(ldConfig, "-p").Output()
+	// #5002 If we failed and our ldconfig is not in standard POSIX location, try that
+	if err != nil && ldConfig != systemLdconfig {
+		sylog.Warningf("%s failed - trying %s", ldConfig, systemLdconfig)
+		out, err = exec.Command(systemLdconfig, "-p").Output()
+	}
+	if err != nil {
+		return nil, fmt.Errorf("could not execute ldconfig: %v", err)
 	}
 
-	var nvidiaFiles []string
-	nvidiaFiles, err := nvidiaContainerCli("list", "--ipcs")
+	// sample ldconfig -p output:
+	// libnvidia-ml.so.1 (libc6,x86-64) => /usr/lib64/nvidia/libnvidia-ml.so.1
+	r, err := regexp.Compile(`(?m)^(.*)\s*\(.*\)\s*=>\s*(.*)$`)
 	if err != nil {
-		sylog.Verbosef("nvidiaContainerCli returned: %v", err)
-		sylog.Verbosef("Falling back to default path %s", persistencedSocket)
+		return nil, fmt.Errorf("could not compile ldconfig regexp: %v", err)
+	}
 
-		// nvidia-container-cli may not be installed, check
-		// default path
-		_, err := os.Stat(persistencedSocket)
-		if os.IsNotExist(err) {
-			sylog.Verbosef("persistenced socket %s not found", persistencedSocket)
-		} else {
-			nvidiaFiles = append(nvidiaFiles, persistencedSocket)
+	// store library name with associated path
+	ldCache := make(map[string]string)
+	for _, match := range r.FindAllSubmatch(out, -1) {
+		if match != nil {
+			// libName is the "libnvidia-ml.so.1" (from the above example)
+			// libPath is the "/usr/lib64/nvidia/libnvidia-ml.so.1" (from the above example)
+			libName := strings.TrimSpace(string(match[1]))
+			libPath := strings.TrimSpace(string(match[2]))
+			ldCache[libPath] = libName
 		}
 	}
+	return ldCache, nil
+}
 
-	return nvidiaFiles
+// elfMachine returns the ELF Machine ID for this system, w.r.t the currently running process
+func elfMachine() (machine elf.Machine, err error) {
+	// get elf machine to match correct libraries during ldconfig lookup
+	self, err := elf.Open("/proc/self/exe")
+	if err != nil {
+		return 0, fmt.Errorf("could not open /proc/self/exe: %v", err)
+	}
+	if err := self.Close(); err != nil {
+		sylog.Warningf("Could not close ELF: %v", err)
+	}
+	return self.Machine, nil
 }
