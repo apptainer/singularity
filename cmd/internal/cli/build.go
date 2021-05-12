a3b5640b4ea9d947716414ed63ad205bb81afe47
Mike Frisch <mikef17@gmail.com>
2021-05-12 11:43:23 -0400

    Add space after user prompts for aesthetic purposes

--- cmd/internal/cli/build.go
+++ cmd/internal/cli/build.go
@@ -343,7 +343,7 @@ func checkBuildTarget(path string) error {
 		}
 		if !buildArgs.update && !forceOverwrite {
 
-			question := fmt.Sprintf("Build target '%s' already exists and will be deleted during the build process. Do you want to continue? [N/y]", f.Name())
+			question := fmt.Sprintf("Build target '%s' already exists and will be deleted during the build process. Do you want to continue? [N/y] ", f.Name())
 
 			img, err := image.Init(abspath, false)
 			if err != nil {
@@ -351,7 +351,7 @@ func checkBuildTarget(path string) error {
 					return fmt.Errorf("while determining '%s' format: %s", f.Name(), err)
 				}
 				// unknown image file format
-				question = fmt.Sprintf("Build target '%s' may be a definition file or a text/binary file that will be overwritten. Do you still want to overwrite it? [N/y]", f.Name())
+				question = fmt.Sprintf("Build target '%s' may be a definition file or a text/binary file that will be overwritten. Do you still want to overwrite it? [N/y] ", f.Name())
 			} else {
 				img.File.Close()
 			}
--- internal/app/singularity/remote_login.go
+++ internal/app/singularity/remote_login.go
@@ -113,7 +113,7 @@ func endPointLogin(ep *endpoint.Config, args *LoginArgs) error {
 		// Interactive login
 		// If a token is already set, prompt to see if we want to replace it
 		if ep.Token != "" {
-			input, err := interactive.AskYNQuestion("n", "An access token is already set for this remote. Replace it? [N/y]")
+			input, err := interactive.AskYNQuestion("n", "An access token is already set for this remote. Replace it? [N/y] ")
 			if err != nil {
 				return fmt.Errorf("while reading input: %s", err)
 			}
