32ca87e42fdb6f921336854d93abdb0c1be57648
David Trudgian <dtrudg@sylabs.io>
2021-07-16 13:10:40 -0500

    Handle absolute symlinks in multi-stage copy
    In a multi-stage build `%copy`, if the source for the copy line is a
    symlink we should dereference it and copy the target. Note that if we
    are copying a directory we shouldn't dereference any symlinks
    within the directory, only the directory itself.
    
    Previously we were doing a straight `cp -frH` to implement this, but
    it does not handle the case where a symlink to be copied is an
    absolute symlink. The absolute symlink's target is correct in the
    context of the stage's rootfs, but we are copying in the host context.
    
    To fix this we manually dereference the top level src of a `%copy` line,
    in a manner that will resolve absolute symlinks relative to the src
    stage's rootfs correctly.
    
    This PR also:
    
     - Splits host->rootfs copy, and stage->stage copy functions.
     - Rewrites the tests for host->rootfs and stage->stage copy
       functions to cover more cases including top-level and nested source
       files, dirs, and absolute + relative symlinks.
     - Uses securejoin to restrict resolution of paths to being inside of a
       rootfs where this is appropriate. This is for least-surprise rather
       than security, as the hostfs is still modifiable in `%setup` etc.
    
    Fixes #159

--- internal/pkg/build/files/copy.go
+++ internal/pkg/build/files/copy.go
@@ -10,8 +10,11 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"path"
 	"path/filepath"
 	"strings"
+
+	"github.com/sylabs/singularity/internal/pkg/util/fs"
 )
 
 // makeParentDir ensures existence of the expected destination directory for the cp command
@@ -39,33 +42,33 @@ func makeParentDir(path string, numSrcPaths int) error {
 	return nil
 }
 
-// Copy calls cp with src and dst as its arguments
-// Checks dst and creates parent directories if they do not exist
-// before calling cp.
-// If followLinks is true, the -L flag to cp will follow all symlinks
-// If followLinks is false, the -H flag to cp will only follow links for specified
-// files or files that resolve directly from a glob pattern. It will not follow
-// links found during directory traversal.
-func Copy(src, dst string, followLinks bool) error {
+// CopyFromHost should be used to copy files into the rootfs from the host fs.
+// src is a path relative to CWD on the host, or an absolute path on the host.
+// dstRel is a destination path inside dstRootfs
+// All symlinks encountered in the copy will be dereferenced (cp -L behavior).
+func CopyFromHost(src, dstRel, dstRootfs string) error {
 	// resolve any bash globbing in filepath
 	paths, err := expandPath(src)
 	if err != nil {
 		return fmt.Errorf("while expanding source path with bash: %s: %s", src, err)
 	}
 
-	if err := makeParentDir(dst, len(paths)); err != nil {
+	// Resolve our destination within the container rootfs
+	dstResolved, err := secureJoinKeepSlash(dstRootfs, dstRel)
+	if err != nil {
+		return fmt.Errorf("while resolving destination: %s: %s", dstRel, err)
+	}
+
+	// Create any parent dirs for dst that don't already exist
+	if err := makeParentDir(dstResolved, len(paths)); err != nil {
 		return fmt.Errorf("while creating parent dir: %v", err)
 	}
 
-	// set flags for cp
-	args := []string{"-fHr"}
-	if followLinks {
-		args = []string{"-fLr"}
-	}
+	args := []string{"-fLr"}
 	// append file(s) to be copied
 	args = append(args, paths...)
 	// append dst as last arg
-	args = append(args, dst)
+	args = append(args, dstResolved)
 
 	var output, stderr bytes.Buffer
 	// copy each file into bundle rootfs
@@ -73,7 +76,64 @@ func Copy(src, dst string, followLinks bool) error {
 	copy.Stdout = &output
 	copy.Stderr = &stderr
 	if err := copy.Run(); err != nil {
-		return fmt.Errorf("while copying %s to %s: %s: %s", paths, dst, err, stderr.String())
+		return fmt.Errorf("while copying %s to %s: %s: %s", paths, dstResolved, err, stderr.String())
+	}
+	return nil
+}
+
+// CopyFromStage should be used to copy files into the rootfs from a previous stage.
+// The srcRel and dstRel are src / dst paths relative to the srcRootfs and dstRootfs.
+// Symlinks are only dereferenced for the specified source or files that resolve
+// directly from a specified glob pattern. Any additional links inside a directory
+// being copied are not dereferenced.
+func CopyFromStage(srcRel, dstRel, srcRootfs, dstRootfs string) error {
+	// An absolute path is required for globbing... but with no symlink resolution or
+	// path cleaning yet.
+	srcAbs := joinKeepSlash(srcRootfs, srcRel)
+
+	// resolve any bash globbing in filepath
+	paths, err := expandPath(srcAbs)
+	if err != nil {
+		return fmt.Errorf("while expanding source path with bash: %s: %s", srcAbs, err)
+	}
+
+	// We manually dereference first-level src symlinks only.
+	for _, srcGlobbed := range paths {
+		// Now re-resolve the source files after globbing by using securejoin,
+		// so that absolute symlinks are dereferenced relative to the source rootfs,
+		// and the source is enforced to be inside the rootfs.
+		srcGlobbedRel := strings.TrimPrefix(srcGlobbed, srcRootfs)
+		srcResolved, err := secureJoinKeepSlash(srcRootfs, srcGlobbedRel)
+		if err != nil {
+			return fmt.Errorf("while resolving source: %s: %s", srcGlobbedRel, err)
+		}
+		// If we are copying into a directory then we must use the original source filename,
+		// for the destination filename, not the one that was resolved out.
+		// I.E. if copying `/opt/view` to `/opt/` where `/opt/view links-> /opt/.view/abc123`
+		// we want to create `/opt/view` in the dest, not `/opt/abc123`.
+		dstResolved, err := secureJoinKeepSlash(dstRootfs, dstRel)
+		if err != nil {
+			return fmt.Errorf("while resolving destination: %s: %s", dstRel, err)
+		}
+		if fs.IsDir(dstResolved) {
+			_, srcName := path.Split(srcGlobbedRel)
+			dstResolved = path.Join(dstResolved, srcName)
+		}
+
+		// Create any parent dirs for dst that don't already exist.
+		if err := makeParentDir(dstResolved, len(paths)); err != nil {
+			return fmt.Errorf("while creating parent dir: %v", err)
+		}
+
+		// Set flags for cp to perform a recursive copy without further symlink dereference.
+		args := []string{"-fr", srcResolved, dstResolved}
+		var output, stderr bytes.Buffer
+		copy := exec.Command("/bin/cp", args...)
+		copy.Stdout = &output
+		copy.Stderr = &stderr
+		if err := copy.Run(); err != nil {
+			return fmt.Errorf("while copying %s to %s: %s: %s", paths, dstResolved, err, stderr.String())
+		}
 	}
 	return nil
 }
