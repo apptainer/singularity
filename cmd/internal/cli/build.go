0dd0bf00d2ddffa2a11311073f1eef3e710d0540
David Trudgian <david.trudgian@sylabs.io>
2021-09-10 16:17:11 -0500

    Update copyright year in --mount modified source

--- cmd/internal/cli/build.go
+++ cmd/internal/cli/build.go
@@ -1,5 +1,5 @@
 // Copyright (c) 2020, Control Command Inc. All rights reserved.
-// Copyright (c) 2018-2020, Sylabs Inc. All rights reserved.
+// Copyright (c) 2018-2021, Sylabs Inc. All rights reserved.
 // This software is licensed under a 3-clause BSD license. Please consult the
 // LICENSE.md file distributed with the sources of this project regarding your
 // rights to use or distribute this software.
