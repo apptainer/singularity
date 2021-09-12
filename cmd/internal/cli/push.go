215cf0f37f9b0ca0a241263e1269679275943871
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2021-09-13 00:18:41 +0200

    Fix LGTM warning: Unreachable statement
    This statement is unreachable.

--- cmd/internal/cli/push.go
+++ cmd/internal/cli/push.go
@@ -9,7 +9,6 @@ package cli
 import (
 	"context"
 	"fmt"
-	"os"
 
 	"github.com/hpcng/singularity/docs"
 	"github.com/hpcng/singularity/internal/app/singularity"
@@ -120,7 +119,6 @@ var PushCmd = &cobra.Command{
 				fmt.Printf("TIP: You can push unsigned images with 'singularity push -U %s'.\n", file)
 				fmt.Printf("TIP: Learn how to sign your own containers by using 'singularity help sign'\n\n")
 				sylog.Fatalf("Unable to upload container: unable to verify signature")
-				os.Exit(3)
 			} else if err != nil {
 				sylog.Fatalf("Unable to push image to library: %v", err)
 			}
