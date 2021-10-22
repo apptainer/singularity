fecb00775de4c0ee9a8e5fb1bf605ddc8c97c185
Adam Hughes <stickmanica@gmail.com>
2021-10-22 19:24:01 +0000

    fix: wire up contexts in ORAS push/pull
    Signed-off-by: Dave Dykstra <dwd@fnal.gov>

--- cmd/internal/cli/push.go
+++ cmd/internal/cli/push.go
@@ -130,7 +130,7 @@ var PushCmd = &cobra.Command{
 				sylog.Fatalf("Unable to make docker oci credentials: %s", err)
 			}
 
-			if err := oras.UploadImage(file, ref, ociAuth); err != nil {
+			if err := oras.UploadImage(cmd.Context(), file, ref, ociAuth); err != nil {
 				sylog.Fatalf("Unable to push image to oci registry: %v", err)
 			}
 			sylog.Infof("Upload complete")
