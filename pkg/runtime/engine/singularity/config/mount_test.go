d27ff7a6f5aa6da78302f5372e77ef6df63eb556
Dave Dykstra <dwd@fnal.gov>
2021-11-08 17:31:38 -0600

    Handle multiple newline delimited mounts in ParseMountString

--- pkg/runtime/engine/singularity/config/mount_test.go
+++ pkg/runtime/engine/singularity/config/mount_test.go
@@ -14,103 +14,115 @@ func TestParseMountString(t *testing.T) {
 	tests := []struct {
 		name        string
 		mountString string
-		want        BindPath
+		want        []BindPath
 		wantErr     bool
 	}{
 		{
 			name:        "sourceOnly",
 			mountString: "type=bind,source=/opt",
-			want:        BindPath{},
+			want:        []BindPath{},
 			wantErr:     true,
 		},
 		{
 			name:        "destinationOnly",
 			mountString: "type=bind,destination=/opt",
-			want:        BindPath{},
+			want:        []BindPath{},
 			wantErr:     true,
 		},
 		{
 			name:        "emptySource",
 			mountString: "type=bind,source=,destination=/opt",
-			want:        BindPath{},
+			want:        []BindPath{},
 			wantErr:     true,
 		},
 		{
 			name:        "emptyDestination",
 			mountString: "type=bind,source=/opt,destination=",
-			want:        BindPath{},
+			want:        []BindPath{},
 			wantErr:     true,
 		},
 		{
 			name:        "invalidType",
 			mountString: "type=potato,source=/opt,destination=/opt",
-			want:        BindPath{},
+			want:        []BindPath{},
 			wantErr:     true,
 		},
 		{
 			name:        "invalidField",
-			mountString: "type=bind,source=/opt,destination=/opt,colour=turquoise",
-			want:        BindPath{},
+			mountString: "type=bind,source=/opt,destination=/opt,color=turquoise",
+			want:        []BindPath{},
 			wantErr:     true,
 		},
 		{
 			name:        "simple",
 			mountString: "type=bind,source=/opt,destination=/opt",
-			want: BindPath{
-				Source:      "/opt",
-				Destination: "/opt",
-				Options:     map[string]*BindOption{},
+			want: []BindPath{
+				{
+					Source:      "/opt",
+					Destination: "/opt",
+					Options:     map[string]*BindOption{},
+				},
 			},
 			wantErr: false,
 		},
 		{
 			name:        "simpleSrc",
 			mountString: "type=bind,src=/opt,destination=/opt",
-			want: BindPath{
-				Source:      "/opt",
-				Destination: "/opt",
-				Options:     map[string]*BindOption{},
+			want: []BindPath{
+				{
+					Source:      "/opt",
+					Destination: "/opt",
+					Options:     map[string]*BindOption{},
+				},
 			},
 			wantErr: false,
 		},
 		{
 			name:        "simpleDst",
 			mountString: "type=bind,source=/opt,dst=/opt",
-			want: BindPath{
-				Source:      "/opt",
-				Destination: "/opt",
-				Options:     map[string]*BindOption{},
+			want: []BindPath{
+				{
+					Source:      "/opt",
+					Destination: "/opt",
+					Options:     map[string]*BindOption{},
+				},
 			},
 			wantErr: false,
 		},
 		{
 			name:        "simpleTarget",
 			mountString: "type=bind,source=/opt,target=/opt",
-			want: BindPath{
-				Source:      "/opt",
-				Destination: "/opt",
-				Options:     map[string]*BindOption{},
+			want: []BindPath{
+				{
+					Source:      "/opt",
+					Destination: "/opt",
+					Options:     map[string]*BindOption{},
+				},
 			},
 			wantErr: false,
 		},
 		{
 			name:        "noType",
 			mountString: "source=/opt,destination=/opt",
-			want: BindPath{
-				Source:      "/opt",
-				Destination: "/opt",
-				Options:     map[string]*BindOption{},
+			want: []BindPath{
+				{
+					Source:      "/opt",
+					Destination: "/opt",
+					Options:     map[string]*BindOption{},
+				},
 			},
 			wantErr: false,
 		},
 		{
 			name:        "ro",
 			mountString: "type=bind,source=/opt,destination=/opt,ro",
-			want: BindPath{
-				Source:      "/opt",
-				Destination: "/opt",
-				Options: map[string]*BindOption{
-					"ro": {},
+			want: []BindPath{
+				{
+					Source:      "/opt",
+					Destination: "/opt",
+					Options: map[string]*BindOption{
+						"ro": {},
+					},
 				},
 			},
 			wantErr: false,
@@ -118,11 +130,13 @@ func TestParseMountString(t *testing.T) {
 		{
 			name:        "readonly",
 			mountString: "type=bind,source=/opt,destination=/opt,readonly",
-			want: BindPath{
-				Source:      "/opt",
-				Destination: "/opt",
-				Options: map[string]*BindOption{
-					"ro": {},
+			want: []BindPath{
+				{
+					Source:      "/opt",
+					Destination: "/opt",
+					Options: map[string]*BindOption{
+						"ro": {},
+					},
 				},
 			},
 			wantErr: false,
@@ -130,11 +144,13 @@ func TestParseMountString(t *testing.T) {
 		{
 			name:        "imagesrc",
 			mountString: "type=bind,source=test.sif,destination=/opt,image-src=/opt",
-			want: BindPath{
-				Source:      "test.sif",
-				Destination: "/opt",
-				Options: map[string]*BindOption{
-					"image-src": {Value: "/opt"},
+			want: []BindPath{
+				{
+					Source:      "test.sif",
+					Destination: "/opt",
+					Options: map[string]*BindOption{
+						"image-src": {Value: "/opt"},
+					},
 				},
 			},
 			wantErr: false,
@@ -142,24 +158,26 @@ func TestParseMountString(t *testing.T) {
 		{
 			name:        "imagesrcNoValue",
 			mountString: "type=bind,source=test.sif,destination=/opt,image-src",
-			want:        BindPath{},
+			want:        []BindPath{},
 			wantErr:     true,
 		},
 		{
 			name:        "imagesrcEmpty",
 			mountString: "type=bind,source=test.sif,destination=/opt,image-src=",
-			want:        BindPath{},
+			want:        []BindPath{},
 			wantErr:     true,
 		},
 		{
 			name:        "id",
 			mountString: "type=bind,source=test.sif,destination=/opt,image-src=/opt,id=2",
-			want: BindPath{
-				Source:      "test.sif",
-				Destination: "/opt",
-				Options: map[string]*BindOption{
-					"image-src": {Value: "/opt"},
-					"id":        {Value: "2"},
+			want: []BindPath{
+				{
+					Source:      "test.sif",
+					Destination: "/opt",
+					Options: map[string]*BindOption{
+						"image-src": {Value: "/opt"},
+						"id":        {Value: "2"},
+					},
 				},
 			},
 			wantErr: false,
@@ -167,28 +185,47 @@ func TestParseMountString(t *testing.T) {
 		{
 			name:        "idNoValue",
 			mountString: "type=bind,source=test.sif,destination=/opt,image-src=/opt,id",
-			want:        BindPath{},
+			want:        []BindPath{},
 			wantErr:     true,
 		},
 		{
 			name:        "idEmpty",
 			mountString: "type=bind,source=test.sif,destination=/opt,image-src=/opt,id=",
-			want:        BindPath{},
+			want:        []BindPath{},
 			wantErr:     true,
 		},
 		{
 			name:        "bindpropagation",
 			mountString: "type=bind,source=/opt,destination=/opt,bind-propagation=shared",
-			want:        BindPath{},
+			want:        []BindPath{},
 			wantErr:     true,
 		},
 		{
 			name:        "csvEscaped",
 			mountString: `type=bind,"source=/comma,dir","destination=/quote""dir"`,
-			want: BindPath{
-				Source:      "/comma,dir",
-				Destination: "/quote\"dir",
-				Options:     map[string]*BindOption{},
+			want: []BindPath{
+				{
+					Source:      "/comma,dir",
+					Destination: "/quote\"dir",
+					Options:     map[string]*BindOption{},
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name:        "multiple",
+			mountString: "type=bind,source=/opt,destination=/opt\ntype=bind,source=/srv,destination=/srv",
+			want: []BindPath{
+				{
+					Source:      "/opt",
+					Destination: "/opt",
+					Options:     map[string]*BindOption{},
+				},
+				{
+					Source:      "/srv",
+					Destination: "/srv",
+					Options:     map[string]*BindOption{},
+				},
 			},
 			wantErr: false,
 		},
